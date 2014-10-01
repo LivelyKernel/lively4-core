b549b2591b7d4660cfa6f9261190338877ab2b8b
Robert Krahn <robert.krahn@gmail.com>
2014-10-01 04:05:14 +0000

    adding failing string tests

--- tests/string-test.js
+++ tests/string-test.js
@@ -10,197 +10,106 @@ var string = jsext.string;
 
 describe('string', function() {
 
+  it("tableize", function() {
+    expect([["a", "b", "c"], ["d", "e", "f"]]).to.eql(Strings.tableize('a b c\nd e f'));
+    expect([["a", 1, "c"], ["d", 2, "f"]]).to.eql(Strings.tableize('a 1 c\nd 2 f'));
+    expect([["Date"], [new Date('06/11/2013')]]).to.eql(Strings.tableize('Date\n06/11/2013'));
+  });
+
+  it("lineLookupByIndex", function() {
+    var string = 'test\n123\nfo\nbarbaz\nzork\n';
+    var lookupFunc = Strings.lineIndexComputer(string);
+    expect(0).to.equal(lookupFunc(0),"char pos: 0");
+    expect(0).to.equal(lookupFunc(1),"char pos: 1");
+    expect(0).to.equal(lookupFunc(4),"char pos: 4");
+    expect(1).to.equal(lookupFunc(5),"char pos: 5");
+    expect(1).to.equal(lookupFunc(7),"char pos: 7");
+    expect(1).to.equal(lookupFunc(8),"char pos: 8");
+    expect(2).to.equal(lookupFunc(9),"char pos: 9");
+    expect(4).to.equal(lookupFunc(23),"char pos: 2");
+    expect(5).to.equal(lookupFunc(24),"char pos: 2");
+    expect(-1).to.equal(lookupFunc(99),"char pos: 9");
+  });
+
+  it("findParagraphs", function() {
+    var tests = [
+      {string: 'foo', expected: ['foo']},
+      {string: 'foo\nbar', expected: ['foo\nbar']},
+      {string: 'foo\n\nbar', expected: ['foo', 'bar']},
+      {string: 'foo\n\n\n\nbar', expected: ['foo', 'bar']},
+      {string: 'a\n\n\n\nb\nc', expected: ['a', '\n\n', 'b\nc'], options: {keepEmptyLines: true}}
+    ];
+    tests.forEach(function(test) {
+      expect(test.expected).to.equal(Strings.paragraphs(test.string, test.options));
+    });
+  });
+
+  it("printTree", function() {
+    var tree = tree = {
+      string: "root",
+      children: [{
+        string: "a",
+        children: [{
+          string: "b",
+          children: [{string: "c"},{string: "d"}]
+        }],
+      },{
+        string: "e",
+        children: [{
+          string: "f",
+          children: [{ string: "g" },{ string: "h" }]
+        }]
+      }]
+    };
+    var expected = "root\n"
+           + "|---a\n"
+           + "|   \\---b\n"
+           + "|      |---c\n"
+           + "|      \\---d\n"
+           + "\\---e\n"
+           + "  \\---f\n"
+           + "    |---g\n"
+           + "    \\---h";
+    var actual = Strings.printTree(tree,
+        function(n) { return n.string; },
+        function(n) { return n.children; }, '   ');
+    expect(expected).to.equal(actual);
+  });
+
+  it("stringMatch", function() {
+    var sucesses = [
+      {string: "foo bar", pattern: "foo bar"},
+      {string: "foo   bar", pattern: "foo bar", normalizeWhiteSpace: true},
+      {string: "foo bar", pattern: "foo __/bar/__"},
+      {string: "foo bar 123 baz", pattern: "foo bar __/[0-9]+/__ baz"},
+      {string: "  foo\n   123\n bla", pattern: "foo\n __/[0-9]+/__\n     bla", ignoreIndent: true}];
+    sucesses.forEach(function(ea) {
+      var match = Strings.stringMatch(
+        ea.string, ea.pattern,
+        {normalizeWhiteSpace: ea.normalizeWhiteSpace, ignoreIndent: ea.ignoreIndent});
+      expect(match.matched).to.equal(true,
+        'stringMatch not matching:\n' + ea.string
+        + '\nwith:\n'+ ea.pattern
+        + '\nbecause:\n ' + Objects.inspect(match));
+    });
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    var failures = [
+      {string: "foo bar 12x baz", pattern: "foo bar __/[0-9]+/__ baz"}];
+    failures.forEach(function(ea) {
+      var match = Strings.stringMatch(
+        ea.string, ea.pattern,
+        {normalizeWhiteSpace: ea.normalizeWhiteSpace});
+      expect(!match.matched).to.equal(true,
+        'stringMatch unexpectedly matched:\n' + ea.string
+        + '\nwith:\n'+ ea.pattern
+        + '\nbecause:\n ' + Objects.inspect(match));
+    });
+  });
+
+  it("reMatches", function() {
+    var string = 'abc def abc xyz';
+    var expected = [{start: 4, end: 11, match: "def abc"}];
+    expect(expected).to.eql(Strings.reMatches(string, /def\s[^\s]+/));
+  });
+
 });
-
-// TestCase.subclass('lively.lang.tests.ExtensionTests.ObjectTest',
-// 'testing', {
-//     testExtendSetsDisplayName: function() {
-//         var obj = {};
-//         Object.extend(obj, {foo: function() {return "bar"}})
-//         this.assertEquals(obj.foo.displayName, "foo")
-//     },
-
-//     testExtendDoesNotOverrideExistingName: function() {
-//         var obj = {};
-//         Object.extend(obj, {foo: function myFoo() {return "bar"}})
-//         this.assertEquals(obj.foo.name, "myFoo", "name changed")
-//         this.assert(!obj.foo.displayName, "displayName is set")
-//     },
-
-//     testExtendDoesNotOverrideExistingDisplayName: function() {
-//         var obj = {};
-//         var f = function() {return "bar"};
-//         f.displayName = "myDisplayFoo"
-//         Object.extend(obj, {foo: f})
-//         this.assertEquals(obj.foo.name, "", "function has a name")
-//         this.assertEquals(obj.foo.displayName, "myDisplayFoo", "displayName was overridden")
-//     },
-
-//     testExtendDoesSourceModule: function() {
-//         var obj = {};
-//         var f = function() {return "bar"};
-//         f.displayName = "myDisplayFoo"
-//         Object.extend(obj, {foo: f})
-//         this.assert(obj.foo.sourceModule, "source module not set")
-//     },
-//     testIsEmpty: function() {
-//         var obj1 = {}, obj2 = {a:3}, obj3 = Object.create(obj2);
-//         this.assertEquals(Object.isEmpty(obj1), true);
-//         this.assertEquals(Object.isEmpty(obj2), false);
-//         this.assertEquals(Object.isEmpty(obj3), true);
-//     },
-//     testDeepCopy: function() {
-//         var obj = {a: 3, b: {c: [{}],d: undefined}, e: null, f: function(){}, g: "string"};
-//         var copy = Object.deepCopy(obj);
-//         this.assertMatches(obj, copy);
-//         this.assertMatches(copy, obj);
-//         this.assert(obj !== copy);
-//         this.assert(obj.b !== copy.b);
-//         this.assert(obj.b.c !== copy.b.c);
-//         this.assert(obj.b.c[0] !== copy.b.c[0]);
-//     }
-// });
-
-// TestCase.subclass('lively.lang.tests.ExtensionTests.ObjectsTest',
-// 'testing', {
-//     testTypeStringOf: function() {
-//         this.assertEquals(Objects.typeStringOf('some string'), 'String');
-//         this.assertEquals(Objects.typeStringOf(0), 'Number');
-//         this.assertEquals(Objects.typeStringOf(null), 'null');
-//         this.assertEquals(Objects.typeStringOf(undefined), 'undefined');
-//         this.assertEquals(Objects.typeStringOf([]), 'Array');
-//         this.assertEquals(Objects.typeStringOf({a: 2}), 'Object');
-//         this.assertEquals(Objects.typeStringOf(new lively.morphic.Morph()), 'Morph');
-//     },
-//     testShortPrintStringOf: function() {
-//         this.assertEquals(Objects.shortPrintStringOf([1,2]), '[...]', 'filled arrays should be displayed as [...]');
-//         this.assertEquals(Objects.shortPrintStringOf([]), '[]', 'empty arrays should be displayed as []');
-//         this.assertEquals(Objects.shortPrintStringOf(0), '0', 'numbers should be displayed as values');
-//         this.assertEquals(Objects.shortPrintStringOf(new lively.morphic.Morph()), 'Morph', 'short typestring of a morph is still Morph');
-//     },
-//     testIsMutableType: function() {
-//         this.assert(Objects.isMutableType([1,2]), 'arrays are mutable');
-//         this.assert(Objects.isMutableType({}), 'empty objects are mutable');
-//         this.assert(Objects.isMutableType(new lively.morphic.Morph()), 'complex objects are mutable');
-//         this.assert(!Objects.isMutableType(2), 'numbers are immutable');
-//     },
-//     testSafeToString: function() {
-//         this.assertEquals(Objects.safeToString(null), 'null');
-//         this.assertEquals(Objects.safeToString(undefined), 'undefined');
-//         this.assertEquals(Objects.safeToString(2), '2');
-//     }
-// });
-
-// TestCase.subclass('lively.lang.tests.ExtensionTests.PropertiesTest',
-// 'running', {
-//     setUp: function() {
-//         var Foo = function() {
-//             this.a = 1;
-//             this.aa = 1;
-//             this.b = Functions.True;
-//         };
-//         Foo.prototype.c = 2;
-//         Foo.prototype.cc = 2;
-//         Foo.prototype.d = Functions.False;
-//         this.sut = new Foo();
-//     }
-// },
-// 'testing', {
-//     testAll: function() {
-//         var expected, result;
-//         expected = ["a", "c"];
-//         result = Properties.all(this.sut, function (name, object) {
-//             return name.length == 1;
-//         });
-//         this.assertMatches(expected, result);
-//         expected = ["aa", "cc"];
-//         result = Properties.all(this.sut, function (name, object) {
-//             return name.length == 2;
-//         });
-//         this.assertMatches(expected, result);
-//     },
-//     testOwn: function() {
-//         var expected = ["a", "aa"];
-//         var result = Properties.own(this.sut);
-//         this.assertMatches(expected, result);
-//     },
-//     testAllProperties: function() {
-//         var expected, result;
-//         expected = ["a", "b", "c", "d"];
-//         result = Properties.allProperties(this.sut, function (object, name) {
-//             return name.length == 1;
-//         });
-//         this.assertMatches(expected, result);
-//         expected = ["aa", "cc"];
-//         result = Properties.allProperties(this.sut, function (object, name) {
-//             return name.length == 2;
-//         });
-//         this.assertMatches(expected, result);
-//     }
-// });
-
-// TestCase.subclass('lively.lang.tests.ExtensionTests.PropertyPath',
-// // todo:
-// // - listen? wrap? watch?
-// 'testing', {
-//     testParsePath: function() {
-//         this.assertEquals([], lively.PropertyPath(undefined).parts());
-//         this.assertEquals([], lively.PropertyPath('').parts());
-//         this.assertEquals([], lively.PropertyPath('.').parts());
-//         this.assertEquals(['foo'], lively.PropertyPath('foo').parts());
-//         this.assertEquals(['foo', 'bar'], lively.PropertyPath('foo.bar').parts());
-//     },
-//     testPathAccesor: function() {
-//         var obj = {foo: {bar: 42}, baz: {zork: {'x y z z y': 23}}};
-//         this.assertEquals(obj, lively.PropertyPath('').get(obj));
-//         this.assertEquals(42, lively.PropertyPath('foo.bar').get(obj));
-//         this.assertEquals(obj.baz.zork, lively.PropertyPath('baz.zork').get(obj));
-//         this.assertEquals(23, lively.PropertyPath('baz.zork.x y z z y').get(obj));
-//         this.assertEquals(undefined, lively.PropertyPath('non.ex.is.tan.t').get(obj));
-//     },
-//     testPathIncludes: function() {
-//         var base = lively.PropertyPath('foo.bar');
-//         this.assert(base.isParentPathOf('foo.bar'), 'equal paths should be "parents"');
-//         this.assert(base.isParentPathOf(base), 'equal paths should be "parents" 2');
-//         this.assert(base.isParentPathOf('foo.bar.baz'), 'foo.bar.baz');
-//         this.assert(!base.isParentPathOf('foo.baz'), 'foo.baz');
-//         this.assert(!base.isParentPathOf('.'), '.');
-//         this.assert(!base.isParentPathOf(''), 'empty string');
-//         this.assert(!base.isParentPathOf(), 'undefined');
-//     },
-//     testRelativePath: function() {
-//         var base = lively.PropertyPath('foo.bar');
-//         this.assertEquals([], base.relativePathTo('foo.bar').parts(), 'foo.bar');
-//         this.assertEquals(['baz', 'zork'], base.relativePathTo('foo.bar.baz.zork').parts(), 'foo.bar.baz.zork');
-//     },
-//     testConcat: function() {
-//         var p1 = lively.PropertyPath('foo.bar'),
-//             p2 = lively.PropertyPath('baz.zork');
-//         this.assertEquals('baz.zork.foo.bar', p2.concat(p1));
-//         this.assertEquals('foo.bar.baz.zork', p1.concat(p2));
-//     },
-//     testSet: function() {
-//         var obj = {foo:[{},{bar:{}}]}, p = lively.PropertyPath('foo.1.bar.baz');
-//         p.set(obj, 3);
-//         this.assertEquals(3, obj.foo[1].bar.baz);
-//     },
-//     testEnsure: function() {
-//         var obj = {}, p = lively.PropertyPath('foo.bar.baz');
-//         p.set(obj, 3, true);
-//         this.assertEquals(3, obj.foo.bar.baz);
-//     },
-//     testSplitter: function() {
-//         var obj = {}, p = lively.PropertyPath('foo/bar/baz', '/');
-//         p.set(obj, 3, true);
-//         this.assertEquals(3, obj.foo.bar.baz);
-//     },
-//     testParentPathOf: function() {
-//         var pp = lively.PropertyPath,
-//             p1 = pp("a.b")
-//         this.assert(p1.isParentPathOf(p1));
-//         this.assert(pp("a").isParentPathOf(p1))
-//         this.assert(pp("").isParentPathOf(pp("")))
-//         this.assert(!p1.isParentPathOf(pp("a")))
-//         this.assert(!p1.isParentPathOf(pp("b.a")))
-//     }
-// });
