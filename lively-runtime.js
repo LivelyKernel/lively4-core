50378ab6572a871f54a92e7fef87b52ba7845670
Robert Krahn <robert.krahn@gmail.com>
2014-11-29 02:19:51 +0000

    lively-runtime: adding test change handler

--- lively-runtime.js
+++ lively-runtime.js
@@ -1,46 +1,72 @@
 lively.require("lively.lang.Runtime").toRun(function() {
 
-  // Create Lively runtime project for lively.lang and register the lib/ files
-  // TODO: add tests
-  var runtime = lively.lang.Runtime;
-  var registry = runtime.Registry.default();
-  var baseDir = "/home/lively/expt/lively-lang/lib/";
-  var files = ["base.js","collection.js","date.js","class.js","events.js","function.js","messenger.js","number.js","object.js","string.js","worker.js"];
-  var project = runtime.Registry.addProject(registry, {name: "lively.lang", resources: {}});
-  files.forEach(function(fn) {
-  	runtime.Project.addResource(project, {name: baseDir+fn, code: "", varRecorder: {}})
-  });
-  
-  // React to changes of the lib files. Note that a change of base.js needs to
-  // be handled differently b/c it can redefine the entire lively.lang object.
-  // Changes of other files require us to update "our" lively.lang object but
-  // also to re-install the globalized version of the interface methods to
-  // String,Array,...
-  runtime.Project.setChangeHandlers(project, [{
-    debug: true,
-    matchesResource: /.*lively.lang\/lib\/.*.js$/,
-    handler: function(change, project, resource, whenHandled) {
-    	var isBase = !!resource.name.match("lib/base.js"),
-  				state = isBase ? {window: {}} : {lively: {lang: lively.lang}};
-  		lively.lang.VM.runEval(
-  		  change.newSource,
-  		  {topLevelVarRecorder: state, context: state, sourceURL: resource.name},
-  		  function(err, result) {
-    			if (!err) {
-    			  var lang = isBase ? state.window.lively.lang : state.lively.lang
-    				try {
-    					if (isBase) { // new lively.lang object
-    					  Object.keys(lively.lang)
-      					  .forEach(function(k) { if (!lang[k]) lang[k] = lively.lang[k]; });
-      					 lively.lang = lang;
-      					lively.lang.deprecatedLivelyPatches();
-    					}
-    				} catch (e) { err = e; }
-    			}
-    			err && show("error when updating the runtime for " + resource.name + "\n" + (err.stack || err));
-    			!err && alertOK("lively;lang updated");
-    	  	whenHandled();
-    		});
+  var r = lively.lang.Runtime.Registry;
+  r.addProject(r.default(), {
+    name: "lively.lang",
+    rootDir: "/home/lively/expt/lively-lang",
+
+    resources: {
+
+      // React to changes of the lib files. Note that a change of base.js needs to
+      // be handled differently b/c it can redefine the entire lively.lang object.
+      // Changes of other files require us to update "our" lively.lang object but
+      // also to re-install the globalized version of the interface methods to
+      // String,Array,...
+
+      "base.js": {
+        matches: /lively.lang\/lib\/base\.js$/,
+        changeHandler: function(change, project, resource) {
+  				var state = {window: {}};
+  				evalCode(change.newSource, state, change.resourceId);
+  				var lang = state.window.lively && state.window.lively.lang
+  				if (!lang) return;
+				  Object.keys(lively.lang)
+					  .forEach(function(k) { if (!lang[k]) lang[k] = lively.lang[k]; });
+					lively.lang = lang;
+					lively.lang.deprecatedLivelyPatches();
+        }
+      },
+
+      "lib code": {
+        matches: /lively.lang\/lib\/.*.js$/,
+        changeHandler: function(change, project, resource) {
+          evalCode(change.newSource, {lively: {lang: lively.lang}}, change.resourceId);
+        }
+      },
+
+      "tests": {
+        matches: /lively.lang\/tests\/.*.js$/,
+        changeHandler: function(change, project, resource) {
+          lively.lang.fun.composeAsync(
+            function(next) {
+              lively.require("lively.MochaTests").toRun(function() { next(); });
+            },
+            function(next) {
+              JSLoader.forcedReload("http://cdnjs.cloudflare.com/ajax/libs/expect.js/0.2.0/expect.min.js");
+              lively.lang.fun.waitFor(3000, function() { return typeof expect !== "undefined" && expect !== chai.expect; }, next);
+            },
+            function(next) {
+              evalCode(change.newSource, {expect: expect, lively: lively}, change.resourceId);
+              next();
+            }
+          )(function(err) {
+            if (err) show(String(err));
+            else alertOK("defining tests for " + change.resourceId);
+          });
+        },
+      }
+
     }
-  }]);
+  });
+
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  function evalCode(code, state, resourceName) {
+    lively.lang.VM.runEval(code,
+      {topLevelVarRecorder: state, context: state, sourceURL: resourceName},
+      function(err, result) {
+    		err && show("error when updating the runtime for " + resourceName + "\n" + (err.stack || err));
+    		!err && alertOK("runtime updated for " + resourceName);
+    	});
+  }
 });
