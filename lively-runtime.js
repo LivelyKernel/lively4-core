b58cbfe748d8a64e43dd20b86d267a54b9973435
Robert Krahn <robert.krahn@gmail.com>
2014-12-29 22:53:53 -0800

    runtime: reloadAll

--- lively-runtime.js
+++ lively-runtime.js
@@ -3,7 +3,36 @@ lively.require("lively.lang.Runtime").toRun(function() {
   var r = lively.lang.Runtime.Registry;
   r.addProject(r.default(), {
     name: "lively.lang",
-    rootDir: "/home/lively/expt/lively-lang",
+    rootDir: "/Users/robert/Lively/lively.lang",
+
+    reloadAll: function(project, thenDo) {
+      // var project = r.default().projects["lively.lang"];
+      // project.reloadAll(project, function(err) { err ? show(err.stack || String(err)) : alertOK("reloaded!"); })
+      var files = [
+        "lib/base.js","lib/class.js","lib/collection.js","lib/date.js","lib/events.js","lib/function.js",
+        "lib/messenger.js","lib/number.js","lib/object.js","lib/string.js","lib/tree.js","lib/worker.js"
+      ];
+
+      lively.lang.fun.composeAsync(
+        function deps(n) { lively.requires("lively.MochaTests").toRun(function() { n(); }); },
+        function readFiles(n) {
+          lively.lang.arr.mapAsyncSeries(files,
+            function(fn,_,n) {
+              lively.shell.cat(fn, {cwd: project.rootDir},
+              function(err, c) { n(err, {name: fn, content: c}); });
+            }, n);
+        },
+        function(fileContents, next) {
+          lively.lang.arr.mapAsyncSeries(fileContents,
+            function(ea,_,n) {
+              r.Project.processChange(
+                project, lively.lang.string.joinPath(project.rootDir, ea.name),
+                ea.content, n);
+            },
+            next);
+        }
+      )(thenDo);
+    },
 
     resources: {
 
