47df8b524e3487b3caececc946c67695246db425
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-07-07 13:25:59 +0200

    change expr.js to es5

--- src/expr.js
+++ src/expr.js
@@ -1,41 +1,47 @@
-define(function() { 'use strict'
-  class Expr {
-    constructor() {
-      this.initialize.apply(this, arguments);
-    }
-    initialize() {}
-  }
+define(function module(foo, bar) {
 
-  class NumExpr extends Expr {
-    initialize(val) {
-      super.initialize();
-      this.val = val;
-    }
-    toString() {
-      return this.val.toString();
-    }
-    result() {
-      return this.val;
-    }
-  }
+  var NumExpr = function() { this.initialize.apply(this, arguments); };
 
-  class AddExpr extends Expr {
-    initialize(expr1, expr2) {
-      super.initialize();
-      this.expr1 = expr1;
-      this.expr2 = expr2;
-    }
-    toString() {
-      return '(' + this.expr1.toString() + ' + ' + this.expr2.toString() + ')';
-    }
-    result() {
-      return this.expr1.result() + this.expr2.result();
-    }
-  }
+  NumExpr.prototype.initialize = function(val) {
+    this.val = val;
+  };
+  NumExpr.prototype.toString = function() {
+    return this.val.toString();
+  };
+  NumExpr.prototype.result = function() {
+    return this.val;
+  };
+
+  var NegExpr = function() { this.initialize.apply(this, arguments); };
+
+  NegExpr.prototype.initialize = function(expr) {
+    this.expr = expr;
+  };
+  NegExpr.prototype.toString = function() {
+    return '(-' + this.expr.toString() + ')';
+  };
+  NegExpr.prototype.result = function() {
+    return -1 * this.expr.result();
+  };
+
+  var AddExpr = function() { this.initialize.apply(this, arguments); };
+
+  AddExpr.prototype.initialize = function(expr1, expr2) {
+    this.expr1 = expr1;
+    this.expr2 = expr2;
+  };
+  AddExpr.prototype.toString = function() {
+    return '(' + this.expr1.toString() + ' + ' + this.expr2.toString() + ')';
+  };
+  AddExpr.prototype.result = function() {
+    var result1 = this.expr1.result(),
+        result2 = this.expr2.result();
+    return result1 + result2;
+  };
 
   return {
-    Expr,
-    NumExpr,
-    AddExpr
+    NumExpr: NumExpr,
+    NegExpr: NegExpr,
+    AddExpr: AddExpr
   };
 });
