06bda4919309601895a7e10beda1f4ce91079b42
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-07-08 15:23:47 +0200

    baseset as class

--- src/baseset.js
+++ src/baseset.js
@@ -2,20 +2,20 @@ define(function module(require) { "use strict"
 
   var withAdvice = require('./../lib/flight/advice').withAdvice;
 
-  var BaseSet = function() { this.initialize.apply(this, arguments); };
-
-  BaseSet.prototype.initialize = function(val) {
-    this.set = new Set();
-    this.downstream = [];
-  };
-  BaseSet.prototype.add = function(item) {
-    this.downstream.forEach(function(ea) { ea.newItem(item); });
-    return this.set.add(item);
-  };
-  BaseSet.prototype.delete = function(item) {
-    this.downstream.forEach(function(ea) { ea.destroyItem(item); });
-    return this.set.delete(item);
-  };
+  var BaseSet = Object.subclass('BaseSet', {
+    initialize: function(val) {
+      this.set = new Set();
+      this.downstream = [];
+    },
+    add: function(item) {
+      this.downstream.forEach(function(ea) { ea.newItem(item); });
+      return this.set.add(item);
+    },
+    delete: function(item) {
+      this.downstream.forEach(function(ea) { ea.destroyItem(item); });
+      return this.set.delete(item);
+    }
+  });
 
   return BaseSet;
 });
