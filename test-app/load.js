c95ab1086d6f1d58a4c7883ba7981d96fd437be7
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-08-07 15:17:01 +0200

    added dropbox testing

--- test-app/load.js
+++ test-app/load.js
@@ -14,7 +14,21 @@ System.import('https://code.jquery.com/jquery-2.1.4.js').then(function(module) {
         url: "https://eval/3+4",
         type: "get",
         success: function(d) { console.log(d)}
-    })
+    });
+
+    var base = "https://api-content.dropbox.com/1/",
+        method = "files/auto/", // get
+        path = "Lively/foo.txt",
+        dropboxAccessToken = "Or5ZKMnok7sAAAAAAANCpDZwejT76YsmrfvOVqQU5al1psc0RmA96NgMId1Dr9PE";
+
+    $.ajax({
+        url: base + method + path +"?access_token=" + dropboxAccessToken,
+        type: "get",
+        success: function(d) {
+            console.log("DROPBOX");
+            console.log(d);
+        }
+    });
 }).catch(function (e) {
     console.error('$', e);
 });
