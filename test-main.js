e31db74458507dfef6e7799cb0bcef6cb7cf9211
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-08-07 12:05:18 +0200

    include service worker in test-main

--- test-main.js
+++ test-main.js
@@ -1,8 +1,6 @@
 var allTestFiles = [];
 var TEST_REGEXP = /(spec|test)\.js$/i;
 
-//console.log('Window Karma Files', window.__karma__.files);
-
 // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function(file) {
   if (TEST_REGEXP.test(file)) {
@@ -14,8 +12,17 @@ Object.keys(window.__karma__.files).forEach(function(file) {
   }
 });
 
-//console.log('RequireJS allTestFiles', allTestFiles);
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceworker.js', {
+    scope: "http://localhost:9876/"
+  }).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
 
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
@@ -25,5 +32,7 @@ require.config({
   deps: allTestFiles,
 
   // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
+  callback: function() {
+    navigator.serviceWorker.ready.then(window.__karma__.start);
+  }
 });
