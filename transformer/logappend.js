d849a7a75eae488c6cd5cb455cb6fb95a9df068c
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-08-13 17:07:17 +0200

    moved log append code to service worker

--- transformer/logappend.js
+++ transformer/logappend.js
@@ -1,49 +1,4 @@
-'use strict';
 
-class LogAppend {
-    match(response) {
-        return response.url.endsWith('.js');
-    }
-
-    transform(response) {
-        return response.clone().blob()
-            .then(function(blob) {
-                function readBlob(blob) {
-                    console.log('Read blob of type ' + blob.type);
-                    return new Promise(function(resolve, reject) {
-                        var reader = new FileReader();
-
-                        reader.addEventListener("load", function() {
-                            resolve(reader.result);
-                        });
-                        reader.addEventListener("error", function(err) {
-                            reject(err, 'Error during reading Blob');
-                        });
-
-                        reader.readAsBinaryString(blob);
-                    });
-                }
-
-                return readBlob(blob)
-                    .then(function srcTransform(content) {
-                        return content + '\nconsole.log(1234567);';
-                    })
-                    .then(function packNewBlob(newContent) {
-                        return new Blob([newContent], {
-                            type: blob.type
-                        });
-                    });
-            })
-            .then(function packNewResponse(newBlob) {
-                // pack new Response from a Blob and the given Response
-                return new Response(newBlob, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: response.headers
-                });
-            });
-    }
-}
 
 //var srcTransform = new BabelsbergSrcTransform();
 //var src = srcTransform.transform(callback.toString());
