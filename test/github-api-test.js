ba228e0d88f8700fbbf4cca879894efd923ea3b7
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-09 16:54:18 +0200

    test ajax request

--- test/github-api-test.js
+++ test/github-api-test.js
@@ -2,24 +2,18 @@ define(function(require) {
     "use strict";
 
     var expect = chai.expect;
-    var $ = require('https://code.jquery.com/jquery-2.1.4.js');
+    require('https://code.jquery.com/jquery-2.1.4.js');
 
     describe('Github API', function() {
-/*
-        it('should DO THINGS', function(done) {
+        it('should load appropriately', function(done) {
             $.ajax({
                 url: "https://eval/3+4",
                 type: "get",
                 success: function(d) {
-                    expect('7').to.be(d);
-                    console.log('FOOOOOOOOOOOOOOOOOOOOOOOOO');
+                    expect('7').to.equal(d);
                     done();
                 }
             });
-        });
-*/
-        it('should DO IT', function() {
-            expect(add(17, 25)).to.equal(42);
         });
     });
 });
