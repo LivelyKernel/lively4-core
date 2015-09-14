764e0d88eae7068d3704225feb8cc40109e98607
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-14 17:12:24 +0200

    stub implementation of client-side github access

--- client/github/github-client.js
+++ client/github/github-client.js
@@ -1,7 +1,19 @@
 define(function(require, exports, module) {
     "use strict";
 
+    require('../../src/external/jquery-1.11.3.js');
+
     module.exports = {
-        answer: 42
+        answer: 42,
+        getRepo: function(cb) {
+            $.ajax({
+                url: "https://githubapi/",
+                type: "get",
+                data: {
+                    foo: 'bar'
+                },
+                success: cb
+            });
+        }
     };
 });
