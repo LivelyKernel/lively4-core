fdeeb631fd90c2d325a88678c36373f22f38f869
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-14 17:13:38 +0200

    import github module and try to load a repo

--- test-app/app.js
+++ test-app/app.js
@@ -1,7 +1,16 @@
 // es6 application code
 
 import Human from './human.js'
+import Github from './../client/github/github-client.js'
+import lively4 from './../src/lively4-client.js'
 
 let foo = new Human("Foo", "Bar");
+
+Github.getRepo((result) => {
+    "use strict";
+    console.log('# # # # # # # # # # # # # # # # # # # # # ');
+    console.log(result);
+    console.log(Github);
+});
 
 document.querySelector("h1").innerHTML = `Hello ${foo.toString()}`;
