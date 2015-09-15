27f79cdfb5a07d74d3b76544041ebfea40b3305c
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-15 17:53:56 +0200

    running test on stub implementation of thenable messages

--- test/messaging-test.js
+++ test/messaging-test.js
@@ -8,14 +8,20 @@ define(function(require) {
         it('should answer messages', function(done) {
             var message = 'expected message';
 
-            messaging.postMessage(message).then(function(event) {
-                console.log('INSIDE PROMISE!!!!!!!!!!!!!!!!!!!!!!!');
-                console.log(event);
-                console.log(event.data);
-                console.log(event);
-                expect(event.data.data.sendedMessage).to.equal(message);
-                done();
-            });
+            messaging.postMessage({
+                meta: 'foo',
+                data: message
+            })
+                .then(function(event) {
+                    console.log('INSIDE PROMISE!!!!!!!!!!!!!!!!!!!!!!!');
+                    console.log(event);
+                    console.log(event.data);
+                    console.log(event.data.meta);
+                    console.log(event.data.meta.receivedMessage);
+                    console.log(event.data.meta.receivedMessage.meta);
+                    expect(event.data.meta.receivedMessage.meta).to.equal('foo');
+                })
+                .then(done);
         });
     });
 });
