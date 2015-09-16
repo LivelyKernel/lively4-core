f3e7907eb017f6d4627eb9ca7d12738c968626dc
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-16 16:09:22 +0200

    deleted client side github module

--- test-app/app.js
+++ test-app/app.js
@@ -1,7 +1,6 @@
 // es6 application code
 
 import Human from './human.js'
-import Github from './../client/github/github-client.js'
 import lively4 from './../src/lively4-client.js'
 import messaging from './../src/client/messaging.js'
 
