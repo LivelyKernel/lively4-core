d5cfa87508d1a625075380dae620e428a286b39e
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-16 12:04:23 +0200

    test involving multiple concurrent message sends

--- test/messaging-test.js
+++ test/messaging-test.js
@@ -5,7 +5,7 @@ define(function(require) {
     var messaging = require('./../src/client/messaging.js');
 
     describe('Client Messaging API', function() {
-        it('should answer messages', function(done) {
+        it('should answer a single message', function(done) {
             var message = 'expected message';
 
             messaging.postMessage({
@@ -15,17 +15,46 @@ define(function(require) {
                 message: message
             })
                 .then(function(event) {
-                    console.log('INSIDE PROMISE!!!!!!!!!!!!!!!!!!!!!!!');
-                    console.log(event);
-                    console.log(event.data);
-                    console.log(event.data.meta);
-                    console.log(event.data.meta.receivedMessage);
-                    console.log(event.data.meta.receivedMessage.meta);
-                    console.log(event.data.meta.receivedMessage.meta.type);
+                    //console.log('INSIDE PROMISE!!!!!!!!!!!!!!!!!!!!!!!');
+                    //console.log(event);
+                    //console.log(event.data);
+                    //console.log(event.data.meta);
+                    //console.log(event.data.meta.receivedMessage);
+                    //console.log(event.data.meta.receivedMessage.meta);
+                    //console.log(event.data.meta.receivedMessage.meta.type);
                     expect(event.data.meta.receivedMessage.meta.type).to.equal('foo');
                     expect(event.data.meta.receivedMessage.message).to.equal(message);
                 })
                 .then(done);
+        });
+
+        var messageId = 0;
+
+        it('should resolve the correct Promise when answering messages', function(done) {
+            var answers = [];
+
+            function buildMessage(id) {
+                var message = 'expected message' + id;
+
+                return messaging.postMessage({
+                    meta: {
+                        type: 'foo'
+                    },
+                    message: message
+                })
+                    .then(function(event) {
+                        expect(event.data.meta.receivedMessage.meta.type).to.equal('foo');
+                        expect(event.data.meta.receivedMessage.message).to.equal(message);
+                    })
+            }
+
+            for(var i = 0; i < 100; i++) {
+                answers.push(buildMessage(i));
+            }
+
+            Promise.all(answers).then(function() {
+                done();
+            });
         });
     });
 });
