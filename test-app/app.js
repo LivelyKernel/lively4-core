3178cc4a6ebf8cc40fadce4e6e3b51ca9f263163
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-18 16:15:30 +0200

    small demo for github integration

--- test-app/app.js
+++ test-app/app.js
@@ -4,6 +4,8 @@ import Human from './human.js'
 import lively4 from './../src/lively4-client.js'
 import messaging from './../src/client/messaging.js'
 
+import $ from './../src/external/jquery-1.11.3.js'
+
 let foo = new Human("Foo", "Bar");
 
 messaging.postMessage({
@@ -18,16 +20,18 @@ messaging.postMessage({
     console.log(e.data);
 });
 
+var GITHUB_CREDENTIALS = {
+    token: 'd1ca84a85c7629b8148ed9a0d5cb28fe2725f544',
+    auth: 'oauth'
+};
+
 messaging.postMessage({
     meta: {
         type: 'github api'
     },
     message: {
         // TODO: use .config file for such parametrization
-        credentials: {
-            token: 'd1ca84a85c7629b8148ed9a0d5cb28fe2725f544',
-            auth: 'oauth'
-        },
+        credentials: GITHUB_CREDENTIALS,
         topLevelAPI: 'getRepo',
         topLevelArguments: ['Lively4', 'manuallycreated'],
         method: 'read',
@@ -40,5 +44,62 @@ messaging.postMessage({
     console.log(e.data.message);
 });
 
+/*
+navigator.storageQuota.queryInfo("temporary").then(function(info) {
+    console.log('quota in bytes', info.quota);
+    console.log('used data in bytes', info.usage);
+});
+*/
+
+$(document).ready(function() {
+    var topLevelAPI = $('<input type="text" value="getRepo">');
+    var topLevelArguments1 = $('<input type="text" value="Lively4">');
+    var topLevelArguments2 = $('<input type="text" value="manuallycreated">');
+    var method = $('<input type="text" value="read">');
+    var args1 = $('<input type="text" value="master">');
+    var args2 = $('<input type="text" value="README.md">');
+    var args3 = $('<textarea style="padding-left:200;" />').text('FOOOOOOOOOOOO');
+    var button = $('<button/>', {
+        text: 'Do it!',
+        click: function () {
+            "use strict";
+
+            messaging.postMessage({
+                meta: {
+                    type: 'github api'
+                },
+                message: {
+                    // TODO: use .config file for such parametrization
+                    credentials: GITHUB_CREDENTIALS,
+                    topLevelAPI: topLevelAPI.val(),
+                    topLevelArguments: [
+                        topLevelArguments1.val(),
+                        topLevelArguments2.val()
+                    ],
+                    method: method.val(),
+                    args: {
+                        read: [args1.val(),args2.val()],
+                        write: [args1.val(), args2.val(), args3.val(), 'auto-commited']
+                    }[ method.val()]
+                }
+            }).then(e => {
+                "use strict";
+
+                if(method.val()) {
+                    args3.text(e.data.message);
+                }
+            });
+        }
+    });
+    $('body').append(
+        args3,
+        topLevelAPI,
+        topLevelArguments1,
+        topLevelArguments2,
+        method,
+        args1,
+        args2,
+        button);
+});
 
 document.querySelector("h1").innerHTML = `Hello ${foo.toString()}`;
