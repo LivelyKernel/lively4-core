bc9ed81e9a63ea80936573229d75eda7c5312bcb
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-09-28 16:58:35 +0200

    draft ace editor

--- draft/file-editor.js
+++ draft/file-editor.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('https://livelykernel.github.io/lively4-core/serviceworker-loader.js', {
-        scope: "https://livelykernel.github.io/lively4-core/"
+        scope: "https://livelykernel.github.io/lively4-core/draft/"
     }).then(function(registration) {
         // Registration was successful
         alert('ServiceWorker registration successful with scope: ', registration.scope);
