b083299e817fc536543842ff7f59547be10d68f0
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-09-28 16:10:02 +0200

    draft ace editor

--- draft/file-editor.js
+++ draft/file-editor.js
@@ -1,12 +1,17 @@
 
+function currentEditor() {
+	 return ace.edit("editor");
+}
+
 
 function loadfile(){
 	alert("load file")
-	$('#editor').setValue("Hello World")
+
+	currentEditor().setValue("Hello World")
 }
 
 
 function savefile(){
-	alert("Text: " + 	$('#editor').getValue())
+	alert("Text: " + 	currentEditor().getValue())
 }
 
