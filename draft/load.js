bd3398c313fc8dabf2cab46f10b800cb9664f0a6
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-09-29 15:43:35 +0200

    draft ace editor

--- draft/load.js
+++ draft/load.js
@@ -9,7 +9,7 @@ if ('serviceWorker' in navigator) {
         	log('READY');
             System.import("file-editor.js").then(function(module) {
                 window.fileEditor = module
-                log("fileEditor loaded 3")
+                log("fileEditor loaded 4")
             })
             // #TODO continue here... loadFile is not in global scope (yet)
 		});
