f5b64cba190a011942e29971d40ff57a496e28a8
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-09-30 09:05:48 +0200

    draft ace editor

--- src/sw/fetch-tasks/github.js
+++ src/sw/fetch-tasks/github.js
@@ -23,54 +23,54 @@ l4.importScripts('src/external/focalStorage.js');
                 return new Response("No GitHub access token available.", { "status" : 404 , "statusText" : "Not found." })
             }
 
-            console.log('got githubCredentials');
+            return new Promise(function(resolve, reject) {
+                console.log('got githubCredentials');
 
-            var githubCredentials = {
-                token: githubToken,
-                auth: 'oauth' 
-            }
-            var s = request.url.replace(expression, '');
-            var exp = new RegExp("([^/]*)/([^/]*)/([^/]*)/([^/]*)/(.*)")
-            var match = exp.exec(s)
-
-
-
-            // Example: https://github.lively4/repo/livelykernel/lively4-core/gh-pages/README.md
-            var message = {
-                topLevelAPI: match[1],
-                topLevelArguments: [match[2], match[3]],
-                method: 'read',
-                args: [match[4], match[5]]
-            }
-
-            var topLevelAPIMapping = {
-                issues: 'getIssues',
-                repo: 'getRepo',
-                user: 'getUser',
-                gist: 'getGist'
-            };
-
-            var callback = function(err, data) {
-                console.log(err, data);
-
-                if(err) {
-                    l4.broadCastMessage(err);
-                    event.respondWith(new Response("Error" + err))
-                }  else {
-                    event.respondWith(new Response(data))
+                var githubCredentials = {
+                    token: githubToken,
+                    auth: 'oauth' 
                 }
-            };
+                var s = request.url.replace(expression, '');
+                var exp = new RegExp("(?:([^/]*)/){4}(.*)")
+                var match = exp.exec(s)
 
-            var credentials = githubCredentials,
-                github = new Github(credentials),
-                topLevelAPIName = topLevelAPIMapping[message.topLevelAPI],
-                topLevelAPIFunction = github[topLevelAPIName],
-                apiObject = topLevelAPIFunction.apply(github, message.topLevelArguments),
-                methodName = message.method,
-                methodFunction = apiObject[methodName],
-                args = message.args.concat(callback);
+                // Example: https://github.lively4/repo/livelykernel/lively4-core/gh-pages/README.md
+                var message = {
+                    topLevelAPI: match[1],
+                    topLevelArguments: [match[2], match[3]],
+                    method: 'read',
+                    args: [match[4], match[5]]
+                }
 
-            methodFunction.apply(apiObject, args);
+                var topLevelAPIMapping = {
+                    issues: 'getIssues',
+                    repo: 'getRepo',
+                    user: 'getUser',
+                    gist: 'getGist'
+                };
+
+                var callback = function(err, data) {
+                    console.log(err, data);
+
+                    if(err) {
+                        l4.broadCastMessage(err);
+                        resolve(new Response("Error" + err))
+                    }  else {
+                        resolve(new Response(data))
+                    }
+                };
+
+                var credentials = githubCredentials,
+                    github = new Github(credentials),
+                    topLevelAPIName = topLevelAPIMapping[message.topLevelAPI],
+                    topLevelAPIFunction = github[topLevelAPIName],
+                    apiObject = topLevelAPIFunction.apply(github, message.topLevelArguments),
+                    methodName = message.method,
+                    methodFunction = apiObject[methodName],
+                    args = message.args.concat(callback);
+
+                methodFunction.apply(apiObject, args);
+            });
         }).catch(function(err) {
                 console.log("focalStorage Error: " + err)
                 return // Error ??
