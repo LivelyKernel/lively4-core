10c6da49c9684060666584de039af8f8b9282cb7
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-01 17:23:39 +0200

    draft ace editor

--- draft/authgithub.js
+++ draft/authgithub.js
@@ -1,48 +1,53 @@
-function getAppInfo() {
-    return {
-        "clientId": "21b67bb82b7af444a7ef",
-        "redirectUri": "https://livelykernel.github.io/lively4-core/oauth/github.html"
-    };
+'use strict';
+
+export function githubAuth() {
+
+	function getAppInfo() {
+	    return {
+	        "clientId": "21b67bb82b7af444a7ef",
+	        "redirectUri": "https://livelykernel.github.io/lively4-core/oauth/github.html"
+	    };
+	}
+
+	function popup(url) {
+	    var width = 525,
+	        height = 525,
+	        screenX = window.screenX,
+	        screenY = window.screenY,
+	        outerWidth = window.outerWidth,
+	        outerHeight = window.outerHeight;
+
+	    var left = screenX + Math.max(outerWidth - width, 0) / 2;
+	    var top = screenY + Math.max(outerHeight - height, 0) / 2;
+	    
+	    var features = [
+	              "width=" + width,
+	              "height=" + height,
+	              "top=" + top,
+	              "left=" + left,
+	              "status=no",
+	              "resizable=yes",
+	              "toolbar=no",
+	              "menubar=no",
+	              "scrollbars=yes"];
+	    popup = window.open(url, "oauth", features.join(","));
+	    if (!popup) {
+	        alert("failed to pop up auth window");
+	    }
+	    // popup.uuid = lively.net.CloudStorage.addPendingRequest(req);
+	    popup.focus();
+	}
+
+	function challengeForAuth() {
+	    var appInfo = getAppInfo();
+	    var url =
+	        "https://github.com/login/oauth/authorize/" +
+	        "?client_id=" + appInfo.clientId +
+	        "&response_type=token" +
+	        // "&state=" + uuid +
+	        "&redirect_uri=" + encodeURIComponent(appInfo.redirectUri);
+	    popup(url);
+	}
+
+	challengeForAuth()
 }
-
-function popup(url) {
-    var width = 525,
-        height = 525,
-        screenX = window.screenX,
-        screenY = window.screenY,
-        outerWidth = window.outerWidth,
-        outerHeight = window.outerHeight;
-
-    var left = screenX + Math.max(outerWidth - width, 0) / 2;
-    var top = screenY + Math.max(outerHeight - height, 0) / 2;
-    
-    var features = [
-              "width=" + width,
-              "height=" + height,
-              "top=" + top,
-              "left=" + left,
-              "status=no",
-              "resizable=yes",
-              "toolbar=no",
-              "menubar=no",
-              "scrollbars=yes"];
-    popup = window.open(url, "oauth", features.join(","));
-    if (!popup) {
-        alert("failed to pop up auth window");
-    }
-    // popup.uuid = lively.net.CloudStorage.addPendingRequest(req);
-    popup.focus();
-}
-
-window.challengeForAuth =  function challengeForAuth() {
-    var appInfo = getAppInfo();
-    var url =
-        "https://github.com/login/oauth/authorize/" +
-        "?client_id=" + appInfo.clientId +
-        "&response_type=token" +
-        // "&state=" + uuid +
-        "&redirect_uri=" + encodeURIComponent(appInfo.redirectUri);
-    popup(url);
-}
-
-
