4e9be683b5effbdb9660dbe6ec1fd0e1daffcda4
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-07 17:59:17 +0200

    draft ace editor

--- oauth/github.js
+++ oauth/github.js
@@ -1,39 +1,21 @@
 function onAuthCallback() {
 
-	var authInfo = getAuthInfoFromUrl()
-
-	if (authInfo && authInfo["access_token"]) {
-
-	    var token = authInfo["access_token"];
-
-	    localStorage.GithubToken = token
-	    focalStorage.setItem("githubToken", token).then(function() {
-	    	window.opener.githubAuth.onAuthenticated(window.uuid, token);
-	    	// window.close()
-	    })
-	    return 
-	}
-
+    var code = codeInfo["code"]
+ 
     var codeInfo = getCodeFromUrl();
     if (!codeInfo) {
-    	alert("failed to receive auth code (the token to get the token)");
-    	return 
-  	}
-
+        alert("failed to receive auth code (the token to get the token)");
+        return
+    }
 
     var code = codeInfo["code"]
+    jQuery.get("https://lively-kernel.org/lively4-auth/github_accesstoken?code=" + code,
+               function(data, status, xhr) {
+                   var authInfo = parseAuthInfoFromUrl(data)
+                   window.opener.githubAuth.onAuthenticated(window.uuid, authInfo);
+                   window.close()
+               })
 
-    jQuery.post("https://github.com/login/oauth/access_token",{
-    	client_id: "21b67bb82b7af444a7ef",
-    	client_secret: "e9ae61b190c5f82a9" + "e3d6d0d2f97e8ad4ba29d18",
-    	code: code,
-    	redirect_uri: "https://livelykernel.github.io/lively4-core/oauth/github.html",
-    	state: ""
-    }, 
-    function(data, status, xhr) {
-    	// we should be redirected.... 
-    },
-    "application/json")
 
 }
 
