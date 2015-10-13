9de270e51659f80639f683b3eedbb47a8557d6a1
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-13 15:27:27 +0200

    draft ace editor

--- src/sw/logging.js
+++ src/sw/logging.js
@@ -1,8 +1,13 @@
 
 
-var nativeLog = console.log
+// guard againsst wrapping twice and ending in endless recursion
+if (!console.log.isWrapped) {
+	var nativeLog = console.log
 
-console.log = function(s) {
-    nativeLog.apply(console, arguments)    
-    l4.broadCastMessage({name: 'log', data: '' + s})
+	console.log = function(s) {
+	    nativeLog.apply(console, arguments)    
+	    l4.broadCastMessage({name: 'log', data: '' + s})
+	}
+
+	console.log.isWrapped = true
 }
