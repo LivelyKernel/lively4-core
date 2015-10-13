f80ca29aca8752cba3935f1b77322b15216b01ae
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-13 13:12:02 +0200

    draft ace editor

--- src/sw/logging.js
+++ src/sw/logging.js
@@ -4,5 +4,5 @@ var nativeLog = console.log
 
 console.log = function(s) {
     nativeLog.apply(console, arguments)    
-    l4.broadCastMessage({name: 'log', data: s})
+    l4.broadCastMessage({name: 'log', data: '' + s})
 }
