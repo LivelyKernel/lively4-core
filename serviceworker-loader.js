50d0f0d918ef01d3f5b6499205eda302b7b8b8f3
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-13 15:18:37 +0200

    draft ace editor

--- serviceworker-loader.js
+++ serviceworker-loader.js
@@ -1,3 +1,4 @@
 'use strict';
 
+importScripts('https://livelykernel.github.io/lively4-core/src/sw/dev.js?' + Date.now());
 importScripts('https://livelykernel.github.io/lively4-core/serviceworker.js?' + Date.now());
