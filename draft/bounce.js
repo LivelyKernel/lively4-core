19754524cd15ae717a5ce8b4ee76e324a20a208a
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-10-26 16:48:29 +0100

    auto commit

--- draft/bounce.js
+++ draft/bounce.js
@@ -1,2 +1,11 @@
 
-$('h1').css({ position: 'absolute', left: 1000 })
+$('<button onclick="eval(ace.edit(\'editor\').getValue())">EVAL</button>').appendTo('body')
+
+var title = $('h1').css({ position: 'absolute', left: 1000 })
+
+
+function bounce() {
+    title.css({top: 100})
+}
+
+bounce();
