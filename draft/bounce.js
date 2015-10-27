55dda5b9df044b13abba6ce7e9500dd76fe77514
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-10-27 13:16:53 +0100

    auto commit

--- draft/bounce.js
+++ draft/bounce.js
@@ -1,1 +1,13 @@
 
+var title = $('h1');
+
+
+function bounce() {
+    var t = window.performance.now()
+    var y = 100 + 50 * -Math.abs(Math.sin(t/1000))
+    title.css({top: y})
+    requestAnimationFrame(bounce)
+}
+
+
+bounce();
