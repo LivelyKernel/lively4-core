f80b4cfe825a6e23470567a3317120fdcc44c9fb
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-10-27 17:08:12 +0100

    SYNC D  es6-module-loader/es6-module-loader-dev.js;D  es6-module-loader/es6-module-loader-dev.js.map;D  es6-module-loader/es6-module-loader-dev.src.js;D  es6-module-loader/es6-module-loader.js;D  es6-module-loader/es6-module-loader.js.map;D  es6-module-loader/es6-module-loader.src.js; M karma.conf.js; M serviceworker.js; M src/sw/fetch-tasks/babel.js;

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// This is a test 2
-
 var l4 = {
     importScripts: (function () {
         var files = new Set();
@@ -16,13 +14,8 @@ var l4 = {
     })()
 };
 
-//l4.importScripts('es6-module-loader/es6-module-loader.src.js');
 
 l4.importScripts('src/sw/core.js');
-
-//l4.importScripts('bundle.js');
-
-// l4.importScripts('serviceworker-cache-polyfill.js');
 
 // --------------------------------------------------------------------
 // Loaders
