7d0b930c4924c4cd812e78eb55149c5e6ddba9cc
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2015-11-02 14:52:52 +0100

    added correct 404 error message to fetch task

--- test/github-fetch-test.js
+++ test/github-fetch-test.js
@@ -9,10 +9,33 @@ define(function(require) {
     
     describe('Github Fetch API', function() {
         it('throw an error if not github token is available', function(done) {
-
-            focalStorage.setItem("githubToken", null).then(function(){
-                done()
-            })
+            focalStorage.getItem("githubToken")
+                .then(function(item){
+                    expect(item).to.exist
+                })
+                .then(done)
+                .catch(done);
         });
+
+        it('fetches a file with a xmlhttp get request from github', function(done) {
+            $.get("https://github.lively4/repo/livelykernel/lively4-core/gh-pages/README.md", undefined,
+                function(result) {
+                    console.log("CONTENT: " + result)
+                    expect(result).to.exist                
+                    done()
+                }).fail(function(err, status) {
+                    done(err)
+                })
+        });
+        it('get an 404 when fetching a non existing file with an xmlhttp get request from github', function(done) {
+            $.get("https://github.lively4/repo/livelykernel/lively4-core/gh-pages/I_do_not_exist.txt", undefined,
+                function(result) {
+                    done(new Error("We expected an error but got this as result: " + result))
+                }).fail(function(err, status) {
+                    expect(err.status).to.be.equal(404)
+                    done()
+                })
+        });
+
     });
 });
