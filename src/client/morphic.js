00fe5e25152e7fac3cb6368bf42b04ee354bceec
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2015-11-02 12:01:54 +0100

    create part by moving children into shadow dom

--- src/client/morphic.js
+++ src/client/morphic.js
@@ -7,3 +7,35 @@ function initDragBehaviour() {
 		
 	});
 }
+
+export function makePart(partRoot) {
+	var shadow = partRoot.createShadowRoot();
+
+	// We need to bring the styles into the shadow root,
+	// because otherwise they will not be applied to the 
+	// shadow dom elements.
+	
+	// collect styles
+	var combinedStyle = "";
+	var styles = document.styleSheets;
+	for (var i = 0; i < styles.length; i++) {
+		var styleSheet = styles[i];
+		for (var j = 0; j < styleSheet.cssRules.length; j++) {
+			var rule = styleSheet.cssRules[j];
+			combinedStyle += rule.cssText + "\n";
+		}
+	}
+
+	// apply style
+	var styleElement = document.createElement("style");
+	styleElement.innerHTML = combinedStyle;
+	shadow.appendChild(styleElement);
+
+	// make a shallow copy of the children object, 
+	// since partRoot.children changes in the following loop
+	var children = $.extend({}, partRoot.children);
+	// append all children to the shadow dom
+	for (var i = 0; i < children.length; i++) {
+		shadow.appendChild(children[i]);
+	}
+}
