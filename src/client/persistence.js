3868179df2f9a9f8efcbae9de24a3fb8b103c143
Daniel Stolpe <daniel.stolpe@gmx.de>
2015-11-20 13:59:37 +0100

    persistence: added timer-based push of detected changes

--- src/client/persistence.js
+++ src/client/persistence.js
@@ -2,6 +2,8 @@
 
 export function babeldummy() {};
 
+var persistenceTimerInterval;
+
 function isLively4Script(node) {
     return node.tagName
         && node.tagName.toLocaleLowerCase() == 'script'
@@ -18,7 +20,14 @@ function initialize(){
                 var shouldSave = nodes.some(node => {
                     return isLively4Script(node);
                 })
-                if (shouldSave) saveDOM();
+                if (shouldSave) {
+                    sessionStorage["lively.scriptMutationsDetected"] = true;
+                    if (isSaveDOMAllowed()) {
+                        saveDOM()
+                    } else {
+                        console.log("Persist to github not checked. Changes will not be pushed.");
+                    }
+                }
             }
         })
     }).observe(document, {childList: true, subtree: true});
@@ -49,15 +58,33 @@ function getURL(){
     return new URL(baseurl + filename)
 }
 
-function saveDOM() {
+export function startPersistenceTimerInterval() {
+    persistenceTimerInterval = setInterval(checkForMutationsToSave, 5000);
+}
+
+export function stopPersistenceTimerInterval() {
+    clearInterval(persistenceTimerInterval);
+}
+
+function checkForMutationsToSave() {
+    if (isSaveDOMAllowed()) {
+        console.log("[persistence] timer-based mutations detected, saving DOM...")
+        saveDOM();
+    }
+}
+
+function isSaveDOMAllowed() {
     var check = $("#persistToGithub");
-    if (!check) return;
+    if (!check) return false;
     
     if (check.size() > 0 && !check[0].checked) {
-        console.log("Persist to github not checked. Changes will not be pushed.");
-        return;
+        return false;
     }
 
+    return true;
+}
+
+function saveDOM() {
     var world = $("html").clone();
     world.find("#editor").empty();
     world.find("#console").empty();
@@ -66,6 +93,8 @@ function saveDOM() {
     world.find("style").filter((i,e) => /\s*\.error_widget_wrapper+/.test(e.textContent)).remove();
     var s = new XMLSerializer();
     var content = "<!DOCTYPE html>" + s.serializeToString(world[0]);
+
+    sessionStorage["lively.scriptMutationsDetected"] = false;
 
     writeFile(content);
 }
