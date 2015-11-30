1b4467eb462256d1aae2273c8f44de25d47e87a9
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-11-30 14:55:39 +0100

    converted tests from karma-require to es2015

--- test/sw-test.js
+++ test/sw-test.js
@@ -1,19 +1,18 @@
-define(function(require) {
-    "use strict";
+"use strict";
 
-    require('https://code.jquery.com/jquery-2.1.4.js');
-    var expect = chai.expect;
+import * as $ from 'https://code.jquery.com/jquery-2.1.4.js';
 
-    describe('Eval', function() {
-        it('should eval 3 + 4 to 7', function(done) {
-            $.ajax({
-                url: "https://eval/3+4",
-                type: "get",
-                success: function(actualResult) {
-                    expect(actualResult).to.equal('7');
-                    done();
-                }
-            });
-        });
+var expect = chai.expect;
+
+describe('Eval', function() {
+  it('should eval 3 + 4 to 7', function(done) {
+    $.ajax({
+      url: "https://eval/3+4",
+      type: "get",
+      success: function(actualResult) {
+        expect(actualResult).to.equal('7');
+        done();
+      }
     });
+  });
 });
