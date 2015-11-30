8426bef9e732bcfa503fbe3808bee8375c1f475d
Jan Graichen <jg@altimos.de>
2015-11-30 22:59:47 +0100

    Simple read files and directories from GH mount

--- src/fs/github.jsx
+++ src/fs/github.jsx
@@ -5,7 +5,33 @@
 import { Base } from './base.jsx'
 
 export class Filesystem extends Base {
+    constructor(path, options) {
+        super(path, options)
+
+        if(options.repo) {
+            this.repo = options.repo
+        } else {
+            throw new Error("[github] repo option required")
+        }
+    }
+
     read(path) {
-        return Promise.resolve(path)
+        return self.fetch('https://api.github.com/repos/' + this.repo + '/contents/' + path)
+            .then(Response.ok)
+            .then((result) => result.json())
+            .then((json) => {
+                if(json instanceof Array) {
+                    return JSON.stringify({
+                        contents: json.map((item) => {
+                            return {name: item['name']}
+                        })
+                    })
+                } else {
+                    return atob(json['content'])
+                }
+            }).catch((err) => {
+                console.error(err)
+                return new Response(err, {status: 500})
+            })
     }
 }
