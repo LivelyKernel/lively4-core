d6c84474251073e08e1545d267dca2ae4a10cfd6
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-11-30 16:05:06 +0100

    eval code on sw side, then send results back to and match on client

--- test/sw/simple-test.js
+++ test/sw/simple-test.js
@@ -1,8 +1,28 @@
 var expect = chai.expect;
 
+import * as messaging from './../../src/client/messaging.js';
+
 describe('SIMPLE TEST', function() {
   it('SHOULD WORK', function(done) {
     expect(8).to.equal(2**3);
     done();
+  });
+
+  it('should answer a single message', function(done) {
+    var message = 'expected message';
+
+    messaging.postMessage({
+      meta: {
+        type: 'bar'
+      },
+      message: '2+3'
+    })
+      .then(function(event) {
+        expect(event.data.meta.receivedMessage.meta.type).to.equal('bar');
+        expect(event.data.message.error).to.equal(undefined);
+        expect(event.data.message.result).to.equal('5');
+      })
+      .then(done)
+      .catch(done);
   });
 });
