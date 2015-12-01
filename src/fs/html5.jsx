9208b60c4c214d7a413f01db3308db41a5f35d17
Jan Graichen <jg@altimos.de>
2015-12-01 11:09:07 +0100

    Starts local file system for lively4 ServiceWorker
     * uses HTML5 FileSystem API
     * accesses the API via MessageChannel message passing with the main tab
     * DO NOT USE (yet)

--- src/fs/html5.jsx
+++ src/fs/html5.jsx
@@ -5,16 +5,75 @@
 import { Base } from './base.jsx'
 
 export class Filesystem extends Base {
+    constructor(path, options) {
+        super(path, options)
+
+        this.quota = options.quota || 1024*1024*10;
+
+        this.fs = new Promise((resolve, reject) => {
+            self.clients.matchAll().then((clients) => {
+                let channel = new MessageChannel()
+                let client  = clients[0]
+
+                channel.port1.onmessage = function(event) {
+                    console.log('Got quota request response', event.data)
+
+                    let grantedQuota = event.data.grantedQuota
+
+                    self.webkitRequestFileSystem(PERSISTENT, grantedQuota, resolve, reject)
+                    // debugger
+                }
+
+                client.postMessage({name: 'swx:requestQuota'}, [channel.port2])
+                // debugger
+            })
+
+            // debugger
+            // self.webkitRequestFileSystem(TEMPORARY, this.quota, resolve, reject)
+        })
+    }
 
     stat(path) {
 
     }
 
     read(file) {
+        return this.fs.then((fs) => {
+            return new Promise((resolve, reject) => {
+                fs.root.getFile(file, function(file) {
+                    let reader = new FileReader()
 
+                    reader.onloadend = function(e) {
+                        console.log('Read complete', e)
+                        resolve(this.result)
+                    }
+
+                    reader.readAsText(file)
+                }, reject)
+            })
+        })
     }
 
     write(file, content) {
+        return this.fs.then((fs) => {
+            return new Promise((resolve, reject) => {
+                fs.root.getFile(file, {create: true, exclusive: false}, function(file) {
+                    file.createWriter(function (writer) {
 
+                        writer.onwriteend = function(e) {
+                            console.log("Write complete", e)
+                            resolve()
+                        }
+
+                        writer.onerror = reject
+
+                        let bb = new BlobBuilder()
+                        bb.append(content)
+
+                        writer.write(bb.getBlob('text/plain'))
+                    }, reject)
+                }, reject)
+            })
+        })
     }
 }
