3bb742e814d68470e3175f9c587a52ca6a548d9c
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-12-02 16:29:48 +0100

    allow async tests

--- src/sw/messaging-tasks/run-sw-tests.js
+++ src/sw/messaging-tasks/run-sw-tests.js
@@ -1,39 +1,5 @@
-l4.isIncluded = 42;
 l4.importScripts('./../../../src/external/bluebird.js');
 l4.importScripts('./../../../node_modules/chai/chai.js');
-
-function describe(name, callback) {
-  l4.describeName = name;
-  l4.testResults[l4.describeName] = {};
-
-  callback();
-}
-
-function it(name, callback) {
-  var fullName = l4.describeName + '---' + name;
-
-  // TODO: async tests
-  if(callback.length > 0) {
-    throw new Error('Async testcases not yet supported');
-  }
-
-  Promise.resolve().spread(()=>{'works'});
-
-  try{
-    callback();
-
-    l4.testResults.push({
-      fullName: fullName,
-      result: 'pass'
-    });
-  } catch(e) {
-    l4.testResults.push({
-      fullName: fullName,
-      result: 'fail',
-      error: packError(e)
-    });
-  }
-}
 
 function packError(error) {
   return {
@@ -54,6 +20,69 @@ function setupTestEnvironment(testFiles) {
   return Promise.all(testFiles.map(testFile => System.import(testFile)));
 }
 
+l4.runTests = function() {
+  l4.describes.forEach(describe.run);
+  return Promise.all(l4.its.map(it.run));
+};
+
+// register a test suite
+function describe(name, callback) {
+  l4.describes.push({
+    name: name,
+    callback: callback
+  });
+}
+
+describe.run = function(description) {
+  l4.describeName = description.name;
+  l4.testResults[l4.describeName] = {};
+
+  description.callback();
+};
+
+// register a test case
+function it(name, callback) {
+  var fullName = l4.describeName + '---' + name;
+  l4.its.push({
+    name: fullName,
+    callback: callback
+  });
+}
+
+it.run = function(description) {
+  var fullName = description.name,
+    callback = description.callback;
+
+  return new Promise(resolve => {
+    if(callback.length > 0) {
+      callback(resolve)
+    } else {
+      callback();
+      resolve();
+    }
+  })
+    .timeout(2000, 'test timed out')
+    .then(() => {
+      l4.testResults.push({
+        fullName: fullName,
+        result: 'pass'
+      });
+    })
+    .catch(Promise.TimeoutError, error => {
+      l4.testResults.push({
+        fullName: fullName,
+        result: 'pending'
+      });
+    })
+    .catch(error => {
+      l4.testResults.push({
+        fullName: fullName,
+        result: 'fail',
+        error: packError(e)
+      });
+    });
+};
+
 l4.messageTask('run test', function match(event) {
   return hasPort(event) &&
     event.data &&
@@ -73,10 +102,13 @@ l4.messageTask('run test', function match(event) {
     });
   }
 
+  l4.describes = [];
+  l4.its = [];
   l4.testResults = [];
   var testFiles = eval(event.data.message);
 
   setupTestEnvironment(testFiles)
+    .then(l4.runTests)
     .then(() => {
       sendTestResults(undefined, l4.testResults);
     })
