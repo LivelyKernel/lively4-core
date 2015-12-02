c08b6e3bc742b31d4c0be27983e7641b0c0bf968
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-12-02 12:51:18 +0100

    adjusted sw test

--- test/sw/simple-swtest.js
+++ test/sw/simple-swtest.js
@@ -1,11 +1,14 @@
 var expect = chai.expect;
 
-describe('SIMPLE TEST', function() {
-  it('SHOULD WORK', function() {
+describe('Simple SW Test', function() {
+  it('should transpile', function() {
     expect(8).to.equal(2 ** 3);
   });
 
-  it('SHOULD NOT WORK', function() {
-    expect(42).to.equal(9 * 6)
+  it('should fail to calculate the answer', function() {
+    function failing() {
+      expect(42).to.equal(9 * 6);
+    }
+    expect(failing).to.throw(Error, /expected 42 to equal 54/);
   })
 });
