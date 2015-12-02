488b7399c8d453802eb5c2f87e73c3a251a63d56
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-12-02 16:57:14 +0100

    some comments

--- src/sw/messaging-tasks/run-sw-tests.js
+++ src/sw/messaging-tasks/run-sw-tests.js
@@ -9,6 +9,7 @@ function packError(error) {
   };
 }
 
+// setup es6 support, then run all test files
 function setupTestEnvironment(testFiles) {
   importScripts('./src/external/es6-module-loader-dev.js');
   System.baseURL = '/base/';
@@ -21,18 +22,19 @@ function setupTestEnvironment(testFiles) {
 }
 
 l4.runTests = function() {
-  l4.describes.forEach(describe.run);
-  return Promise.all(l4.its.map(it.run));
+  l4.testing.describes.forEach(describe.run);
+  return Promise.all(l4.testing.its.map(it.run));
 };
 
 // register a test suite
 function describe(name, callback) {
-  l4.describes.push({
+  l4.testing.describes.push({
     name: name,
     callback: callback
   });
 }
 
+// actually run a testSuite
 describe.run = function(description) {
   l4.describeName = description.name;
   description.callback();
@@ -41,12 +43,13 @@ describe.run = function(description) {
 // register a test case
 function it(name, callback) {
   var fullName = l4.describeName + '---' + name;
-  l4.its.push({
+  l4.testing.its.push({
     name: fullName,
     callback: callback
   });
 }
 
+// run a test case and memorize its result
 it.run = function(description) {
   var fullName = description.name,
     callback = description.callback;
@@ -61,19 +64,19 @@ it.run = function(description) {
   })
     .timeout(2000, 'test timed out')
     .then(() => {
-      l4.testResults.push({
+      l4.testing.testResults.push({
         fullName: fullName,
         result: 'pass'
       });
     })
     .catch(Promise.TimeoutError, error => {
-      l4.testResults.push({
+      l4.testing.testResults.push({
         fullName: fullName,
         result: 'pending'
       });
     })
     .catch(error => {
-      l4.testResults.push({
+      l4.testing.testResults.push({
         fullName: fullName,
         result: 'fail',
         error: packError(error)
@@ -100,16 +103,18 @@ l4.messageTask('run test', function match(event) {
     });
   }
 
-  l4.describes = [];
-  l4.its = [];
-  l4.testResults = [];
+  l4.testing = {
+    describes: [],
+    its: [],
+    testResults: []
+  };
 
   var testFiles = event.data.message;
 
   setupTestEnvironment(testFiles)
     .then(l4.runTests)
     .then(() => {
-      sendTestResults(undefined, l4.testResults);
+      sendTestResults(undefined, l4.testing.testResults);
     })
     .catch(sendTestResults);
 
