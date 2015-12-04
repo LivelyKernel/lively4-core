271abf9ebb962990f5ab150184f356aca96e9e9c
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2015-12-04 12:37:35 +0100

    removed unused morphic.js

--- src/client/morphic/component-creator.js
+++ src/client/morphic/component-creator.js
@@ -39,6 +39,65 @@ export function saveTemplate(template) {
   return completeHTML;
 }
 
+
+export function packShadowDOM(subtreeRoot) {
+  var shadow;
+  // if there is a shadow root already,
+  // we clean it up, since we cannot create a new one
+  if (subtreeRoot.shadowRoot) {
+    shadow = subtreeRoot.shadowRoot;
+    $(shadow.children).each(function(idx) {
+      shadow.removeChild(this);
+    });
+  } else {
+    shadow = subtreeRoot.createShadowRoot();
+  }
+
+  // We need to bring the styles into the shadow root,
+  // because otherwise they will not be applied to the 
+  // shadow dom elements.
+  
+  // collect styles
+  // Maybe we should not filter rules due to dynamically
+  // assigned classes?
+  var combinedStyle = collectAppliedCssRules(subtreeRoot);
+
+  // apply style
+  var styleElement = document.createElement("style");
+  styleElement.innerHTML = combinedStyle;
+  shadow.appendChild(styleElement);
+
+  // make a shallow copy of the children object, 
+  // since subtreeRoot.children changes in the following loop
+  var children = $.extend({}, subtreeRoot.children);
+  // append all children to the shadow dom
+  for (var i = 0; i < children.length; i++) {
+    shadow.appendChild(children[i]);
+  }
+}
+
+export function unpackShadowDOM(subtreeRoot) {
+  var shadow = subtreeRoot.shadowRoot;
+  if (!shadow) {
+    return;
+  }
+
+  // move all elements but style out of the shadow dom
+  $(shadow.children).filter(":not(style)").each(function(idx) {
+    subtreeRoot.appendChild(this);
+  });
+
+  // remove all remaining child nodes
+  $(shadow.children).each(function(idx) {
+    shadow.removeChild(this);
+  });
+
+  // We cannot remove the shadow root, so to make the content visible,
+  // add a content node to the shadow dom. This should be equivalent to having 
+  // no shadow dom at all.
+  shadow.appendChild(document.createElement("content"));
+}
+
 function collectAppliedCssRules(rootElement) {
   var combinedStyle = "";
   var styles = document.styleSheets;
@@ -75,3 +134,4 @@ function selectorMatchesTree(selector, rootElement) {
   // if we reach this, none of the tree nodes matches the selector
   return false;
 }
+
