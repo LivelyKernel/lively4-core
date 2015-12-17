9c4bc2e43521b20f870d092090702a05dec48993
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2015-12-17 23:08:47 +0100

    some refactoring of component bin

--- templates/classes/ComponentBin.js
+++ templates/classes/ComponentBin.js
@@ -8,48 +8,57 @@ export default class ComponentBin extends Morph {
   attachedCallback() {
     console.log("ComponentBin attached!!!");
     
-    this.setupComponentList();
-  }
-
-  setupComponentList() {
-    statFile("https://lively4/templates").then(response => {
-      response = JSON.parse(response);
-      // just consider html-files being templates
-      var infoFiles = response.contents.filter(file => {
-        return file.type === "file" && file.name.slice(-5) === ".json";
-      }).map(file => {
-        return loadFile("https://lively4/templates/" + file.name)
-      });
-
-      Promise.all(infoFiles).then(files => {
-        files.map(JSON.parse).forEach(compInfo => {
-          this.appendButton(compInfo.name, () => {
-            var component = document.createElement(compInfo["html-tag"]);
-            this.parentElement.insertBefore(component, this.nextSibling);
-            componentLoader.loadUnresolved(document.body);
-          })
-        })
-      }).catch(err => {
-        console.log(err);
-      });
-
-      // infoFiles.forEach(file => {
-      //   this.appendButton(file.name, () => {
-      //     // the tag name is the file name without '.html'
-      //     var tag = file.name.substring(0, file.name.length-5);
-      //     var component = document.createElement(tag);
-      //     this.parentElement.insertBefore(component, this.nextSibling);
-      //     componentLoader.loadUnresolved(document.body);
-      //   });
-      // });
+    this.loadComponentList().then(componentList => {
+      this.componentList = componentList;
+      this.createTiles(componentList);
     });
   }
 
-  appendButton(name, callback) {
+  loadComponentList() {
+    return new Promise((resolve, reject) => {
+      statFile("https://lively4/templates").then(response => {
+        response = JSON.parse(response);
+        // just consider html-files being templates
+        var infoFilesPromises = response.contents.filter(file => {
+          return file.type === "file" && file.name.slice(-5) === ".json";
+        }).map(file => {
+          return loadFile("https://lively4/templates/" + file.name)
+        });
+
+        Promise.all(infoFilesPromises).then(files => {
+          // save the parsed list
+          var componentList = files.map(JSON.parse);
+          resolve(componentList);
+        });
+      }).catch(err => {
+        console.log(err);
+      });      
+    });
+  }
+
+  createTiles(componentList) {
+    componentList.forEach(compInfo => {
+      this.appendTile(compInfo, () => {
+        var component = document.createElement(compInfo["html-tag"]);
+        this.parentElement.insertBefore(component, this.nextSibling);
+        componentLoader.loadUnresolved(document.body);
+      });
+    });
+    
+  }
+
+
+    //   }).catch(err => {
+    //     console.log(err);
+    //   });
+    // });
+
+  appendTile(config, callback) {
     var list = this.getSubmorph(".button-list");
     var button = document.createElement("button");
-    button.innerHTML = name;
-    button.className = "list-button";
+    button.className = "tile";
+    button.innerHTML = config["name"];
+    button.style.backgroundImage = "url(/lively4-core/templates/" + (config["thumbnail"] || "thumbnails/default-placeholder.png") + ")";
     button.addEventListener("click", callback);
 
     list.appendChild(button);
