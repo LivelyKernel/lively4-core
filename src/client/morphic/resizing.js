d618635d5a6550ef036ccb617eabfe61324ace84
athomschke <astrid.thomschke@gmail.com>
2016-01-06 17:03:55 +0100

    implemented resizing by pressing arrow keys

--- src/client/morphic/resizing.js
+++ src/client/morphic/resizing.js
@@ -1,25 +1,68 @@
 export function activate() {
   console.log("using Resizing");
-  $("body").on("mousedown", start);
-  $("body").on("mousemove", move);
-  $("body").on("mouseup", stop);
+  $("body").on("keydown", changeSize);
+  // showHandles(window.that);
+  // Object.observe(window.observables, thatChangedCallback);
 }
 
 export function deactivate() {
   console.log("deactivate Resizing");
-  $("body").off("mousedown", start);
-  $("body").off("mousemove", move);
-  $("body").off("mouseup", stop);
+  $("body").off("keydown", changeSize);
+  // hideHandles(window.that);
+  // Object.unobserve(window.observables, thatChangedCallback);
 }
 
-function start(e) {
+// function thatChangedCallback(changes) {
+//   hideHandles(changes[0].oldValue);
+//   showHandles(changes[0].object.that);
+// }
 
+function changeSize(e) {
+  if (!window.that) return;
+  dispatchArrow(e.keyCode);
 }
 
-function move(e) {
-
+function dispatchArrow(keyCode) {
+  switch(keyCode) {
+    case 37: return left();
+    case 38: return up();
+    case 39: return right();
+    case 40: return down();
+ }
 }
 
-function stop(e) {
 
+function setStyle(node, property, value) {
+  node.style.setProperty(property, value)
 }
+
+function sizeof(node) {
+  return {
+    x: parseInt(node.style.getPropertyValue('width')) || 10,
+    y: parseInt(node.style.getPropertyValue('height')) || 10
+  }
+}
+
+function right() {
+  setStyle(that, 'width', '' + (sizeof(that).x + 10) + 'px');
+}
+
+function left() {
+  setStyle(that, 'width', '' + Math.max(sizeof(that).x - 10, 10) + 'px');
+}
+
+function down() {
+  setStyle(that, 'height', '' + (sizeof(that).y + 10) + 'px');
+}
+
+function up() {
+  setStyle(that, 'height', '' + Math.max(sizeof(that).y - 10, 10) + 'px');
+}
+
+// function hideHandles(node) {
+
+// }
+
+// function showHandles(node) {
+
+// }
