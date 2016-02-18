e884ded6848ca0ded24574f77270471e52e0ab83
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2016-02-18 12:30:59 +0100

    morphic clean up

--- src/client/morphic/resizing.js
+++ src/client/morphic/resizing.js
@@ -1,68 +1,43 @@
-export function activate() {
-  console.log("using Resizing");
-  $("body").on("keydown", changeSize);
-  // showHandles(window.that);
-  // Object.observe(window.observables, thatChangedCallback);
+export var resizing;
+
+export function start(e) {
+  e.preventDefault();
+
+  let $el = $(window.that)
+  let offsetWindow = $el.offset();
+
+  removeRestrictions($el)
+
+  resizing = {
+    left: offsetWindow.left,
+    top: offsetWindow.top,
+    offsetX: offsetWindow.left + $el.width() - e.pageX,
+    offsetY: offsetWindow.top + $el.height() - e.pageY
+  };
 }
 
-export function deactivate() {
-  console.log("deactivate Resizing");
-  $("body").off("keydown", changeSize);
-  // hideHandles(window.that);
-  // Object.unobserve(window.observables, thatChangedCallback);
+export function stop(e) {
+  e.preventDefault();
+
+  resizing = false;
 }
 
-// function thatChangedCallback(changes) {
-//   hideHandles(changes[0].oldValue);
-//   showHandles(changes[0].object.that);
-// }
+export function move(e) {
+  e.preventDefault();
 
-function changeSize(e) {
-  if (!window.that) return;
-  dispatchArrow(e.keyCode);
-}
-
-function dispatchArrow(keyCode) {
-  switch(keyCode) {
-    case 37: return left();
-    case 38: return up();
-    case 39: return right();
-    case 40: return down();
- }
-}
-
-
-function setStyle(node, property, value) {
-  node.style.setProperty(property, value)
-}
-
-function sizeof(node) {
-  return {
-    x: parseInt(node.style.getPropertyValue('width')) || 10,
-    y: parseInt(node.style.getPropertyValue('height')) || 10
+  if (resizing) {
+    $(window.that).css({
+      width: e.pageX - resizing.left + resizing.offsetX,
+      height: e.pageY - resizing.top + resizing.offsetY
+    });
   }
 }
 
-function right() {
-  setStyle(that, 'width', '' + (sizeof(that).x + 10) + 'px');
+function removeRestrictions($el) {
+  $el.css({
+    "min-width": "none",
+    "min-height": "none",
+    "max-width": "none",
+    "max-height": "none"
+  })
 }
-
-function left() {
-  setStyle(that, 'width', '' + Math.max(sizeof(that).x - 10, 10) + 'px');
-}
-
-function down() {
-  setStyle(that, 'height', '' + (sizeof(that).y + 10) + 'px');
-}
-
-function up() {
-  setStyle(that, 'height', '' + Math.max(sizeof(that).y - 10, 10) + 'px');
-}
-
-// function hideHandles(node) {
-
-// }
-
-// function showHandles(node) {
-
-// }
