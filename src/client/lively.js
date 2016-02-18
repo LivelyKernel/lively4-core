c82409597a3e72abca0dbb09106cf2f3667a4428
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2016-02-18 17:00:39 +0100

    - context menu closes on click in world - connected container and browser

--- src/client/lively.js
+++ src/client/lively.js
@@ -12,12 +12,19 @@ import * as jquery from '../external/jquery.js';
 
 export { scripts }
 export { messaging }
-export { preferences } 
-export { persistence } 
-export { components } 
+export { preferences }
+export { persistence }
+export { components }
 
 var worldmenu
 
+export function boundEval(str, ctx) {
+  var interactiveEval = function(text) { return eval(text) };
+  var transpiledSource = babel.transform(str).code
+  // #TODO alt: babel.run
+  // #TODO context does not seem to work!
+  return interactiveEval.call(ctx, transpiledSource);
+}
 
 export function pt(x,y) {
     return {x: x, y: y}
@@ -26,19 +33,46 @@ export function pt(x,y) {
 export function setPosition(obj, point) {
     obj.style.position = "absolute"
     obj.style.left = ""+  point.x + "px"
-    obj.style.top = "" + point.y + "px"    
+    obj.style.top = "" + point.y + "px"
+}
+
+export function openFile(url) {
+  if (url.hostname == "lively4"){
+    var container  = $('lively-container')[0];
+    if (container) {
+      container.followPath(url.pathname)
+    } else {
+      console.log("fall back on editor: "+ url)
+      editFile(url)
+    }
+  } else {
+    editFile(url)
+  }
+}
+
+export function editFile(url) {
+  var editor  = document.createElement("lively-editor")
+  lively.components.openInWindow(editor).then((container) => {
+      lively.setPosition(container, lively.pt(100, 100))
+      editor.setURL(url)
+      editor.loadFile()
+  })
+}
+
+export function hideContextMenu() {
+  if (worldmenu) {
+    $(worldmenu).remove()
+    worldmenu = null
+  }
 }
 
 export function openContextMenu(container, evt) {
-    console.log("open context menu")
-    if (worldmenu) {
-        $(worldmenu).remove()
-        worldmenu = null 
-    }
+  console.log("open context menu")
+  this.hideContextMenu()
 	System.import(preferences.getBaseURL() + "/src/client/contextmenu.js").then(function(contextmenu) {
 		 contextmenu.openIn(container, evt, (menu) => {
-            worldmenu = menu
-        })	
+          worldmenu = menu
+        })
     })
 }
 
