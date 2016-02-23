ec8433acca0b26828450f2979c33c0a2aab1a511
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-02-23 16:58:51 +0100

    added description to the View class and its .layer method

--- src/view.js
+++ src/view.js
@@ -4,6 +4,11 @@ define(function module(require) { "use strict";
   var removeIfExisting = require('./utils').removeIfExisting;
   var identity = require('./utils').identity;
 
+  /**
+   *
+   * @class View
+   * @classdesc This is the class representing a view.
+   */
   var View = Object.subclass('View', {
     initialize: function() {
       this.items = [];
@@ -30,8 +35,11 @@ define(function module(require) { "use strict";
         this.downstream.forEach(function(ea) { ea.destroyItemFromUpstream(item); });
       }
     },
+    // Get persistent version of the current state of the Selection.
     /**
-     *  Get persistent version of the current state of the Selection.
+     * Returns an Array of the objects that are currently in the set. This Array does not update automatically.
+     * @function View#now
+     * @return {Array}
      */
     now: function() {
       var arr = [];
@@ -41,9 +49,17 @@ define(function module(require) { "use strict";
 
       return arr;
     },
+    /**
+     * Returns the current number of objects the set contains.
+     * @function View#size
+     * @return {Number}
+     */
     size: function() { return this.now().length; },
     /**
-     *  Specify callbacks that are executed everytime an object is added/removed from the set
+     * Takes a callback that consumes a single parameter. This callback is called whenever an object is added to the reactive set with that very object.
+     * @function View#enter
+     * @param {View~enterCallback} callback - this is executed everytime an object is added to the set
+     * @return {View} The callee of this method.
      */
     enter: function(callback) {
         this.enterCallbacks.push(callback);
@@ -51,6 +67,12 @@ define(function module(require) { "use strict";
 
         return this;
     },
+    /**
+     * Similar to \texttt{enter}, but the callback is called everytime an object is removed from the set.
+     * @function View#exit
+     * @param {View~exitCallback} callback
+     * @return {View} The callee of this method.
+     */
     exit: function(callback) {
         this.exitCallbacks.push(callback);
 
@@ -58,8 +80,9 @@ define(function module(require) { "use strict";
     },
     /**
      * Define partial behavior attached to each object while it is contained in the set.
-     * @param partialBehavior
-     * @returns {View} The callee of this method. This is done for method chaining.
+     * @function View#layer
+     * @param {Object} partialBehavior - the mixin to be applied.
+     * @returns {View} The callee of this method.
      */
     // TODO: is this currently limited to 1 layer per item-view combination?
     layer: function(partialBehavior) {
@@ -87,6 +110,18 @@ define(function module(require) { "use strict";
         return this;
     }
   });
+
+  /**
+   * This callback is call whenever an item is added to this {@link View}.
+   * @callback View~enterCallback
+   * @param {Object} item - the item that was just added to the {@link View}.
+   */
+
+  /**
+   * This callback is call whenever an item is removed from this {@link View}.
+   * @callback View~exitCallback
+   * @param {Object} item - the item that was just removed from the {@link View}.
+   */
 
   return View;
 });
