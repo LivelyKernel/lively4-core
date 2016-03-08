620c8aed3f5b434416f3aca808f858aa4d944e78
Lively4 <lively4@lively-kernel.org>
2016-03-08 11:00:06 +0100

    SYNC  M bin/syncgithub.sh;

--- bin/syncgithub.sh
+++ bin/syncgithub.sh
@@ -3,6 +3,6 @@ git status --porcelain | grep  "??" | sed 's/^?? /git add /' | bash
 echo -n "SYNC " > COMMIT ; 
 git status --porcelain | grep -v "??" | tr "\n" ";">> COMMIT;
 cat COMMIT 
- git commit -F COMMIT .; 
-# git pull --no-edit; 
-# git push origin gh-pages
+git commit -F COMMIT .; 
+git pull --no-edit; 
+git push origin gh-pages
