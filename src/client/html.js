23e2eebf530ad44172b0137a10d7fd508be3f86b
Lively4 <lively4@lively-kernel.org>
2016-03-08 13:24:08 +0100

    SYNC  M src/client/html.js;

--- src/client/html.js
+++ src/client/html.js
@@ -3,27 +3,35 @@
 
 export default class HTML {
 
- static fixLinks(nodes, dir, followPath) {
-  if (! followPath) {
-    debugger
-    throw new Error("argument followPath missing")
-  }
-  if (! nodes) return
+  static fixLinks(nodes, dir, followPath) {
+    if (! followPath) {
+      throw new Error("argument followPath missing");
+    }
+    if (! nodes) return;
+    
     Array.prototype.forEach.call(nodes, node => {
       if (node.getAttribute) {
         var href = node.getAttribute("href")
         if (href) {
           // #TODO load inplace....
-          var path
+          var path;
           if (href.match(/([A-Za-z]+):\/\/.+/)) {
             // ignore FULL URLS
-            console.log("ignore "  + href)
+            console.log("ignore "  + href);
           } else if (href.match(/^\//)) {
-            path = href // ABSOLTUE paths
+            path = href; // ABSOLTUE paths
           } else {
             path = dir + href // that leaves us RELATIVE paths
           }
           if (path) {
+            if(!path.match(/\.[a-z]+$/)) {
+              
+              // no ending?
+              // we could check, or assume md for the moment
+              path += ".md"
+              console.log("assume Markdown for "+ path)
+            }
+            
             console.log("fix "  + href + " to " + path + "(dir " + dir + ")")
             $(node).click(() => { followPath(path); return false; });
 
