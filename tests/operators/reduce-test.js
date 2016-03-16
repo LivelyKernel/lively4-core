91ca2eddc05673ca8cf5b875793c348675fa8132
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-03-16 16:23:44 +0100

    added test for .reduce operator

---
+++ tests/operators/reduce-test.js
@@ -0,0 +1,47 @@
+define(function module(require) { "use strict";
+    var withLogging = require('../../src/withlogging');
+    var select = require('../../src/select');
+
+    describe('.reduce operator', function() {
+        var AValueClass = function() { this.initialize.apply(this, arguments); };
+
+        AValueClass.prototype.initialize = function(value) {
+            this.value = value;
+        };
+
+        it('Example', function() {
+            this.timeout(10000);
+
+            withLogging.call(AValueClass);
+
+            var currentValue,
+                timesCalled = 0,
+                threshold = { min: 0 },
+                initialValue = 3;
+            var instance1 = new AValueClass(42);
+            var baseView = select(AValueClass, function(data) {
+                return data.value > threshold.min;
+            }).reduce(function(aggregation) {
+                timesCalled++;
+                currentValue = aggregation;
+            }, function(acc, instance) {
+                return acc + instance.value;
+            }, initialValue);
+
+            expect(currentValue).to.equal(initialValue + instance1.value);
+            expect(timesCalled).to.equal(1);
+
+            var instance2 = new AValueClass(5);
+            expect(currentValue).to.equal(
+                initialValue +
+                instance1.value +
+                instance2.value
+            );
+            expect(timesCalled).to.equal(2);
+
+            threshold.min = 50;
+            expect(currentValue).to.equal(initialValue);
+            expect(timesCalled).to.equal(4);
+        });
+    });
+});
