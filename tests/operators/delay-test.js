91c99dc554e51e189cc44ca174e8650f1a15094a
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-03-16 12:28:42 +0100

    added complex test involving events, map, and select

--- tests/operators/delay-test.js
+++ tests/operators/delay-test.js
@@ -2,9 +2,55 @@ define(function module(require) { "use strict";
     var withLogging = require('../../src/withlogging');
     var select = require('../../src/select');
 
-    describe('interpretation', function() {
-        it('runs an empty program', function() {
-            expect((undefined)).to.be.undefined;
+    describe('.delay operator', function() {
+        it('OtherClass example', function(done) {
+            var OtherClass = require('../../src/expr').OtherClass;
+            withLogging.call(OtherClass);
+
+            var otherInstance1 = new OtherClass(42);
+            var baseView = select(OtherClass, function(data) {
+                return data.value === 42;
+            });
+            var otherInstance2 = new OtherClass(42);
+            var delayedView = baseView.delay(500);
+            var otherInstance3 = new OtherClass(42);
+            var otherInstance4;
+
+            expect(delayedView.now().length).to.equal(0);
+
+            setTimeout(function() {
+                otherInstance4 = new OtherClass(42);
+                otherInstance3.value = 17;
+            }, 150);
+
+            setTimeout(function() {
+                otherInstance3.value = 42;
+            }, 350);
+
+            setTimeout(function() {
+                expect(delayedView.now().length).to.equal(2);
+                expect(delayedView.now()).to.include(otherInstance1);
+                expect(delayedView.now()).to.include(otherInstance2);
+
+                otherInstance1.value = 17;
+
+                expect(delayedView.now().length).to.equal(1);
+                expect(delayedView.now()).to.include(otherInstance2);
+            }, 550);
+
+            setTimeout(function() {
+                expect(delayedView.now().length).to.equal(2);
+                expect(delayedView.now()).to.include(otherInstance2);
+                expect(delayedView.now()).to.include(otherInstance4);
+            }, 750);
+
+            setTimeout(function() {
+                expect(delayedView.now().length).to.equal(3);
+                expect(delayedView.now()).to.include(otherInstance2);
+                expect(delayedView.now()).to.include(otherInstance3);
+                expect(delayedView.now()).to.include(otherInstance4);
+                done();
+            }, 1000);
         });
     });
 });
