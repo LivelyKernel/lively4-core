9159c75118ea3d1158ec60243b24d9dcedec8aa0
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-04-01 16:38:36 +0200

    correct path to relative images in same folder or subfolder of .md files

--- src/client/html.js
+++ src/client/html.js
@@ -44,6 +44,26 @@ export default class HTML {
           } else {
             console.log("ignore " + href)
           }
+        } else if (node.getAttribute('src')) {
+          // image tag
+          let src = node.getAttribute('src'),
+              // TODO: remove duplicate
+              swAwareBasePath = 'https://lively4/',
+              path;
+          // TODO: remove duplucated logic
+          if (src.match(/([A-Za-z]+):\/\/.+/)) {
+            // ignore FULL URLS
+            console.log("ignore "  + src);
+          } else if (src.match(/^\//)) {
+            path = src; // ABSOLTUE paths
+          } else {
+            path = swAwareBasePath + dir + src // that leaves us RELATIVE paths
+          }
+          if (path) {
+            node.setAttribute('src', path);
+          } else {
+            console.log("ignore image " + href)
+          }
         }
         this.fixLinks(node.childNodes, dir, followPath)
       }
