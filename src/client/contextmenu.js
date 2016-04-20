dae56cee7f5aedf4c8b4374bd34e4b8031f8a6c0
jens.lincke <jens.lincke@hpi.uni-potsdam.de>
2016-04-20 16:22:25 +0200

    SYNC  M src/client/contextmenu.js; M templates/lively-container.html; M templates/lively-sync.html;

--- src/client/contextmenu.js
+++ src/client/contextmenu.js
@@ -12,6 +12,7 @@ export default class ContextMenu {
     this.hide()
     return lively.components.openInWindow(comp).then((w) => {
         lively.setPosition(w, lively.pt(evt.pageX, evt.pageY))
+        if (comp.windowTitle) w.setAttribute("title", comp.windowTitle())
         return comp
     })
   }
