137fc379e77d8cff7153ecdfa5cde1acae3996f8
jens.lincke <jens.lincke@hpi.uni-potsdam.de>
2016-04-21 23:49:53 +0200

    SYNC  M templates/classes/Sync.js; M templates/lively-sync.html;

--- templates/classes/Sync.js
+++ templates/classes/Sync.js
@@ -5,49 +5,62 @@ import Morph from './Morph.js';
 export default class Sync extends Morph {
   initialize() {
     this.windowTitle = "Github Sync"
-    var container = $(this.shadowRoot).find(".container")[0];
+    var container = this.q(".container");
     lively.html.registerButtons(this)
     lively.html.registerInputs(this)
     this.updateLoginStatus()
-    console.log("install..")
-    
+
     var travis = this.shadowRoot.querySelector("#travisLink")
     travis.onclick = () => {
       window.open(travis.getAttribute("href"))
       return false
     } 
-    
   }
+
+  // #TODO pull into Morph?  
+  q(selector) {
+    return this.shadowRoot.querySelector(selector)
+  }
+  
+  // #TODO into Morph or Tool
   clearLog(s) {
-    // this.shadowRoot.querySelector("#log").innerHTML = "" + s
-    var editor= this.shadowRoot.querySelector("#log").editor
+    var editor= this.q("#log").editor
     if (editor) editor.setValue("")
   }
 
   log(s) {
-    var editor = this.shadowRoot.querySelector("#log").editor
+    var editor = this.q("#log").editor
     if (editor) editor.setValue(editor.getValue() + "\n" + s)
   }
 
   async updateLoginStatus() {
     var token = await lively.focalStorage.getItem("githubToken")
-    this.shadowRoot.querySelector("#loginButton").innerHTML = 
+    this.q("#loginButton").innerHTML = 
         token ? "logout" : "login";
     var login = token ? true : false;
     this.loggedin = login;
 
-    this.shadowRoot.querySelector("#gitusername").value = 
+    this.q("#gitusername").value = 
       await lively.focalStorage.getItem("githubUsername");
-    this.shadowRoot.querySelector("#gitemail").value = 
+    this.q("#gitemail").value = 
       await lively.focalStorage.getItem("githubEmail");
     
-    var value =await lively.focalStorage.getItem("githubRepository") 
-    if (value)
-      this.shadowRoot.querySelector("#gitrepository").value = value;
+    var value = await lively.focalStorage.getItem("githubRepository") 
+    if (value)this.q("#gitrepository").value = value;
     
     this.updateContextSensitiveButtons();
     this.updateRepositoryList();
     this.updateBranchesList();
+  }
+  
+  
+  // #TODO pull into tool?
+  store(key, value) {
+    return lively.focalStorage.setItem(key, value)
+  }
+  
+  load(key) {
+    return lively.focalStorage.getItem(key)
   }
 
   login() {
@@ -55,11 +68,9 @@ export default class Sync extends Morph {
       if (result) return result
       return new Promise((resolve, reject) => {
         lively.authGithub.challengeForAuth(Date.now(), (token) => {  
-      	  lively.focalStorage.setItem("githubUsername", 
-      	    this.shadowRoot.querySelector("#gitusername").value);
-      	  lively.focalStorage.setItem("githubEmail", 
-      	    this.shadowRoot.querySelector("#gitemail").value);
-      	  lively.focalStorage.setItem("githubToken", token);
+      	  this.store("githubUsername", this.q("#gitusername").value);
+      	  this.store("githubEmail", this.q("#gitemail").value);
+      	  this.store("githubToken", token);
       	  this.updateLoginStatus();
       	  resolve(token);
         });
@@ -71,13 +82,13 @@ export default class Sync extends Morph {
 
   async getHeaders() {
     return new Headers({
-      "gitrepository": this.shadowRoot.querySelector("#gitrepository").value, 
-      "gitusername":  this.shadowRoot.querySelector("#gitusername").value,
-      "gitpassword":  await lively.focalStorage.getItem("githubToken"), 
-      "gitemail": this.shadowRoot.querySelector("#gitemail").value,
-      "gitrepositoryurl": this.shadowRoot.querySelector("#gitrepositoryurl").value,
-	    "gitrepository": this.shadowRoot.querySelector("#gitrepository").value,
-	    "gitrepositorybranch": this.shadowRoot.querySelector("#gitrepositorybranch").value,
+      "gitrepository":        this.q("#gitrepository").value, 
+      "gitusername":          this.q("#gitusername").value,
+      "gitpassword":          await this.load("githubToken"), 
+      "gitemail":             this.q("#gitemail").value,
+      "gitrepositoryurl":     this.q("#gitrepositoryurl").value,
+	    "gitrepository":        this.q("#gitrepository").value,
+	    "gitrepositorybranch":  this.q("#gitrepositorybranch").value,
     })
   }
 
@@ -102,7 +113,7 @@ export default class Sync extends Morph {
 
   async onLoginButton() {
     this.clearLog()
-    if (await lively.focalStorage.getItem("githubToken")) { 
+    if (await this.load("githubToken")) { 
       this.logout() 
     } else { 
       this.login()
@@ -123,8 +134,8 @@ export default class Sync extends Morph {
 
   async onCloneButton(){
     if (window.confirm("Do you want to clone into " + 
-        this.shadowRoot.querySelector("#gitrepository").value)) {
-      this.shadowRoot.querySelector("#cloneButton").disabled= true
+        this.q("#gitrepository").value)) {
+      this.q("#cloneButton").disabled= true
       await this.gitControl("clone")
       this.updateContextSensitiveButtons()
       this.updateRepositoryList()
@@ -141,6 +152,16 @@ export default class Sync extends Morph {
 
   onChangelogButton() {
    this.gitControl("log");
+  }
+  
+  onCommitButton() {
+   this.gitControl("commit");
+  }
+  
+  onDeleteButton() {
+    if (window.confirm("Do you want to delete " + this.q("#gitrepository").value + " repository?")) {
+      this.gitControl("delete");
+    }
   }
   
   get storagePrefix() {
