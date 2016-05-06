f4a2eba33c8204a6523fa127cb79286072292231
Meike Baumgärtner <meike.baumgaertner@googlemail.com>
2016-05-06 21:24:32 +0200

    Add tests for src/path.jsx

---
+++ src/__tests__/test-path.js
@@ -0,0 +1,28 @@
+jest.unmock('../path.jsx');
+
+const path = require('../path.jsx');
+
+describe('join', () => {
+  it('joins "d" and "s" to equal "d/s"', () => {
+    expect(path.join("d", "s")).toBe("d/s");
+  });
+  it('joins "d" and "/s" to equal "d/s"', () => {
+    expect(path.join("d", "/s")).toBe("/s");
+  });
+  it('joins "d" and "/s" to equal "d/s"', () => {
+    expect(path.join("/d", "s")).toBe("/d/s");
+  });
+});
+
+
+describe('normalize', () => {
+	it('normalizes "/a/../b/" to equal "/b"', () => {
+    	expect(path.normalize("/a/../b/")).toBe("/b");
+	});
+	it('normalizes "/a//./b" to equal "/a/b"', () => {
+    	expect(path.normalize("/a//./b")).toBe("/a/b");
+	});
+	it('normalizes "/a/../b/c/d/../f/../../g" to equal "/b/g"', () => {
+    	expect(path.normalize("/a/../b/c/d/../f/../../g")).toBe("/b/g");
+	});
+});
