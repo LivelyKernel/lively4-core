1a102fa3cb15a9e2fd836ded71515bee5e341827
TwoLaid <timo.djuerken@gmail.com>
2016-05-18 10:57:07 +0200

    Reworked Expr into a class first transformation and Proxy

--- astPlaygroundRecursive.js
+++ astPlaygroundRecursive.js
@@ -1,3 +1,4 @@
+
 //import {livelyModules} from '../lively.modules/dist/lively.modules.js';
 //import {livelyAST} from '../lively.ast/dist/lively.ast.js';
 
@@ -10,90 +11,201 @@ System["import"]("https://lively-kernel.org/lively4/lively.modules/dist/lively.m
   });
 });
 
+
+class Expr {
+  constructor(expression, callback, context) {
+    this.originalExpr = expression;
+    this.callback = callback;
+    this.context = context;
+
+    this.lastValue = undefined;
+
+    this._ast = lively.ast.parseFunction(expression);
+    this._declarations = lively.ast.query.topLevelDeclsAndRefs(this._ast);
+    this.observables = this.detectObservables();
+
+    this.transformExpression();
+    this.proxify();
+    
+    this.test();
+  }
+  
+  test() {
+    let result = this.expr(this.context);
+    
+    console.log('test returned', result);
+
+    if (this.lastValue === undefined) {
+      this.lastValue = result;
+    } else {
+      if (this.lastValue !== result) {
+        console.log('value changed!');
+        this.callback();
+      }
+      this.lastValue = result;
+    }
+  }
+  
+  transformExpression() {
+    let off = 0;
+    
+    let src = this.originalExpr.toString();
+    
+    for (let i = this._declarations.refs.length - 1; i >= 0; i--) {
+      let node = this._declarations.refs[i];
+      let left = src.substring(0, node.start - 1);
+      let right = src.substring(node.start - 1);
+      src = left + '__context.' + right;
+      off += 10;
+    }
+
+    let body = src.substring(this._ast.body.start - 1, this._ast.body.end + off);
+    console.log(body);
+    this.expr = eval('(function(__context)' + body + ')');
+    console.log(this.expr);
+  }
+  
+  detectObservables() {
+    var state = {
+      localVariables: [],
+      variablesToObserve: new Set(),
+      ownVariablesToObserve: []
+    };
+
+    lively.ast.acorn.walk.recursive(this._ast, state, {
+      'VariableDeclarator': function VariableDeclarator(node, state, c) {
+        var varName = node.id.name;
+        state.localVariables.push(varName);
+  
+        c(node.init, state);
+      },
+      'MemberExpression': function MemberExpression(node, state, c) {
+        var tmpState = jQuery.extend(true, {}, state);
+  
+        c(node.object, tmpState);
+        node.object.name = tmpState.varName;
+  
+        c(node.property, tmpState);
+        node.property.name = tmpState.varName;
+  
+        var varName = node.object.name + '.' + node.property.name;
+        state.varName = varName;
+        state.variablesToObserve.add(varName);
+      },
+      'ThisExpression': function ThisExpression(node, state, c) {
+        //makes no sense all alone, so only used to get 'varName'
+  
+        state.varName = 'this';
+      },
+      'Identifier': function Identifier(node, state, c) {
+        if (state.localVariables.indexOf(node.name) === -1) {
+          state.variablesToObserve.add(node.name);
+        }
+  
+        state.varName = node.name;
+      }
+    });
+  
+    return state;
+  }
+  
+  proxify() {
+    for(let observable of this.observables.variablesToObserve) {
+      let observableParts = observable.split('.');
+      let contextVariableName = observableParts.shift();
+      let objectPropertyName = observableParts.join("");
+  
+      let object = null;
+      if(contextVariableName != observable) {
+        object = context[contextVariableName];
+  
+        if(object) {
+          this.proxifyVariable(object, objectPropertyName, callback);
+        }
+      } else {
+        //has no dot in its name, so is probably a global var?
+      }
+      
+    }
+  }
+  
+  proxifyVariable(object, variable, callback) {
+    console.log(object, variable, callback);
+    
+    ['__lively_expr_getters',
+     '__lively_expr_setters',
+     '__lively_expr_vars'
+    ].forEach((k) => {
+      if (object[k] === undefined) {
+        object[k] = {};
+      }
+    })
+    
+    if (object.__lively_expr_watchers === undefined) {
+      object.__lively_expr_watchers = new Set();
+    }
+
+    // TODO: handle multiple expressions on same object
+    
+    let oldGetter = object.__lookupGetter__(variable) || object.__lively_expr_getters[variable];
+
+    let oldSetter = object.__lookupSetter__(variable) || object.__lively_expr_setters[variable];
+
+    if (oldGetter) object.__lively_expr_getters[variable] = oldGetter;
+    if (oldSetter) object.__lively_expr_setters[variable] = oldSetter;
+
+    // save current value for default getter
+    object.__lively_expr_vars[variable] = object[variable];
+    
+    object.__lively_expr_watchers.add(this);
+    
+    console.log('old getter:', oldGetter);
+    console.log('old setter:', oldSetter);
+
+    var newGetter = function() {
+      if (this.__lively_expr_getters[variable]) {
+        return this.__lively_expr_getters[variable].call(this);
+      }
+      return this.__lively_expr_vars[variable];
+    };
+    
+    var newSetter = function(newValue) {
+      if (this.__lively_expr_setters[variable]) {
+        this.__lively_expr_setters[variable].call(this, newValue);
+      } else {
+        this.__lively_expr_vars[variable] = newValue;
+      }
+
+      // alert watchers
+      this.__lively_expr_watchers.forEach((w) => {
+        w.test();
+      });
+    };
+    
+    object.__defineGetter__(variable, newGetter);
+    object.__defineSetter__(variable, newSetter);
+  }
+}
+
 //var code = "var ret = 0; if(obj.b.c.d.e == this.a.b.d.e) null(); ret = this.a; ret = ret + obj.b;";
 //var code = "var width = 1 + obj.dimensions.width + obj.margin.top.left; var isTooBig = lively.width() / 100 || asdf; width > 100";
 //var code = "var width = obj.width(); width < 100";
-var code = "obj.width > 100";
-var callback = function() {console.log('changed')};
+
+
 var object = {
   height: 100,
   width: 200
 };
 var context = {obj: object};
 
-expr(code, callback, context);
+var expr = new Expr(function() { 
+  return obj.width > 100 && obj.width < 1000 
+}, () => {
+  console.log('changed');
+}, context);
 
-function expr(expression, callback, context) {
-  let ast = lively.ast.parse(expression);
-  let observables = detectObservables(ast);
-  proxify(context, observables.variablesToObserve, callback);
-}
+setTimeout(function() {
+  context.obj.width = 0;  
+}, 1000);
 
-function proxify(context, observables, callback) {
-  for(let observable of observables) {
-    let observableParts = observable.split('.');
-    let contextVariableName = observableParts.shift();
-    let objectPropertyName = observableParts.join("");
 
-    let object = null;
-    if(contextVariableName != observable) {
-      object = context[contextVariableName];
-
-      if(object) {
-        proxifyVariable(object, objectPropertyName, callback);
-      }
-    } else {
-      //has no dot in its name, so is probably a global var?
-    }
-    
-  }
-}
-
-function proxifyVariable(object, variable, callback) {
-  console.log(object, variable, callback);
-  console.log("TODO: implement active-expressions::proxifyVariable");
-}
-
-function detectObservables(ast) {
-  var state = {
-    localVariables: [],
-    variablesToObserve: [],
-    ownVariablesToObserve: []
-  };
-
-  lively.ast.acorn.walk.recursive(ast, state, {
-    'VariableDeclarator': function VariableDeclarator(node, state, c) {
-      var varName = node.id.name;
-      state.localVariables.push(varName);
-
-      c(node.init, state);
-    },
-    'MemberExpression': function MemberExpression(node, state, c) {
-      var tmpState = jQuery.extend(true, {}, state);
-
-      c(node.object, tmpState);
-      node.object.name = tmpState.varName;
-
-      c(node.property, tmpState);
-      node.property.name = tmpState.varName;
-
-      var varName = node.object.name + '.' + node.property.name;
-      state.varName = varName;
-      state.variablesToObserve.push(varName);
-    },
-    'ThisExpression': function ThisExpression(node, state, c) {
-      //makes no sense all alone, so only used to get 'varName'
-
-      state.varName = 'this';
-    },
-    'Identifier': function Identifier(node, state, c) {
-      if (state.localVariables.indexOf(node.name) === -1) {
-        state.variablesToObserve.push(node.name);
-      }
-
-      state.varName = node.name;
-    }
-  });
-
-  return state;
-}
