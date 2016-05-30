4f5eb52f4e26235fb983b846ce982a39480c9880
Philipp Otto <philipp@scm.io>
2016-05-30 12:22:43 +0200

    adapt front-end to back-end changes

--- templates/classes/Services.js
+++ templates/classes/Services.js
@@ -2,8 +2,15 @@
 
 import Morph from './Morph.js';
 
+const isLocalHost = document.location.hostname.indexOf("localhost") > -1;
+const localBaseUrl = "http://localhost:9007/";
+const remoteBaseUrl = "https://lively-kernel.org/lively4services/";
+const baseUrl = isLocalHost ? localBaseUrl : remoteBaseUrl;
+
+var services = {};
+
 export default class Services extends Morph {
-  
+
   initialize() {
     this.windowTitle = "Services"
     this.serviceList = this.getSubmorph(".items");
@@ -14,25 +21,25 @@ export default class Services extends Morph {
       evt.target.getSubmorph('.item').classList.add("selected");
       this.showService(pid);
     });
-    
+
     this.serviceTop = this.getSubmorph('.service-top');
     this.serviceNameInput = this.getSubmorph('.service-top input');
-    
+
     this.addButton = this.getSubmorph("#addButton");
     this.addButton.addEventListener("click", (evt) => {
       this.serviceTop.removeAttribute("data-pid");
-      this.serviceNameInput.value = "";
+      this.serviceNameInput.value = "Enter name...";
       this.codeEditor.editor.setValue("");
       this.logEditor.editor.setValue("");
       this.unselectAll();
     });
-    
+
     this.removeButton = this.getSubmorph("#removeButton");
     this.removeButton.addEventListener("click", (evt) => {
       var pid = this.serviceTop.getAttribute('data-pid');
       this.removeService(pid);
     });
-    
+
     this.refreshButton = this.getSubmorph("#refreshButton");
     this.refreshButton.addEventListener("click", (evt) => { this.refreshServiceList() });
     this.startButton = this.getSubmorph("#startButton");
@@ -48,42 +55,44 @@ export default class Services extends Morph {
     this.debugButton = this.getSubmorph("#debugButton");
     this.debugButton.addEventListener("click", (evt) => {
       lively.openComponentInWindow('lively-iframe').then(component => {
-	      component.setURL('https://lively-kernel.org/lively4servicesDebug/?port=5858')
+        component.setURL('https://lively-kernel.org/lively4servicesDebug/?port=5858')
       });
     });
-    
+
     this.codeEditor = this.getSubmorph("#code");
     this.logEditor = this.getSubmorph("#log");
-    
+
     this.refreshServiceList();
   }
-  
+
   unselectAll() {
     var children = this.serviceList.children;
     for (var i = 0; i < children.length; i++) {
       children[i].getSubmorph('.item').classList.remove("selected");
     }
   }
-  
+
   showService(pid) {
-    this.serviceTop.setAttribute("data-pid", pid);
     var that = this;
-    $.get("https://lively-kernel.org/lively4services/get?pid=" + pid, null, function(service) {
+    this.serviceTop.setAttribute("data-pid", pid);
+    var serviceName = services[pid].name;
+    this.serviceNameInput.value = serviceName;
+    $.get(baseUrl + "get?serviceName=" + serviceName, null, function(service) {
       that.codeEditor.editor.setValue(service.code);
       that.logEditor.editor.setValue(service.log);
     });
   }
-  
+
   startService(pid) {
     var that = this;
-    
+
     console.log(pid)
-    
-    if (pid === null) {
+
+    // if (pid === null) {
       var name = that.serviceNameInput.value;
       var code = that.codeEditor.editor.getValue();
       $.ajax({
-        url: 'https://lively-kernel.org/lively4services/start',
+        url: baseUrl + 'start',
         type: 'POST',
         data: JSON.stringify({ name: name, code: code }),
         contentType: "application/json",
@@ -92,17 +101,18 @@ export default class Services extends Morph {
           that.refreshServiceList();
         }
       });
-    }else {
-      //implement resume process
-    }
+    // }else {
+    //   //implement resume process
+    // }
   }
-  
+
   stopService(pid) {
     var that = this;
+    var serviceName = services[pid].name;
     $.ajax({
-      url: 'https://lively-kernel.org/lively4services/stop',
+      url: baseUrl + 'stop',
       type: 'POST',
-      data: JSON.stringify({ pid: pid }),
+      data: JSON.stringify({ serviceName: serviceName }),
       contentType: "application/json",
       success: function(res) {
         console.log(res);
@@ -110,13 +120,14 @@ export default class Services extends Morph {
       }
     });
   }
-  
+
   removeService(pid) {
     var that = this;
+    var serviceName = services[pid].name;
     $.ajax({
-      url: 'https://lively-kernel.org/lively4services/remove',
+      url: baseUrl + 'remove',
       type: 'POST',
-      data: JSON.stringify({ pid: pid }),
+      data: JSON.stringify({ serviceName: serviceName }),
       contentType: "application/json",
       success: function(res) {
         console.log(res);
@@ -124,10 +135,11 @@ export default class Services extends Morph {
       }
     });
   }
-  
+
   refreshServiceList() {
     var that = this;
-    $.get("https://lively-kernel.org/lively4services/list", null, function(services) {
+    $.get(baseUrl + "list", null, function(_services) {
+      services = _services;
       // Clear all items
       while (that.serviceList.firstChild) {
         that.serviceList.removeChild(that.serviceList.firstChild);
@@ -138,31 +150,31 @@ export default class Services extends Morph {
         return;
       }
       // List all services
-  	  var now = new Date().getTime();
-  	  for (var pid in services) {
-  	    var service = services[pid];
-  	    var item = document.createElement("lively-services-item");
-  	    item.setAttribute("data-pid", pid);
-  	    var title = service.name + ' (#' + pid + ')';
-  	    item.getSubmorph('h1').innerHTML = title;
-  	    
-  	    var status = "unkown";
-  	    var statusText = "unkown";
-  	    if (service.status === 0) {
-  	      status = "not-running";
-  	      var uptime = (now - service.start)/1000 + "s";
-  	      statusText = "not running (" + uptime + ")";
-  	    } else if (service.status === 1) {
-  	      status = "running";
-  	      var uptime = (now - service.start)/1000 + "s";
-  	      statusText = "running (" + uptime + ")";
-  	    }
-  	    
-  	    
-  	    item.getSubmorph('.status').classList.add(status);
-  	    item.getSubmorph('small').innerHTML = statusText;
-  	    that.serviceList.appendChild(item);
-  	  }
-  	});
+      var now = new Date().getTime();
+      for (var pid in services) {
+        var service = services[pid];
+        var item = document.createElement("lively-services-item");
+        item.setAttribute("data-pid", pid);
+        var title = service.name + ' (#' + pid + ')';
+        item.getSubmorph('h1').innerHTML = title;
+
+        var status = "unkown";
+        var statusText = "unkown";
+        if (service.status === 0) {
+          status = "not-running";
+          var uptime = (now - service.start)/1000 + "s";
+          statusText = "not running (" + uptime + ")";
+        } else if (service.status === 1) {
+          status = "running";
+          var uptime = (now - service.start)/1000 + "s";
+          statusText = "running (" + uptime + ")";
+        }
+
+
+        item.getSubmorph('.status').classList.add(status);
+        item.getSubmorph('small').innerHTML = statusText;
+        that.serviceList.appendChild(item);
+      }
+    });
   }
 }
