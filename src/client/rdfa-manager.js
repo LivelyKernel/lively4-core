e3e87c6d8373364fee2f6989059424fb898248e7
numberpi <daniel.stolpe@gmx.de>
2016-06-16 14:52:12 +0200

    added collapsible tree to rdfa table

--- src/client/rdfa-manager.js
+++ src/client/rdfa-manager.js
@@ -30,8 +30,8 @@ export default class RdfaManager {
       boxDiv.style.background = "#aaa";
       boxDiv.style.right = 0;
       boxDiv.style.bottom = 0;
-      boxDiv.style.width = "200px";
-      boxDiv.style.height = "200px";
+      boxDiv.style.width = "400px";
+      boxDiv.style.height = "400px";
       
       document.body.appendChild(boxDiv);
     }
@@ -64,22 +64,64 @@ export default class RdfaManager {
   
   static generateJSONTableRows(div, remote) {
     div[0].style.overflow = "auto";
-    var table = $('<table>');
+    var table = $('<table>').attr("id", "rdfaTable");
     div.append(table);
     this.buildJSONRdfaDataStructure(remote).then((data) => {
       data.forEach((projection) => {
-        table.append(
-          $('<tr>')
-            .append($('<td>').text(projection._data_.subject)));
+        table.append($('<tr>').attr('data-depth', 0).addClass("collapse")
+          .append($('<td>')
+            .append($('<i>').addClass("fa").addClass("fa-minus-square").addClass("treeToggle").attr('aria-hidden', true))
+            .append(" ")
+            .append(projection._data_.subject)
+          )
+        );
         let properties = projection._data_.properties;
         for (let property in properties) {
           table.append(
-            $('<tr>')
+            $('<tr>').attr('data-depth', 1).addClass("collapse")
               .append($('<td>'))
               .append($('<td>').text(property))
               .append($('<td>').text(properties[property])));
         }
       });
+    });
+    
+    $('#rdfaTable').on('click', '.treeToggle', function () {
+      //Gets all <tr>'s  of greater depth
+      //below element in the table
+      var findChildren = function (tr) {
+          var depth = tr.data('depth');
+          return tr.nextUntil($('tr').filter(function () {
+              return $(this).data('depth') <= depth;
+          }));
+      };
+
+      var el = $(this);
+      var tr = el.closest('tr'); //Get <tr> parent of toggle button
+      var children = findChildren(tr);
+
+      //Remove already collapsed nodes from children so that we don't
+      //make them visible. 
+      //(Confused? Remove this code and close Item 2, close Item 1 
+      //then open Item 1 again, then you will understand)
+      var subnodes = children.filter('.expand');
+      subnodes.each(function () {
+          var subnode = $(this);
+          var subnodeChildren = findChildren(subnode);
+          children = children.not(subnodeChildren);
+      });
+
+      //Change icon and hide/show children
+      if (tr.hasClass('collapse')) {
+          tr.removeClass('collapse').addClass('expand');
+          $(tr).find('.fa-minus-square').removeClass('fa-minus-square').addClass("fa-plus-square");
+          children.hide();
+      } else {
+          tr.removeClass('expand').addClass('collapse');
+          $(tr).find('.fa-plus-square').removeClass('fa-plus-square').addClass("fa-minus-square");
+          children.show();
+      }
+      return children;
     });
   }
 
