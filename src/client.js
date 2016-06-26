c9c7052ebfed47d3bf041540a71ebab115775b8b
Jan Graichen <jg@altimos.de>
2016-06-27 00:06:53 +0200

    Drop printing SW state changes

--- src/client.js
+++ src/client.js
@@ -60,8 +60,6 @@ export default async function() {
         serviceWorker.addEventListener('statechange', fn)
       })
 
-      serviceWorker.addEventListener('statechange', (e) => console.log(e.target.state))
-
       await swState
     }
 
