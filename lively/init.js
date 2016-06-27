5fde1466c92dc10ce83565e0e285d91908b10e40
Jan Graichen <jg@altimos.de>
2016-06-27 17:07:13 +0200

    Add JSX attributes support

--- lively/init.js
+++ lively/init.js
@@ -3,6 +3,18 @@ function toHTML(desc) {
   if (typeof desc === 'object') {
     let el = document.createElement(desc.elementName)
 
+    if (desc.attributes) {
+      for(let key of Object.keys(desc.attributes)) {
+        el[key] = desc.attributes[key]
+      }
+    }
+
+    if (desc.attributes.style) {
+      for(let key of Object.keys(desc.attributes.style)) {
+        el.style[key] = desc.attributes.style[key]
+      }
+    }
+
     if (desc.children) {
       for (let child of desc.children) {
         el.appendChild(toHTML(child))
@@ -21,7 +33,12 @@ class FileBrowser extends HTMLElement {
   }
 
   createdCallback() {
-    let a = toHTML(<div>
+    let style = {
+      color: 'red',
+      fontSize: 20
+    }
+
+    let a = toHTML(<div className='abc' style={style}>
       <h2>File Browser</h2>
     </div>)
 
