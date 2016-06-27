6fd65de7d0a152245588278baae9f9f896df9b5b
Fabio Niephaus <code@fniephaus.com>
2016-06-27 11:51:05 +0200

    Extract event handler methods; improve UX and UI

--- templates/classes/Services.js
+++ templates/classes/Services.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Morph from './Morph.js';
 
 const isLocalHost = document.location.hostname.indexOf('localhost') > -1;
@@ -18,129 +16,92 @@ export default class Services extends Morph {
     this.logType = 'stdout';
     this.services = {};
 
-    this.serviceList = this.getSubmorph('.items');
-    $(this.serviceList).on('click', 'lively-services-item', (evt) => {
-      this.unselectAll();
-      evt.target.getSubmorph('.item').classList.add('selected');
-      this.pid = evt.target.getAttribute('data-id');
-      this.showService();
-    });
-
     this.serviceTop = this.getSubmorph('#service-top');
     this.entryPoint = this.getSubmorph('#entryPoint');
 
+    this.serviceList = this.getSubmorph('.items');
+    $(this.serviceList).on('click', 'lively-services-item', this.itemClick.bind(this));
+
     this.addButton = this.getSubmorph('#addButton');
-    this.addButton.addEventListener('click', (evt) => {
-      this.serviceTop.removeAttribute('data-id');
-      this.entryPoint.value = '';
-      this.entryPoint.focus();
-      this.logEditor.setValue('');
-      this.unselectAll();
-    });
-
+    this.addButton.addEventListener('click', this.addButtonClick.bind(this));
     this.removeButton = this.getSubmorph('#removeButton');
-    this.removeButton.addEventListener('click', (evt) => {
-      this.removeService();
-    });
-
+    this.removeButton.addEventListener('click', this.removeButtonClick.bind(this));
     this.editButton = this.getSubmorph('#editButton');
-    this.editButton.addEventListener('click', (evt) => {
-      lively.openBrowser(servicesURL + 'lively/');
-    });
-
+    this.editButton.addEventListener('click', this.editButtonClick.bind(this));
     this.settingsButton = this.getSubmorph('#settingsButton');
-    this.settingsButton.addEventListener('click', (evt) => {
-      var userInput;
-      userInput = window.prompt('Please enter service endpoint', servicesURL);
-      if (userInput !== null) {
-        servicesURL = userInput;
-      }
-      userInput = window.prompt('Please enter debugger endpoint', debuggerURL);
-      if (userInput !== null) {
-        debuggerURL = userInput;
-      }
-    });
+    this.settingsButton.addEventListener('click', this.settingsButtonClick.bind(this));
     this.startButton = this.getSubmorph('#startButton');
-    this.startButton.addEventListener('click', (evt) => {
-      this.startService();
-    });
+    this.startButton.addEventListener('click', this.startButtonClick.bind(this));
     this.stopButton = this.getSubmorph('#stopButton');
-    this.stopButton.addEventListener('click', (evt) => {
-      this.stopService();
-    });
+    this.stopButton.addEventListener('click', this.stopButtonClick.bind(this));
     this.debugButton = this.getSubmorph('#debugButton');
-    this.debugButton.addEventListener('click', (evt) => {
-      lively.openComponentInWindow('lively-iframe').then(component => {
-        if (!(this.pid in this.services)) {
-          console.log('Nothing to debug');
-          return;
-        }
-        var debuggerPort = this.services[this.pid].debugPort;
-        component.setURL(debuggerURL + '?port=' + debuggerPort);
-      });
-    });
-
+    this.debugButton.addEventListener('click', this.debugButtonClick.bind(this));
     this.stdoutButton = this.getSubmorph('#stdoutButton');
     this.stderrButton = this.getSubmorph('#stderrButton');
-    this.stdoutButton.addEventListener('click', (evt) => {
-      this.stdoutButton.classList.add('active');
-      this.stderrButton.classList.remove('active');
-      this.logType = 'stdout';
-      this.refreshLog();
-    });
-    this.stderrButton.addEventListener('click', (evt) => {
-      this.stderrButton.classList.add('active');
-      this.stdoutButton.classList.remove('active');
-      this.logType = 'stderr';
-      this.refreshLog();
-    });
+    this.stdoutButton.addEventListener('click', this.stdoutButtonClick.bind(this));
+    this.stderrButton.addEventListener('click', this.stderrButtonClick.bind(this));
 
     this.logEditor = this.getSubmorph('#log').editor;
     this.logEditor.setReadOnly(true);
 
     this.refreshServiceList();
-    this.refreshInterval = setInterval(() => {
+    this.refreshInterval = window.setInterval(() => {
       this.refreshServiceList();
     }, 5000);
     this.logInterval = null;
 
     this.detachedCallback = () => {
-      clearInterval(this.refreshInterval);
-      clearInterval(this.logInterval);
+      window.clearInterval(this.refreshInterval);
+      window.clearInterval(this.logInterval);
     };
   }
 
-  unselectAll() {
-    var children = this.serviceList.children;
-    for (var i = 0; i < children.length - 1; i++) {
-      children[i].getSubmorph('.item').classList.remove('selected');
+  /*
+  * Event handlers
+  */
+  itemClick(evt) {
+    this.unselectAll();
+    evt.target.getSubmorph('.item').classList.add('selected');
+    this.pid = evt.target.getAttribute('data-id');
+    this.showService();
+  }
+
+  addButtonClick(evt) {
+    this.serviceTop.removeAttribute('data-id');
+    this.entryPoint.value = '';
+    this.entryPoint.focus();
+    this.logEditor.setValue('');
+    this.unselectAll();
+  }
+
+  removeButtonClick() {
+    if (this.pid === null) {
+      console.log('Nothing to remove');
+      return;
+    }
+    this.post('remove', { id: this.pid }, function(res) {
+      console.log(res);
+      this.refreshServiceList();
+    }.bind(this));
+  }
+
+  editButtonClick() {
+    lively.openBrowser(servicesURL + 'lively/');
+  }
+
+  settingsButtonClick() {
+    var userInput;
+    userInput = window.prompt('Please enter service endpoint', servicesURL);
+    if (userInput !== null) {
+      servicesURL = userInput;
+    }
+    userInput = window.prompt('Please enter debugger endpoint', debuggerURL);
+    if (userInput !== null) {
+      debuggerURL = userInput;
     }
   }
 
-  post(endpoint, data, success, error) {
-    $.ajax({
-      url: servicesURL + endpoint,
-      type: 'POST',
-      data: JSON.stringify(data),
-      contentType: 'application/json',
-      success: success,
-      error: error || function(jqXHR, textStatus, errorThrown) {
-        console.log(errorThrown);
-      }
-    });
-  }
-
-  showService() {
-    this.entryPoint.value = this.services[this.pid].entryPoint;
-    this.refreshLog();
-    if (this.logInterval === null) {
-      this.logInterval = setInterval(function() {
-        this.refreshLog();
-      }.bind(this), 2000);
-    }
-  }
-
-  startService() {
+  startButtonClick() {
     var data;
     if (this.pid !== null) {
       data = { id: this.pid };
@@ -153,48 +114,111 @@ export default class Services extends Morph {
     }.bind(this));
   }
 
-  stopService() {
+  stopButtonClick(evt) {
     this.post('stop', { id: this.pid }, function(res) {
       console.log(res);
       this.refreshServiceList();
-      clearInterval(this.logInterval);
+      window.clearInterval(this.logInterval);
       this.logInterval = null;
     }.bind(this));
   }
 
-  removeService() {
-    if (this.pid === null) {
-      console.log('Nothing to remove');
-      return;
-    }
-    this.post('remove', { id: this.pid }, function(res) {
-      console.log(res);
-      this.refreshServiceList();
-    }.bind(this));
+  debugButtonClick(evt) {
+    lively.openComponentInWindow('lively-iframe').then(component => {
+      if (!(this.pid in this.services)) {
+        console.log('Nothing to debug');
+        return;
+      }
+      var debuggerPort = this.services[this.pid].debugPort;
+      component.setURL(debuggerURL + '?port=' + debuggerPort);
+    });
   }
 
+  stdoutButtonClick(evt) {
+    this.stdoutButton.classList.add('active');
+    this.stderrButton.classList.remove('active');
+    this.logType = 'stdout';
+    this.refreshLog();
+  }
+
+  stderrButtonClick(evt) {
+    this.stderrButton.classList.add('active');
+    this.stdoutButton.classList.remove('active');
+    this.logType = 'stderr';
+    this.refreshLog();
+  }
+
+  /*
+  * Helper functions
+  */
+  post(endpoint, data, success, error) {
+    $.ajax({
+      url: servicesURL + endpoint,
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: success,
+      error: this.handleAjaxError.bind(this)
+    });
+  }
+
+  handleAjaxError(jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown);
+    this.removeAllItems();
+    this.showMessageInServiceList("Cannot connect to server.");
+  }
+
+  unselectAll() {
+    var children = this.serviceList.children;
+    for (var i = 0; i < children.length - 1; i++) {
+      children[i].getSubmorph('.item').classList.remove('selected');
+    }
+    this.pid = null;
+  }
+
+  removeAllItems() {
+    var selectedPID = null;
+    while (this.serviceList.firstChild) {
+      item = this.serviceList.firstChild;
+      if (selectedPID === null && !item.classList.contains('empty') &&
+          item.getSubmorph('.item').classList.contains('selected')) {
+        selectedPID = item.getAttribute('data-id');
+      }
+      this.serviceList.removeChild(this.serviceList.firstChild);
+    }
+    return selectedPID;
+  }
+
+  showService() {
+    this.entryPoint.value = this.services[this.pid].entryPoint;
+    this.refreshLog();
+    if (this.logInterval === null) {
+      this.logInterval = window.setInterval(function() {
+        this.refreshLog();
+      }.bind(this), 2000);
+    }
+  }
+
+  showMessageInServiceList(msg) {
+    var item = document.createElement('div');
+    item.classList.add('empty');
+    item.innerHTML = msg;
+    this.serviceList.appendChild(item);
+  }
+
+  /*
+  * Refresh functions
+  */
   refreshServiceList() {
     $.ajax({
       url: servicesURL + 'list',
       success: function(services) {
         this.services = services;
         var item;
-        var selectedPID = null;
-        // Clear all items
-        while (this.serviceList.firstChild) {
-          item = this.serviceList.firstChild;
-          if (selectedPID === null && !item.classList.contains('empty') &&
-              item.getSubmorph('.item').classList.contains('selected')) {
-            selectedPID = item.getAttribute('data-id');
-          }
-          this.serviceList.removeChild(this.serviceList.firstChild);
-        }
+        var selectedPID = this.removeAllItems();
         // Check if any service running
         if (Object.keys(services).length === 0) {
-          item = document.createElement('div');
-          item.classList.add('empty');
-          item.innerHTML = "No services available yet.";
-          this.serviceList.appendChild(item);
+          this.showMessageInServiceList("No services available yet.");
           return;
         }
         // List all services
@@ -225,13 +249,14 @@ export default class Services extends Morph {
           item.getSubmorph('small').innerHTML = statusText;
           this.serviceList.appendChild(item);
         }
-      }.bind(this)
+      }.bind(this),
+      error: this.handleAjaxError.bind(this)
     });
   }
 
   refreshLog() {
     this.post('get', { id: this.pid }, function(res) {
-      this.logEditor.setValue(res.log);
+      this.logEditor.setValue(res[this.logType]);
       this.logEditor.gotoPageDown();
     }.bind(this));
   }
