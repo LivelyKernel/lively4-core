f8971eb6ddd33eaff3b8f157e673c78301ae9699
Meike Baumgärtner <meike.baumgaertner@googlemail.com>
2016-07-01 18:45:46 +0200

    Add wrapper for service worker cache API

---
+++ src/cache.js
@@ -0,0 +1,15 @@
+function open() {
+  return caches.open('lively4')
+}
+
+export function put(request, response) {
+  return open().then((cache) => cache.put(request, response))
+}
+
+export function delete(request) {
+  return open().then((cache) => cache.delete(request))
+}
+
+export function match(request) {
+  return open().then((cache) => cache.match(request))
+}
