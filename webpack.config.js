2dab8bd424d56c189304ec3b2951d43e59fefd5c
Jan Graichen <jg@altimos.de>
2016-07-02 22:25:13 +0200

    Revert "Drop rollup-loader"
    This reverts commit 8499e889c5234271403976f58688f796bca75477.
    
    WebPack alone has massive problems with requiring files out-of-tree that
    needs to be transpiled (and parse package.json etc. with options not
    relvant for *that* compile)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path'),
-  webpack = require('webpack')
+  webpack = require('webpack');
 
 let config = {
   BASE: false,
@@ -42,7 +42,7 @@ module.exports = {
   devtool: 'source-map',
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'babel' },
+      { test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'rollup' },
       { test: /\.json$/, loader: 'json' }
     ]
   },
@@ -54,32 +54,34 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin(values)
   ],
-  babel: {
-    compact: true,
-    comments: false,
-    babelrc: false,
-    plugins: [
-      require("babel-plugin-syntax-async-functions"),
-      require("babel-plugin-syntax-async-generators"),
-      require("babel-plugin-syntax-class-properties"),
-      require("babel-plugin-syntax-decorators"),
-      require("babel-plugin-syntax-do-expressions"),
-      require("babel-plugin-syntax-exponentiation-operator"),
-      require("babel-plugin-syntax-export-extensions"),
-      require("babel-plugin-syntax-function-bind"),
-      require("babel-plugin-syntax-object-rest-spread"),
-      require("babel-plugin-syntax-trailing-function-commas"),
-      require("babel-plugin-transform-async-to-generator"),
-      require("babel-plugin-transform-async-to-module-method"),
-      require("babel-plugin-transform-class-properties"),
-      require("babel-plugin-transform-decorators-legacy").default,
-      require("babel-plugin-transform-do-expressions"),
-      require("babel-plugin-transform-es2015-destructuring"),
-      require("babel-plugin-transform-exponentiation-operator"),
-      require("babel-plugin-transform-export-extensions"),
-      require("babel-plugin-transform-function-bind"),
-      require("babel-plugin-transform-object-rest-spread"),
-      require("babel-plugin-transform-es2015-modules-commonjs")
-    ]
-  }
+  rollup: [
+    require('rollup-plugin-babel')({
+      compact: true,
+      comments: false,
+      babelrc: false,
+      runtimeHelpers: true,
+      plugins: [
+        require("babel-plugin-syntax-async-functions"),
+        require("babel-plugin-syntax-async-generators"),
+        require("babel-plugin-syntax-class-properties"),
+        require("babel-plugin-syntax-decorators"),
+        require("babel-plugin-syntax-do-expressions"),
+        require("babel-plugin-syntax-exponentiation-operator"),
+        require("babel-plugin-syntax-export-extensions"),
+        require("babel-plugin-syntax-function-bind"),
+        require("babel-plugin-syntax-object-rest-spread"),
+        require("babel-plugin-syntax-trailing-function-commas"),
+        require("babel-plugin-transform-async-to-generator"),
+        require("babel-plugin-transform-async-to-module-method"),
+        require("babel-plugin-transform-class-properties"),
+        require("babel-plugin-transform-decorators-legacy").default,
+        require("babel-plugin-transform-do-expressions"),
+        require("babel-plugin-transform-es2015-destructuring"),
+        require("babel-plugin-transform-exponentiation-operator"),
+        require("babel-plugin-transform-export-extensions"),
+        require("babel-plugin-transform-function-bind"),
+        require("babel-plugin-transform-object-rest-spread"),
+      ]
+    })
+  ]
 };
