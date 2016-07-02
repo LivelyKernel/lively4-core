48ced2c5a6e33850a348837ecd99203bfe61c4f1
Meike Baumgärtner <meike.baumgaertner@googlemail.com>
2016-07-02 21:35:07 +0200

    Ad possibility to invalidate cache lines after a certain time

--- src/__tests__/test-cache.js
+++ src/__tests__/test-cache.js
@@ -3,12 +3,17 @@ jest.unmock('../cache.js');
 
 const cache = require('../cache.js');
 global.caches = {open: (cacheName) => ""};
+function Response(a) {};
+function Blob(a, b) {};
+global.Response = Response;
+global.Blob = Blob;
 
 
 describe('Cache', () => {
   let cache_name = 'lively4';
+  let age_cache_name = 'lively4-cache-line-ages';
 
-  it('matches requests', async () => {
+  it('matches requests without timeout', async () => {
     let ret = {match: (request) => ""};
     let obj = new Object(null);
 
@@ -22,6 +27,31 @@ describe('Cache', () => {
     expect(ret.match).toHaveBeenCalled();
     expect(ret.match).toHaveBeenCalledWith(obj);
   });
+
+  // it('matches requests with valid timeout', async () => {
+  //   let ret = {match: (request) => ""};
+  //   let ret_age = {text: () => ""};
+  //   let obj = new Object(null);
+
+  //   spyOn(ret, 'match');
+  //   spyOn(caches, 'open').and.returnValue(Promise.resolve(ret));
+  //   spyOn(cache, 'purge');
+  //   spyOn(cache, 'getAgeOf').and.returnValue(Promise.resolve(ret_age));
+  //   spyOn(ret_age, 'text').and.returnValue(Promise.resolve("20"));
+  //   spyOn(Date, 'now').and.returnValue(100);
+
+  //   await cache.match(obj, 100);
+
+  //   expect(ret_age.text).toHaveBeenCalled();
+  //   expect(cache.purge).not.toHaveBeenCalled();
+  //   expect(cache.getAgeOf).toHaveBeenCalled();
+  //   expect(cache.getAgeOf).toHaveBeenCalledWith(obj);
+  //   expect(caches.open).toHaveBeenCalled();
+  //   expect(caches.open).toHaveBeenCalledWith(cache_name);
+  //   expect(caches.open).toHaveBeenCalledWith(age_cache_name);
+  //   expect(ret.match).toHaveBeenCalled();
+  //   expect(ret.match).toHaveBeenCalledWith(obj);
+  // });
 
   it('purges requests', async () => {
     let ret = {delete: (request, response) => ""};
@@ -42,15 +72,40 @@ describe('Cache', () => {
     let ret = {put: (request) => ""};
     let obj = new Object(null);
     let obj2 = new Object(null);
+    let time_obj = new Object(null);
 
     spyOn(ret, 'put');
+    spyOn(Date, 'now').and.returnValue(time_obj);
     spyOn(caches, 'open').and.returnValue(Promise.resolve(ret));
+    spyOn(global, 'Response').and.callThrough();
+    spyOn(global, 'Blob').and.callThrough();
 
     await cache.put(obj, obj2);
 
+    expect(global.Blob).toHaveBeenCalled();
+    expect(global.Blob).toHaveBeenCalledWith([time_obj.toString()], {type : 'text/html'});
+    expect(global.Response).toHaveBeenCalled();
+    expect(global.Response).toHaveBeenCalledWith(new Blob());
     expect(caches.open).toHaveBeenCalled();
     expect(caches.open).toHaveBeenCalledWith(cache_name);
+    expect(caches.open).toHaveBeenCalledWith(age_cache_name);
     expect(ret.put).toHaveBeenCalled();
     expect(ret.put).toHaveBeenCalledWith(obj, obj2);
+    expect(ret.put).toHaveBeenCalledWith(obj, new Response());
+  });
+
+  it('gets ages of cached requests', async () => {
+    let ret = {match: (request) => ""};
+    let obj = new Object(null);
+
+    spyOn(ret, 'match');
+    spyOn(caches, 'open').and.returnValue(Promise.resolve(ret));
+
+    await cache.getAgeOf(obj);
+
+    expect(caches.open).toHaveBeenCalled();
+    expect(caches.open).toHaveBeenCalledWith(age_cache_name);
+    expect(ret.match).toHaveBeenCalled();
+    expect(ret.match).toHaveBeenCalledWith(obj);
   });
 });
--- src/cache.js
+++ src/cache.js
@@ -1,15 +1,34 @@
-function open() {
-  return caches.open('lively4')
+function open(cache_name) {
+  return caches.open(cache_name)
 }
 
 export function put(request, response) {
-  return open().then((cache) => cache.put(request, response))
+  let blob_text = [Date.now().toString()]
+  let blob_type = {type : 'text/html'}
+  let blob = new Blob(blob_text, blob_type)
+  let resp = new Response(blob)
+  open('lively4-cache-line-ages').then((cache) => cache.put(request, resp))
+  return open('lively4').then((cache) => cache.put(request, response))
 }
 
 export function purge(request) {
-  return open().then((cache) => cache.delete(request))
+  open('lively4-cache-line-ages').then((cache) => cache.delete(request))
+  return open('lively4').then((cache) => cache.delete(request))
 }
 
-export function match(request) {
-  return open().then((cache) => cache.match(request))
+export async function match(request, timeout=-1) {
+  if (timeout != -1) {
+    let age = await getAgeOf(request)
+    console.log(age)
+    let age_v = await age.text()
+    if (age && Date.now() - parseInt(age_v) >= timeout) {
+      purge(request)
+      return Promise.resolve(undefined)
+    }
+  }
+  return open('lively4').then((cache) => cache.match(request))
+}
+
+export function getAgeOf(request) {
+  return open('lively4-cache-line-ages').then((cache) => cache.match(request))
 }
