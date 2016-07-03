823cabce0ccbf79efcbfad2f3b2117ba838b5ebe
numberpi <daniel.stolpe@gmx.de>
2016-07-03 19:22:06 +0200

    add imdbrotten utils for wikidatasdk

--- src/client/wiki-data-adapter.js
+++ src/client/wiki-data-adapter.js
@@ -1,20 +1,52 @@
 import * as wdk from '../external/wikidata-sdk.js';
 
-export default function rotten2imdb(rottenId="m/spectre_2015") {
+export function rotten2imdb(rottenId) {
   var sparql = `
   PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 
-  SELECT ?obj ?imdb_id
+  SELECT ?imdb_id
   WHERE
   {
       ?obj wdt:P345 ?imdb_id.
-      ?obj wdt:P1258 ${rottenId}.
+      ?obj wdt:P1258 "${rottenId}".
   }
   `;
+  
+  return wikiDataQuery(sparql);
+}
+
+export function imdb2rotten(imdbId) {
+  var sparql = `
+  PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+
+  SELECT ?rotten_id
+  WHERE
+  {
+      ?obj wdt:P345 "${imdbId}".
+      ?obj wdt:P1258 ?rotten_id.
+  }
+  `;
+  return wikiDataQuery(sparql);
+}
+
+function wikiDataQuery(sparql) {
   var url = wdk.sparqlQuery(sparql);
-  $.get(url)
-  .then(wdk.simplifySparqlResults)
-  .then((simplifiedResults) => {
-    console.log(simplifiedResults);
+  return new Promise((resolve, reject) => {
+    $.get(url)
+      .then(wdk.simplifySparqlResults)
+      .then(resolve)
+      .fail(reject);
   });
 }
+
+
+
+// sample code
+/*
+
+import * as wda from 'src/client/wiki-data-adapter.js';
+
+wda.rotten2imdb("m/spectre_2015");
+wda.imdb2rotten("tt2379713");
+
+*/
