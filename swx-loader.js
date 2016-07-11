ce45c184acd1eb18fa8c022c638ff3c5e76420a3
Jan Graichen <jg@altimos.de>
2016-07-11 12:10:12 +0200

    Update loader from upstream

--- swx-loader.js
+++ swx-loader.js
@@ -48629,8 +48629,6 @@
 	
 	var _loader = __webpack_require__(5);
 	
-	function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
-	
 	const system = () => {
 	  if (!self.System) {
 	    let scope = new URL(self.registration.scope);
@@ -48649,15 +48647,9 @@
 	  return System;
 	};
 	
-	const init = (() => {
-	  var _ref = _asyncToGenerator(function* () {
-	    return system().import('/swx.js');
-	  });
-	
-	  return function init() {
-	    return _ref.apply(this, arguments);
-	  };
-	})();
+	const init = () => {
+	  return system().import('/swx.js');
+	};
 
 /***/ }
 /******/ ]);
