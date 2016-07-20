898bbe5a1ce496b34a2fef8cf8ee7e8381337da3
numberpi <daniel.stolpe@gmx.de>
2016-07-20 12:53:08 +0200

    [wip] saving triples and using GreenTurtle-graph

---
+++ src/client/rdfa/RdfaSubject.js
@@ -0,0 +1,6 @@
+export default class RdfaSubject {
+  constructor(id) {
+    this.id = id;
+    this.predicates = [];
+  }
+}
---
+++ src/client/rdfa/RdfaTriple.js
@@ -0,0 +1,7 @@
+export default class RdfaTriple {
+  constructor(subject, property, value) {
+    this.subject = subject;
+    this.property = property;
+    this.value = value;
+  }
+}
