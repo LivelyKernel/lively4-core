a95108377357818bb25208b96f73fb6f676a289b
numberpi <daniel.stolpe@gmx.de>
2016-07-21 15:29:35 +0200

    refactoring and preparations to use DocumentData from GreenTurtle

---
+++ src/client/rdfa/rdfa-api.js
@@ -0,0 +1,125 @@
+import * as rdfa from '../../external/RDFa.js';
+import generateUuid from '../uuid.js';
+import graphFactory from './rdfa-graph-factory.js';
+import Firebase from './firebase.js';
+import RdfaTriple from './RdfaTriple.js';
+
+export default class RdfaApi {
+
+  static reloadData() {
+    return new Promise((resolve, reject) => {
+      let listenerFunc = (() => {
+        lively.notify("RDFa loaded");
+        document.removeEventListener("rdfa.loaded", listenerFunc);
+        this.notifyRdfaEventListener();
+        resolve(document.data);
+      });
+      document.addEventListener("rdfa.loaded", listenerFunc, false);
+      //GreenTurtle.implementation.processors["microdata"].enabled = true;
+      GreenTurtle.attach(document);
+    });
+  }
+
+  static initializeFirebase() {
+    this.firebase = new Firebase({
+      apiKey: "AIzaSyCdiOSF0DUialcbR86BoJAmdj_RQFWgUk8",
+      authDomain: "webdev16-rdfa.firebaseapp.com",
+      databaseURL: "https://webdev16-rdfa.firebaseio.com",
+      storageBucket: "webdev16-rdfa.appspot.com",
+    });
+  }
+
+  static readDataFromFirebase(bucket) {
+    return this.firebase.database().ref("rdfaTriples/" + bucket).once('value')
+      .then((data) => {
+        let triples = [];
+        let values = data.val();
+        
+        for (let id in values) {
+          let val = values[id];
+          let triple = new RdfaTriple(val.subject, val.predicate, val.value)
+          triples.push(triple);
+        }
+        let graph = graphFactory.fromTriples(triples);
+        //TODO
+        //let documentData = GreenTurtle.implementation.createDocumentData(graph, "about:blank");
+        //documentData.merge(graph.subjects, {prefixes: graph.prefixes});
+        return graph;
+      })
+  }
+  
+  static getBucketListFromFirebase() {
+    return this.firebase.database().ref("rdfaTriples").once('value')
+    .then(data => {
+      const buckets = [];
+      data.forEach(keyObject => {
+        buckets.push(keyObject.key);
+      })
+      
+      return buckets;
+    });
+  }
+
+  static addRdfaEventListener(mappings, callback) {
+    let mappingArray = Array.isArray(mappings) ? mappings : [mappings];
+    mappingArray.forEach((mapping) => {
+      this.rdfaListener.push({mapping: mapping, callback: callback})
+    })
+  }
+
+  static notifyRdfaEventListener() {
+    this.rdfaListener.forEach((listener) => {
+      let projections = document.data.rdfa.query(listener.mapping);
+      if (projections.length > 0) {
+        //TODO
+        //listener.callback(graphFactory.fromGreenTurtleProjections(projections));
+      }
+    })
+  }
+  
+  static getRDFaTriples() {
+    let subject2uuid = {};
+    let projections = document.data.rdfa.query();
+    let triples = [];
+    document.data.getSubjects().forEach((subject) => {
+      let subjectIdentifier = subject;
+      if (!subject2uuid[subject]) {
+        if (graphFactory.isBlankNode(subject)) {
+          subjectIdentifier = '_:' + generateUuid();
+        }
+        subject2uuid[subject] = subjectIdentifier;
+      }
+    });
+    projections.forEach((projection) => {
+      let subject = projection.getSubject();
+      let properties = projection._data_.properties;
+      for (let property in properties) {
+        let values = properties[property];
+        let processedValues = values.map((value) => {
+          return subject2uuid[value] || value
+        });
+        processedValues.forEach(processedValue => {
+          triples.push(new RdfaTriple(subject2uuid[subject], property, processedValue));
+        });
+      }
+    });
+    return triples;
+  }
+  
+  static storeRDFaTriplesToFirebase(bucket, triples = this.getRDFaTriples()) {
+    let path = "rdfaTriples/" + bucket + "/";
+    let updates = {};
+    triples.forEach((triple) => {
+      let key = this.firebase.database().ref(path).push().key;
+      updates[key] = triple;
+    });
+   return this.firebase.database().ref(path).update(updates).then(() => {
+      lively.notify("Updated RDFa data", path);
+    }).catch((reason) => {
+      lively.notify("Failed to update RDFa data to " + path, reason);
+    });
+  }
+}
+
+RdfaApi.rdfaListener = [];
+RdfaApi.initializeFirebase();
---
+++ src/client/rdfa/rdfa-graph-factory.js
@@ -0,0 +1,76 @@
+import * as rdfa from '../../external/RDFa.js';
+
+export default class GraphFactory {
+  
+  static initialize() {
+    this.blankNodePattern = /^_:.+/;
+    this.uuidPattern = /\S{8}-\S{4}-4\S{3}-\S{4}-\S{12}/;
+  }
+  
+  static fromTriples(triples) {
+    const turtleParser = GreenTurtle.implementation.processors['text/turtle'].createParser();
+    const graph = turtleParser.context;
+
+    triples.forEach(triple => {
+      let subject = triple.subject;
+      let type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#PlainLiteral";
+      let value = triple.value;
+      
+      //TODO define correct type of triple
+      /*
+      if (this.isSubject(value)) {
+        type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#object";
+      }*/
+
+      const object = {type: type, value: value};
+      turtleParser.addTriple(subject, triple.predicate, object);
+    });
+
+    return graph;
+  }
+  
+  static isUuid(string) {
+    return typeof string == 'string' && string.match(this.uuidPattern);
+  }
+  
+  static isBlankNode(string) {
+    if (string && typeof string == 'string') {
+      return this.blankNodePattern.test(string);
+    }
+    return false;
+  }
+  
+  //TODO refactoring
+  static fromGreenTurtleProjections(projections) {
+    const objectGraph = {subjects: []};
+    const subjectMapping = {};
+    
+    projections.forEach((projection) => {
+      const subject = new RdfaSubject(projection._data_.subject);
+      objectGraph.subjects.push(subject);
+      const properties = projection._data_.properties;
+      for (let property in properties) {
+        const predicate = new RdfaPredicate(property, properties[property]);
+        subject.predicates.push(predicate); 
+      }
+      
+      subjectMapping[projection.getSubject()] = subject;
+    });
+    
+    objectGraph.subjects.forEach((subject) => {
+      subject.predicates.forEach((predicate) => {
+        const values = predicate.values;
+        for (let i = 0; i < values.length; i++) {
+          let value = values[i];
+          if (this.isBlankNode(value) && subjectMapping[value]) {
+            values[i] = subjectMapping[value];
+          }
+        }
+      });
+    });
+    
+    return objectGraph;
+  }
+}
+
+GraphFactory.initialize();
