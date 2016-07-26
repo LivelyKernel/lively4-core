ecf76b7b1f3b3c46b8cdd8685c9a89703d28c225
Fabio Niephaus <code@fniephaus.com>
2016-07-26 12:48:30 +0200

    Stub out ensureRemoteServicesMounted()

--- test/templates/lively-services-test.js
+++ test/templates/lively-services-test.js
@@ -39,6 +39,7 @@ describe("Services Tool",  function() {
       that = c;
       that.refreshServiceList = function() { listRefreshed = true; };
       that.refreshLog = function() { logRefreshed = true; };
+      that.ensureRemoteServicesMounted = function() {};
       done();
     });
   });
