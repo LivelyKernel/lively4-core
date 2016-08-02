ebd7505aa9525695f00be56f1103e0756d6b717a
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-02 16:16:59 +0200

    run basic test case

---
+++ test/expected/base.js
@@ -0,0 +1,10 @@
+var x;
+let y = 42;
+
+function z() {}
+
+({
+  x,
+  y,
+  z
+});
---
+++ test/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var transformFileSync = require('babel-core').transformFileSync;
+var path = require('path');
+var fs = require('fs');
+var assert = require('assert');
+
+var plugin = require('../dist/index').default;
+
+var tests = [
+    {file: 'base'},
+    {file: 'base2'},
+//    {file: 'none'},
+  //  {file: 'locals-declared'}
+];
+
+function normalize(str) {
+    return str.toString().replace(/(\r\n|\n|\r)/gm, '\n');
+}
+
+describe('transform code', function () {
+    tests.forEach(function(test){
+        it(`No preset ${test.file}`, function() {
+            var transform = normalize(transformFileSync(path.join(__dirname, `src/${test.file}.js`), {
+                plugins: [[plugin, test.options]],
+                babelrc: false // So we don't get babelrc from whole project
+            }).code);
+            var expected = normalize(fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`), {encoding: 'utf-8'}));
+
+            assert.equal(transform, expected);
+        })
+    })
+});
