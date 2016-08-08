44f641bc5f0cc415a8260a89b6b351e09c682e6e
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-08 15:16:38 +0200

    expose base active expressions

--- src/active-expressions.js
+++ src/active-expressions.js
@@ -1,2 +1,4 @@
 // TODO: expose implementation strategies
 export { aexpr } from './interpretation/interpretation-active-expressions.js';
+
+export { BaseActiveExpression } from './base/base-active-expressions.js';
