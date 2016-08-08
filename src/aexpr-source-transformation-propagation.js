1f8dbe012c3adbc68a2151124e07d1bd69159876
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-08 16:31:40 +0200

    first running implementation

--- src/aexpr-source-transformation-propagation.js
+++ src/aexpr-source-transformation-propagation.js
@@ -1,18 +1,105 @@
-export function aexpr(...args) {
-    console.log('aexpr', ...args);
-    return { onChange(cb) { console.log('onChange', cb); } }
+import { BaseActiveExpression } from 'active-expressions';
+import Stack from 'stack-es2015-modules';
+
+let expressionAnalysisMode = false;
+
+class ExpressionAnalysis {
+    // Do the function execution in ExpressionAnalysisMode
+    static check(aexpr) {
+        aexprStack.withElement(aexpr, () => {
+            // TODO: provide API for running the expression and returning its value, rather that relying on the instance property directly
+            aexpr.func();
+        });
+    }
 }
+
+// TODO: CompositeKeyStore as separate Module
+const compositeKeyStore = new Map();
+class CompositeKey {
+    static get(obj1, obj2) {
+        if(!compositeKeyStore.has(obj1)) {
+            compositeKeyStore.set(obj1, new Map());
+        }
+
+        let secondKeyMap = compositeKeyStore.get(obj1);
+
+        if(!secondKeyMap.has(obj2)) {
+            secondKeyMap.set(obj2, {});
+        }
+
+        return secondKeyMap.get(obj2);
+    }
+}
+
+class HookStorage {
+    constructor() {
+        this.objPropsByAExpr = new Map();
+    }
+
+    associate(aexpr, obj, prop) {
+        if(!this.objPropsByAExpr.has(aexpr)) {
+            this.objPropsByAExpr.set(aexpr, new Set());
+        }
+
+        let objPropSet = this.objPropsByAExpr.get(aexpr);
+
+        objPropSet.add(CompositeKey.get(obj, prop));
+        console.log('added', obj, prop);
+    }
+
+    disconnectAll(aexpr) {
+        this.objPropsByAExpr.delete(aexpr);
+    }
+
+    getAExprsFor(obj, prop) {
+        let comp = CompositeKey.get(obj, prop);
+        return Array.from(this.objPropsByAExpr.keys()).filter(aexpr => {
+            return this.objPropsByAExpr.get(aexpr).has(comp);
+        });
+    }
+}
+
+const aexprStorage = new HookStorage();
+const aexprStack = new Stack();
+
+class RewritingActiveExpression extends BaseActiveExpression {
+
+    constructor(func){
+        super(func);
+
+        ExpressionAnalysis.check(this);
+    }
+}
+
+export function aexpr(func) {
+    console.log('aexpr', func);
+    return new RewritingActiveExpression(func);
+}
+
 export function getMember(obj, prop) {
     console.log('getMember', obj, prop);
+    let currentAExpr = aexprStack.top();
+    if(currentAExpr) {
+        aexprStorage.associate(currentAExpr, obj, prop);
+    }
     return obj[prop];
 }
-export function setMember(obj, prop, operator, val) {
-    console.log('setMember', obj, prop, operator, val);
-    return obj[prop] = val;
-}
+
 export function getAndCallMember(obj, prop, args) {
     console.log('getAndCallMember', obj, prop, ...args);
+    let currentAExpr = aexprStack.top();
+    if(currentAExpr) {
+        aexprStorage.associate(currentAExpr, obj, prop);
+    }
     return obj[prop](...args);
+}
+
+export function setMember(obj, prop, operator, val) {
+    console.log('setMember', obj, prop, operator, val);
+    // TODO: check actual operator
+    let result = obj[prop] = val;
+    aexprStorage.getAExprsFor(obj, prop).forEach(aexpr => aexpr.checkAndNotify());
+    return result;
 }
 
 export default aexpr;
--- test/aexpr.spec.js
+++ test/aexpr.spec.js
@@ -1,23 +1,20 @@
 'use strict';
 
-import {} from '../src/aexpr-source-transformation-propagation.js';
+import { aexpr, setMember, getMember, getAndCallMember } from '../src/aexpr-source-transformation-propagation.js';
 
-describe('Sinon', function() {
-
-    it('should be supported', () => {
-        var spy = sinon.spy();
-
-        spy(42);
-
-        expect(spy.withArgs(42).calledOnce).to.be.true;
-    });
+describe('Propagation Logic', function() {
 
     it('should be supported with proper integration', () => {
-        var spy = sinon.spy();
+        let obj = { prop: 42 },
+            spy = sinon.spy();
 
-        spy(42);
+        aexpr(() => getMember(obj, "prop")).onChange(spy);
 
-        expect(spy).to.be.calledWith(42);
+        expect(spy).not.to.be.called;
+
+        setMember(obj, "prop", "=", 17);
+
+        expect(spy).to.be.calledOnce;
     });
 
 });
