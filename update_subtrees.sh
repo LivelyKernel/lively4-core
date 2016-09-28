6ef2a8c65ac8d980ae13abfe3fdc9108ab8fc6db
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-09-28 17:21:27 +0200

    SYNC  M start.html; M update_subtrees.sh;

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -1,2 +1,3 @@
 git subtree pull -P src/external/lively.graphics https://github.com/LivelyKernel/lively.graphics.git master
 git subtree pull -P src/external/lively.lang https://github.com/LivelyKernel/lively.lang.git master
+git subtree pull -P src/external/active-expressions https://github.com/LivelyKernel/active-expressions.git master
