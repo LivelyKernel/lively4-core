7dbd73402373195faf547c63a1978884eedf6eb6
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-09-28 17:43:32 +0200

    added a script for pushing subtrees

--- push_subtrees.sh
+++ push_subtrees.sh
@@ -1,3 +1,5 @@
+## #TODO this should go into the lively4-server (or something)
+
 # git subtree push -P src/external/lively.graphics https://github.com/LivelyKernel/lively.graphics.git master
 # git subtree push -P src/external/lively.lang https://github.com/LivelyKernel/lively.lang.git master
 git subtree push -P src/external/active-expressions https://github.com/LivelyKernel/active-expressions.git master
