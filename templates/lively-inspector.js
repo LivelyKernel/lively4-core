8bd73c4f7ccd85445ff9b59bc34351036866f53b
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-10-28 17:18:35 +0200

    [inspector] some cleanup

--- templates/lively-inspector.js
+++ templates/lively-inspector.js
@@ -14,8 +14,8 @@ export default class Inspector   extends Morph {
 
   displayFunction(value) {
     var node = document.createElement("div");
-    node.classList.add("element")
-    node.innerHTML = this.expandTemplate(node) + " function " + value.name + "("
+    node.classList.add("element");
+    node.innerHTML = this.expandTemplate(node) + " function " + value.name + "(";
     // node.innerHTML = "<pre>" + value + "</pre>";
     return node;
   }
@@ -25,22 +25,22 @@ export default class Inspector   extends Morph {
    */
   onSelect(node, obj) {
     if (this.selectedNode)
-      this.selectedNode.classList.remove("selected")
-    this.selectedNode  = node
-    this.selectedNode.classList.add("selected")
+      this.selectedNode.classList.remove("selected");
+    this.selectedNode  = node;
+    this.selectedNode.classList.add("selected");
   
     this.selection = obj;
-    lively.showElement(obj)
-    window.that = obj // #Experimental
+    lively.showElement(obj);
+    window.that = obj; // #Experimental
     this.get("#editor").doitContext = obj;
   }
   
   renderObject(node, obj, expanded, name) {
-    node.isExpanded = expanded
+    node.isExpanded = expanded;
     if (!name) {
-      name = ""
+      name = "";
     }
-    var className = obj.constructor.name
+    var className = obj.constructor.name;
     
     node.innerHTML = `${this.expandTemplate(node)}`+
       ` <a id='tagname' class='tagname'>${name} ${className}</a> `+
@@ -48,7 +48,7 @@ export default class Inspector   extends Morph {
        this.contentTemplate +
       '<span class="syntax">}'+"</span>";
   
-    this.attachHandlers(node, obj)
+    this.attachHandlers(node, obj);
     
     var contentNode = node.querySelector("#content");
     if (node.isExpanded) {
@@ -61,41 +61,40 @@ export default class Inspector   extends Morph {
   
   displayObject(obj, expand, name) {
     var node = document.createElement("div");
-    node.classList.add("element")
-    this.renderObject(node, obj, expand, name)
+    node.classList.add("element");
+    this.renderObject(node, obj, expand, name);
     return node;
   }
   
   changeAttributeValue(obj, attrName, attrValue) {
-    obj.setAttribute(attrName, attrValue)
-    lively.notify("edited " + attrName + " to " + attrValue)
+    obj.setAttribute(attrName, attrValue);
+    lively.notify("edited " + attrName + " to " + attrValue);
   }
   
   expandTemplate(node) {
     return "<span class='syntax'><a id='expand'><span style='font-size:9'>" +
-      (node.isExpanded ? "&#9654" : "&#9660;") + "</span></a></span>"
+      (node.isExpanded ? "&#9660;" : "&#9654") + "</span></a></span>";
   }
   
   get contentTemplate() {
-    return "<span id='content'><a id='more' class='more'>...</a></span>"
+    return "<span id='content'><a id='more' class='more'>...</a></span>";
   }
 
   get quoteTemplate() {
-    return `<span class='syntax'>"</span>`
+    return `<span class='syntax'>"</span>`;
   }
   
-
   attachHandlers(node, obj) {
     // jqyery would make this cleaner here...
-    var moreNode = node.querySelector("#more")
+    var moreNode = node.querySelector("#more");
     if (moreNode) moreNode.onclick = (evt) => {
       this.render(node, obj, true);
     };
-    var expandNode = node.querySelector("#expand")
+    var expandNode = node.querySelector("#expand");
     if (expandNode) expandNode.onclick = (evt) => {
       this.render(node, obj, !node.isExpanded);
     };
-    var tagNode = node.querySelector("#tagname")
+    var tagNode = node.querySelector("#tagname");
     if (tagNode) tagNode.onclick = (evt) => {
       this.onSelect(node, obj);
     };
@@ -103,26 +102,26 @@ export default class Inspector   extends Morph {
   
   renderAttributes(node, obj) {
     if (obj.attributes) {
-      var attrNode = node.querySelector("#attributes")
+      var attrNode = node.querySelector("#attributes");
       lively.array(obj.attributes).forEach(ea => {
-        var eaNode = document.createElement("span")
-        eaNode.innerHTML = ` <span class='attrName'>${ea.name}=</span>${this.quoteTemplate}<span class="attrValue">${ea.value}</span>${this.quoteTemplate}`
-        var valueNode = eaNode.querySelector(".attrValue") 
+        var eaNode = document.createElement("span");
+        eaNode.innerHTML = ` <span class='attrName'>${ea.name}=</span>${this.quoteTemplate}<span class="attrValue">${ea.value}</span>${this.quoteTemplate}`;
+        var valueNode = eaNode.querySelector(".attrValue") ;
         // Editing of attribute values in inspector
         valueNode.onclick = evt => {
-          eaNode.querySelector(".attrValue").contentEditable = true
-          return true
-        }
+          eaNode.querySelector(".attrValue").contentEditable = true;
+          return true;
+        };
         // accept changes in content editable attribute value
         valueNode.onkeydown = evt => {
           if(evt.keyCode == 13) { // on enter -> like in input fields
-           valueNode.contentEditable = false
-            this.changeAttributeValue(obj, ea.name, valueNode.textContent)
+           valueNode.contentEditable = false;
+            this.changeAttributeValue(obj, ea.name, valueNode.textContent);
             evt.preventDefault();
           }
-        }
-        attrNode.appendChild(eaNode)
-      })
+        };
+        attrNode.appendChild(eaNode);
+      });
     }
   }
 
@@ -135,20 +134,20 @@ export default class Inspector   extends Morph {
     if (obj instanceof Comment) {
       tagName = "comment";
     }
-    var lt = "<span class='syntax'>&lt;</span>"
-    var gt = "<span class='syntax'>&gt;</span>"
+    var lt = "<span class='syntax'>&lt;</span>";
+    var gt = "<span class='syntax'>&gt;</span>";
     node.innerHTML = this.expandTemplate(node)  + `${lt}`+
       `<a id='tagname' class='tagname'>${tagName.toLowerCase()}</a>`+
       `<span id='attributes'></span>${gt}` + this.contentTemplate +
       `${lt}<span class='tagname'>${tagName.toLowerCase()}</span>${gt}`;
     if (tagName == "shadowroot") {
       node.innerHTML = this.expandTemplate(node) + "<a id='tagname' class='tagname'>#shadow-root</a>" +
-        this.contentTemplate 
+        this.contentTemplate; 
     }
     
     if (tagName == "comment") {
       node.innerHTML = "<a id='tagname' class='tagname'>&lt!-- </a>" + 
-        this.contentTemplate +" --&gt" 
+        this.contentTemplate +" --&gt" ;
     }
     // if (!node.isAutoExpanded) html = this.expandTemplate + html
   }
@@ -162,19 +161,19 @@ export default class Inspector   extends Morph {
     }
 
     if (expanded === false) {
-      node.isExpanded = false // force colapse 
+      node.isExpanded = false; // force colapse 
     }
     
-    this.renderHeader(node, obj)
-    this.renderAttributes(node, obj)
+    this.renderHeader(node, obj);
+    this.renderAttributes(node, obj);
 
     var contentNode = node.querySelector("#content");
-    this.attachHandlers(node, obj)
+    this.attachHandlers(node, obj);
 
     if (node.isExpanded) {
       contentNode.innerHTML = "";
       if (obj instanceof Comment) {
-        contentNode.innerHTML = obj.textContent
+        contentNode.innerHTML = obj.textContent.replace(/</,"&lt;").replace(/>/,"&gt;");
       }
       if (obj.shadowRoot) {
         contentNode.appendChild(this.display(obj.shadowRoot))  ;
@@ -188,7 +187,7 @@ export default class Inspector   extends Morph {
   
   renderText(node, obj, expanded) {
     if (obj.textContent.match(/^[ \n]*$/)) 
-      return // nothing to render here... skip, empty lines or just spaces
+      return; // nothing to render here... skip, empty lines or just spaces
     if (obj.textContent.length > 100)
       node.innerHTML = "<pre>" +  obj.textContent + "</pre>";
     else {
@@ -200,7 +199,7 @@ export default class Inspector   extends Morph {
     node = document.createElement("span");
     node.setAttribute("class","element");
     this.render(node, obj, expanded); 
-    return node
+    return node;
   }
     
   displayNode(obj, expanded) {
@@ -219,13 +218,13 @@ export default class Inspector   extends Morph {
       node = document.createElement("span");
       node.setAttribute("class","element");
     }
-    node.target = obj
+    node.target = obj;
     this.render(node, obj, expanded); 
     return node;
   }
   
   isNode(obj) {
-    return obj instanceof HTMLElement || obj instanceof ShadowRoot || obj instanceof Comment || obj instanceof Text
+    return obj instanceof HTMLElement || obj instanceof ShadowRoot || obj instanceof Comment || obj instanceof Text;
   }
   
   render(node, obj, expanded) {
