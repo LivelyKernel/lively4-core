bf73d00724cab2aa97ea41d6b771f01602b395e2
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-11-03 16:28:53 +0100

    disable index-based search widget

--- src/client/load.js
+++ src/client/load.js
@@ -44,8 +44,10 @@ if ('serviceWorker' in navigator || window.lively4chrome) {
         return lively.modules.importPackage(lively4url)})
       .then(function(module) {
         lively.initializeHalos();
-        if (!window.lively4chrome)
-          lively.initializeSearch(); // disable search widget in chrome extension setting
+        
+        // disable search widget for now
+        // if (!window.lively4chrome)
+        //   lively.initializeSearch(); // disable search widget in chrome extension setting
         lively.components.loadUnresolved();
         console.log("running on load callbacks:");
         loadCallbacks.forEach(function(cb){
@@ -99,39 +101,39 @@ if ('serviceWorker' in navigator || window.lively4chrome) {
         event.ports[0].postMessage({error: err})
     }
 
+    // support for service worker local filesytem
     switch(event.data.name) {
       case 'swx:readFile':
-          fs.then((fs) => {
-              fs.root.getFile(event.data.file, undefined, function(fileEntry) {
-                  fileEntry.file(function(file) {
-                      var reader = new FileReader()
-                      reader.onloadend = function(e) {
-                          console.log('[LFS] read complete', e)
-                          event.ports[0].postMessage({content: reader.result})
-                      }
-                      reader.readAsText(file)
-                  })
-              }, reject)
-          }).catch(reject)
-          break
-        case 'swx:writeFile':
-          fs.then((fs) => {
-              fs.root.getFile(event.data.file, {create: true, exclusive: false}, function(file) {
-                  file.createWriter(function (writer) {
-                      writer.onwriteend = function(e) {
-                          console.log("[LFS] write complete", e)
+        fs.then((fs) => {
+            fs.root.getFile(event.data.file, undefined, function(fileEntry) {
+                fileEntry.file(function(file) {
+                    var reader = new FileReader()
+                    reader.onloadend = function(e) {
+                        console.log('[LFS] read complete', e)
+                        event.ports[0].postMessage({content: reader.result})
+                    }
+                    reader.readAsText(file)
+                })
+            }, reject)
+        }).catch(reject)
+        break
+      case 'swx:writeFile':
+        fs.then((fs) => {
+            fs.root.getFile(event.data.file, {create: true, exclusive: false}, function(file) {
+                file.createWriter(function (writer) {
+                  writer.onwriteend = function(e) {
+                      console.log("[LFS] write complete", e)
 
-                          event.ports[0].postMessage({
-                              content: event.data.content
-                          })
-                      }
-                      writer.onerror = reject
-                      writer.write(new Blob([event.data.content], {type: 'text/plain'}))
-                  }, reject)
-              }, reject)
-          }).catch(reject)
-
-          break
+                      event.ports[0].postMessage({
+                          content: event.data.content
+                      })
+                  }
+                  writer.onerror = reject
+                  writer.write(new Blob([event.data.content], {type: 'text/plain'}))
+                }, reject)
+            }, reject)
+        }).catch(reject)
+        break
     }
   })
 }
