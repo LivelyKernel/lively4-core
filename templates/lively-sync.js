d4890e8e72004532009d88f0f1ba6947c5d03b78
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-11-09 23:18:13 +0100

    AUTO-COMMIT templates/lively-sync.js

--- templates/lively-sync.js
+++ templates/lively-sync.js
@@ -144,7 +144,7 @@ export default class Sync extends Morph {
   onSyncButton() {
     this.gitControl("status", (status) => {
       if (!status.match("AUTO-COMMIT-")) {
-        this.sync()  
+        // this.sync()  
       } else {
         if (window.confirm("Contains auto commits")) {
           lively.notify("haha")
