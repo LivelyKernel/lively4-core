0efb3cac60664e5aab88b9a8f85470685f57bb4c
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-11-10 17:06:16 +0100

    [demos] working on local system.import
    SQUASHED: AUTO-COMMIT-demos-example.html,AUTO-COMMIT-demos-layers.js,AUTO-COMMIT-demos-mymodule.js,

---
+++ demos/layers.js
@@ -0,0 +1,14 @@
+import * as cop from 'src/external/ContextJS.js'
+
+cop.create(window, "LivelySystemLayer").refineObject(System, {
+	
+	import(name, parentName, parentAddress) {
+		name = name.replace(/^.\//,"https://lively-kernel.org/lively4/lively4-jens/demos/")
+		
+		lively.notify("import "+ name + ", " + parentName +","+ parentAddress)
+		return cop.proceed(name, parentName, parentAddress)	
+	}
+})
+
+
+// LivelySystemLayer.beGlobal()
