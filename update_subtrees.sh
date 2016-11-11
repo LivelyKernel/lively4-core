98f857b0d5b07d4d87469cc4d559a2696d38cdfd
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2016-11-11 15:01:44 +0100

    AUTO-COMMIT update_subtrees.sh

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -2,3 +2,4 @@ git subtree pull -P src/external/lively.graphics https://github.com/LivelyKernel
 git subtree pull -P src/external/lively.lang https://github.com/LivelyKernel/lively.lang.git master
 git subtree pull -P src/external/active-expressions https://github.com/LivelyKernel/active-expressions.git master
 git subtree pull -P src/external/lively4-serviceworker https://github.com/LivelyKernel/lively4-serviceworker master
+git subtree pull -P src/external/plugin-babel https://github.com/systemjs/plugin-babel.git master
