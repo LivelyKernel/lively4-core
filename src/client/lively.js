a6d82b9cf395e178385ae00bb8c5d5342779accb
null <null>
2016-12-09 15:32:29 +0100

    persistent workspaces by default AND editor demo
    SQUASHED: AUTO-COMMIT-demos-editors.html,AUTO-COMMIT-demos-hello.html,AUTO-COMMIT-demos-workspace.html,AUTO-COMMIT-src-client-lively.js,

--- src/client/lively.js
+++ src/client/lively.js
@@ -296,6 +296,7 @@ export default class Lively {
       pos = pos || lively.pt(100,100);
       comp.changeMode("javascript");
       comp.enableAutocompletion();
+      comp.setAttribute("persistent", "true");
       comp.editor.setValue(string);
       lively.setPosition(container,pos);
       container.setAttribute("title", "Workspace");
