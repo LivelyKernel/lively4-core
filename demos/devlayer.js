3a996ce01a27e3a622f0de9a25b3e12f717d17b1
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-01-16 14:22:28 +0100

    AUTO-COMMIT demos/devlayer.js

--- demos/devlayer.js
+++ demos/devlayer.js
@@ -1,6 +1,8 @@
 import * as cop  from "src/external/ContextJS/src/contextjs.js";
 import aceEditor from 'templates/juicy-ace-editor.js'
 
+
+// fall back to simple eval while we are working on the real code....
 cop.layer(window, "DevLayer").refineClass(aceEditor, {
 
  async boundEval(str, context) {
