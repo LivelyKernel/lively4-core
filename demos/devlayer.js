636f76ce16221c25f77ae7a68c0d166d8c1ac2a1
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-01-16 14:16:32 +0100

    AUTO-COMMIT demos/devlayer.js

---
+++ demos/devlayer.js
@@ -0,0 +1,30 @@
+import * as cop  from "src/external/ContextJS/src/contextjs.js";
+import aceEditor from 'templates/juicy-ace-editor.js'
+
+cop.layer("DevLayer").refineClass(aceEditor, {
+
+ async boundEval(str, context) {
+    // using lively vm:
+    // return lively.vm.runEval(str, {targetModule: this.getTargetModule(), context: context})
+    
+    // src, topLevelVariables, thisReference, <- finalStatement
+    
+    try {
+      console.log('eval with context', context);
+      let id = generateUUID();
+      setCode(id, str);
+      return System.import('workspace:' + id);
+    } catch(err) {
+      return Promise.resolve({ value: err, isError: true });
+    }
+    
+    /*
+    return new Promise((resolve) => {
+      var result = (function() {
+      	return eval(str)
+      }).bind(context)()
+      resolve({value: result})
+    })
+    */
+  }
+})
