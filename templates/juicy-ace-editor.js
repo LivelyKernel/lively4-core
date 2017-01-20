c6ffce6f130c1ffee636a93687245b8c9d7be078
onsetsu <stefan.ramson@gmail.com>
2017-01-20 23:23:40 +0100

    AUTO-COMMIT templates/juicy-ace-editor.js

--- templates/juicy-ace-editor.js
+++ templates/juicy-ace-editor.js
@@ -330,7 +330,7 @@ export default class AceEditor extends HTMLElement {
     try {
       console.log('eval with context', context);
       window.__global_this__ = this.getDoitContext();
-      this.__id__  = this.__id__ || generateUUID().replace('-', '_');
+      this.__id__  = this.__id__ || generateUUID().replace(/-/g, '_');
       console.log('id:', this.__id__);
       let id = this.__id__;
       window.__topLevelVarRecorder_ModuleName__ = id;
