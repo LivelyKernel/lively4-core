2d07959749bb50f30f6ec9702fd17183f833b208
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-01-25 15:01:47 +0100

    dev layer example
    SQUASHED: AUTO-COMMIT-demos-copevents.js,AUTO-COMMIT-templates-turtle-canvas.html,

---
+++ demos/copevents.js
@@ -0,0 +1,17 @@
+// Intercepts events with ContextJS
+
+import * as cop from "src/external/ContextJS/src/contextjs.js"
+
+cop.layer(window, "DebugEventsLayer").refineClass(HTMLElement, {
+	
+	addEventListener(eventName, cb, options) {
+		return cop.proceed.call(this, eventName, (e) => {
+			var msg = "DEBUG" + this + 'add event ' + eventName
+			lively.showElement(this, 1).textContent = msg
+			cb(e)
+		}, options)
+	}	
+})
+
+
+DebugEventsLayer.beNotGlobal()
