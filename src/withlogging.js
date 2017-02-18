f45fe5e153ea2af9920512054ca4eff903bec656
Stefan Lehmann <stefan.lehmann@hpi.de>
2017-02-18 19:36:34 +0100

    applied same refactoring as in plain repository: __livingSet__ to _instances_

--- src/withlogging.js
+++ src/withlogging.js
@@ -4,14 +4,14 @@ import View from './view.js';
 export default function withLogging() {
   withAdvice.call(this.prototype);
   var Class = this;
-  Class.__livingSet__ = new View();
+  Class._instances_ = new View();
 
   this.prototype.after('initialize', function() {
     console.log('Created', this);
-    Class.__livingSet__.safeAdd(this);
+    Class._instances_.safeAdd(this);
   });
   this.prototype.before('destroy', function() {
     console.log('Destroy', this);
-    Class.__livingSet__.safeRemove(this);
+    Class._instances_.safeRemove(this);
   });
 }
