cdaac85464adcb924aff8f24011c68e7ae2a6e97
null <null>
2017-05-18 23:57:38 +0200

    started spike for triple notes
    SQUASHED: AUTO-COMMIT-src-client-triples-triples.js,AUTO-COMMIT-templates-triple-notes.html,AUTO-COMMIT-templates-triple-notes.js,

--- src/client/triples/triples.js
+++ src/client/triples/triples.js
@@ -1,9 +1,46 @@
 
 
-
-export default async function loadDropbox() {
-  let text = await lively.files.statFile("https://lively4/dropbox/")
-  let json = JSON.parse(text);
-  return json;
+class Knot {
+  
+  
+  
+  static localFromResponse(response) {
+    
+  }
 }
 
+class Triple extends Knot {
+  
+}
+
+class Graph {
+ 
+}
+
+// Have to be transparent
+class LateBoundReference {
+  constructor(url) {
+    
+  }
+  
+  get() {
+    
+  }
+}
+
+export default async function loadDropbox(directory) {
+  let directoryURL = new URL(directory);
+  let text = await lively.files.statFile(directory);
+  lively.notify(text);
+  let json = JSON.parse(text);
+  let fileDescriptors = json.contents;
+  fileDescriptors = fileDescriptors.filter(desc => desc.type === "file");
+  let fileNames = fileDescriptors.map(desc => desc.name);
+  let knots = await Promise.all(fileNames.map(fileName => {
+    let path = new URL(fileName, directoryURL);
+    return fetch(path)
+      .then(response => response.text())
+      .then(text => ({ text, fileName }));
+  }));
+  return knots;
+}
--- templates/triple-notes.js
+++ templates/triple-notes.js
@@ -1,114 +1,168 @@
-
 import Morph from "./Morph.js"
+
 import d3 from 'src/external/d3.v4.js';
+
+import loadDropbox from 'src/client/triples/triples.js';
+
+//const MIN_MAGNIFICATION = 0.01;
+//const MAX_MAGNIFICATION = 4;
+
+class Node {
+  constructor(label) {
+    this._label = label
+    this.r = ~~d3.randomUniform(8, 28)();
+  }
+  label() {
+    return this._label;
+  }
+}
+class Link {}
 
 export default class TripleNotes extends Morph {
 
   initialize() {
-    this.windowTitle = "Triple Notes"
-var width,height
-    var chartWidth, chartHeight
-    var margin
-    var svg = d3.select(this.get('#graph')).append("svg");
-    var chartLayer = svg.append("g").classed("chartLayer", true)
+    this.windowTitle = "Triple Notes";
     
+    var width,height;
+    var chartWidth, chartHeight;
+    var margin;
+    var svg = d3.select(this.get('#graph'))
+      .append("svg");
+    var chartLayer = svg;//.append("g").classed("chartLayer", true);
+
     main.call(this);
+			
+    function getGraph() {
+      let king1 = { label: 'Arthur' };
+      let king2 = { label: 'Bob' };
+      let king3 = { label: 'Ceasar' };
+      let successor = { label: 'successor' };
+      let successor2 = { label: 'successor2' };
+      let successor3 = { label: 'successor3' };
+      let since = { label: 'since' };
+      let t1 = { isTriple: true, s: king2, p: successor, o: king1}
+      let t2 = { isTriple: true, s: king3, p: successor, o: king2}
+      let t3 = { isTriple: true, s: king3, p: successor2, o: king2}
+      let t4 = { isTriple: true, s: king3, p: successor3, o: king2}
+      return [
+        king1, king2, king3, successor, since, t1, t2, t3, t4
+      ];
+    }
     
-    function main() {
-        var range = 100
+    async function main() {
+        let //knots = await loadDropbox("https://lively4/dropbox/");
+        
+        knots = getGraph();
+        let nodes = knots.map(knot => knot );
+        let links = [];
+        knots
+          .filter(knot => knot.isTriple)
+          .forEach(triple => {
+            links.push({
+              source: triple.s,
+              target: triple
+            });
+            links.push({
+              source: triple,
+              target: triple.o
+            });
+          });
+          //:d3.range(0, range).map(function(){ return {source:~~d3.randomUniform(range)(), target:~~d3.randomUniform(range)()} })
         var data = {
-            nodes:d3.range(0, range).map(function(d){ return {label: "l"+d ,r:~~d3.randomUniform(8, 28)()}}),
-            links:d3.range(0, range).map(function(){ return {source:~~d3.randomUniform(range)(), target:~~d3.randomUniform(range)()} })        
+            nodes,
+            links        
         }
         
-        setSize.call(this, data)
-        drawChart(data)
+        setSize.call(this, data);
+        drawChart(data);
     }
     
     function setSize(data) {
-        width = this.get("#graph").clientWidth
-        height = this.get("#graph").clientHeight
-        margin = {top:0, left:0, bottom:0, right:0 }
+        width = this.get("#graph").clientWidth;
+        height = this.get("#graph").clientHeight;
+        margin = {top:0, left:0, bottom:0, right:0 };
         
         
         chartWidth = width - (margin.left+margin.right)
         chartHeight = height - (margin.top+margin.bottom)
         
-        svg.attr("width", width).attr("height", height)
+        //chartLayer.attr("width", width).attr("height", height)
         
         chartLayer
             .attr("width", chartWidth)
             .attr("height", chartHeight)
+            //.call(zoom)
             .attr("transform", "translate("+[margin.left, margin.top]+")")
     }
     
     function drawChart(data) {
         
         var simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function(d) { return d.index }))
-            .force("collide",d3.forceCollide( function(d){return d.r + 8 }).iterations(16) )
+            .force("link", d3.forceLink().id(d => d.index))
+            .force("collide",d3.forceCollide(d => d.r + 8).iterations(16) )
             .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter(chartWidth / 2, chartWidth / 2))
             .force("y", d3.forceY(0))
             .force("x", d3.forceX(0))
     
-        var link = svg.append("g")
-            .attr("class", "links")
-            .selectAll("line")
-            .data(data.links)
-            .enter()
+        let linkContainer = chartLayer.append("g").classed("linkContainer", true);
+        var link = linkContainer.selectAll("line")
+            .data(data.links).enter()
             .append("line")
             .attr("stroke", "black")
         
-        var node = svg.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle")
-            .data(data.nodes)
-            .enter().append("circle")
-            .attr("r", function(d){  return d.r })
-            .call(d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended));    
-        
-        
-        var ticked = function() {
+        let nodeContainer = chartLayer.append("g").classed("nodeContainer", true);
+        let node = nodeContainer.selectAll(".node")
+          .data(data.nodes).enter()
+          .append("g")
+          .attr("class", "node")
+          .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+        node.append("circle")
+          .attr("r", d => 10); // d.r
+
+        node.append("text")
+          .attr("dx", 12)
+          .attr("dy", ".35em")
+          .text(d => d.label || d.p && 'TRIPLE' + d.p.label);
+
+        function recalculatePositions() {
+            node.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+
             link
-                .attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
-    
-            node
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
         }  
         
         simulation
             .nodes(data.nodes)
-            .on("tick", ticked);
+            .on("tick", recalculatePositions);
     
         simulation.force("link")
             .links(data.links);    
         
-        
-        
         function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
+          if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
         }
         
         function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
         }
         
         function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
+          if (!d3.event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
         }
+        
     }
   }
 }
