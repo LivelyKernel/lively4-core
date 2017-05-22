98a313e5293032f47c9fbc37ff92fdf45419902b
onsetsu <stefan.ramson@gmail.com>
2017-05-22 09:30:01 +0200

    first visible triple graph
    SQUASHED: AUTO-COMMIT-src-client-triples-drawTools.js,AUTO-COMMIT-src-client-triples-triples.js,AUTO-COMMIT-templates-triple-notes.html,AUTO-COMMIT-templates-triple-notes.js,

---
+++ src/client/triples/drawTools.js
@@ -0,0 +1,144 @@
+/**
+ * Append a circular class node with the passed attributes.
+ * @param parent the parent element to which the circle will be appended
+ * @param radius
+ * @param [cssClasses] an array of additional css classes
+ * @param [tooltip]
+ * @param [backgroundColor]
+ * @returns {*}
+ */
+export function appendCircularClass (parent, radius, cssClasses, tooltip, backgroundColor) {
+	var circle = parent.append("circle")
+		.classed("class", true)
+		.attr("r", radius);
+
+	addCssClasses(circle, cssClasses);
+	addToolTip(circle, tooltip);
+	addBackgroundColor(circle, backgroundColor);
+
+	return circle;
+};
+
+function addCssClasses(element, cssClasses) {
+	if (cssClasses instanceof Array) {
+		cssClasses.forEach(function (cssClass) {
+			element.classed(cssClass, true);
+		});
+	}
+}
+
+function addToolTip(element, tooltip) {
+	if (tooltip) {
+		element.append("title").text(tooltip);
+	}
+}
+
+function addBackgroundColor(element, backgroundColor) {
+	if (backgroundColor) {
+		element.style("fill", backgroundColor);
+	}
+}
+
+/**
+ * Appends a rectangular class node with the passed attributes.
+ * @param parent the parent element to which the rectangle will be appended
+ * @param width
+ * @param height
+ * @param [cssClasses] an array of additional css classes
+ * @param [tooltip]
+ * @param [backgroundColor]
+ * @returns {*}
+ */
+export function appendRectangularClass (parent, width, height, cssClasses, tooltip, backgroundColor) {
+	var rectangle = parent.append("rect")
+		.classed("class", true)
+		.attr("x", -width / 2)
+		.attr("y", -height / 2)
+		.attr("width", width)
+		.attr("height", height);
+
+	addCssClasses(rectangle, cssClasses);
+	addToolTip(rectangle, tooltip);
+	addBackgroundColor(rectangle, backgroundColor);
+
+	return rectangle;
+};
+
+export function drawPin(container, dx, dy, onClick) {
+	var pinGroupElement = container
+		.append("g")
+		.classed("hidden-in-export", true)
+		.attr("transform", "translate(" + dx + "," + dy + ")");
+
+	pinGroupElement.append("circle")
+		.classed("class pin feature", true)
+		.attr("r", 12)
+		.on("click", function () {
+			if (onClick) {
+				onClick();
+			}
+			d3.event.stopPropagation();
+		});
+
+	pinGroupElement.append("line")
+		.attr("x1", 0)
+		.attr("x2", 0)
+		.attr("y1", 12)
+		.attr("y2", 16);
+
+	return pinGroupElement;
+};
+
+export function drawRectHalo (node, width, height, offset) {
+	var container;
+	if (node.nodeElement)
+		container=node.nodeElement();
+	else
+	  container=node.labelElement();
+
+	if (!container){
+		console.log("no container found");
+		return;
+	}
+
+	var haloGroupElement = container
+		.append("g")
+		.classed("hidden-in-export", true);
+
+	if (node.inverse && node.inverse()){
+		var addHeight=node.inverse().height();
+		haloGroupElement.append("rect")
+			.classed("searchResultA", true)
+			.attr("x", (-width - offset) / 2)
+			.attr("y", (-offset - height) / 2)
+			.attr("width", width + offset)
+			.attr("height", height+ addHeight + offset);
+	}
+	else {
+		haloGroupElement.append("rect")
+			.classed("searchResultA", true)
+			.attr("x", (-width - offset) / 2)
+			.attr("y", (-offset - height) / 2)
+			.attr("width", width + offset)
+			.attr("height", height + offset);
+	}
+	return haloGroupElement;
+
+};
+export function drawHalo (container, radius) {
+	if (container===undefined){
+		return null;
+		// there is no element to add the halo to;
+		// this means the node was not rendered previously
+	}
+
+	var haloGroupElement = container
+		.append("g")
+		.classed("hidden-in-export", true);
+
+	haloGroupElement.append("circle",":first-child")
+		.classed("searchResultA", true)
+		.attr("r", radius + 15);
+	return haloGroupElement;
+};
+
