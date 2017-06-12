3141e1ef8652eedc1cf5d00e95283e44dd221824
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-06-12 10:58:41 +0200

    clean up old student projects

--- bin/env.js
+++
@@ -1,7 +0,0 @@
-export default async function env(env, args) {
-    for(let key in env.ENV) {
-        env.fd[1].emit('data', key.toString() + '=' + env.ENV[key].toString() + '\r\n')
-    }
-
-    return 0
-}
