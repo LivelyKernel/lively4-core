c9acd491503a36b055e914e350430db590d589df
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-06-14 11:15:37 +0200

    cleanup

--- add_subtree.sh
+++
@@ -1,2 +0,0 @@
-# #TODO refactor this into lively4-server
-git subtree add -P src/external/lively4-search https://github.com/LivelyKernel/lively4-search.git master
--- update_subtrees.sh
+++
@@ -1,7 +0,0 @@
-
-# #TODO make subtrees part of package conf...? And move scripts into tools such as lively-sync and the lively4-server...
-git subtree pull -P src/external/lively.graphics https://github.com/LivelyKernel/lively.graphics.git master
-git subtree pull -P src/external/lively.lang https://github.com/LivelyKernel/lively.lang.git master
-git subtree pull -P src/external/active-expressions https://github.com/LivelyKernel/active-expressions.git master
-git subtree pull -P src/external/lively4-serviceworker https://github.com/LivelyKernel/lively4-serviceworker master
-git subtree pull -P src/external/plugin-babel https://github.com/systemjs/plugin-babel.git master
