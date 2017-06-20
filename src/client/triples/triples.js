9010790c20dc77e0f4f1f90a288c3e33592c31dd
onsetsu <stefan.ramson@gmail.com>
2017-06-20 14:35:06 +0200

    AUTO-COMMIT src/client/triples/triples.js

--- src/client/triples/triples.js
+++ src/client/triples/triples.js
@@ -1,5 +1,5 @@
 import focalStorage from 'src/external/focalStorage.js';
-import uuid from '../uuid.js';
+//import uuid from '../uuid.js';
 
 const STORAGE_PREFIX = 'triple-notes:';
 const STORAGE_PREFIX_ITEMS = STORAGE_PREFIX + 'items:';
