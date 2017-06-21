b8ea6b6fe8dd006276235c90854d1e7b0c31dc02
onsetsu <stefan.ramson@gmail.com>
2017-06-21 19:52:07 +0200

    AUTO-COMMIT templates/knot-input.js

--- templates/knot-input.js
+++ templates/knot-input.js
@@ -1,7 +1,51 @@
 import Morph from './Morph.js';
 
 export default class KnotInput extends Morph {
+  async initialize() {
+    this.windowTitle = "Knot Input";
+
+    this.spo.forEach(({ input }) => this.get(input).addEventListener('keyup',  event => {
+      if (event.keyCode == 13) { // ENTER
+        this.save();
+      }
+    }));
+    
+    this.spo.forEach(({ list }) => this.prepareDatalist(list));
+  }
+  async prepareDatalist(listSelector) {
+    let graph = Graph.getInstance();
+    await graph.loadFromDir('https://lively4/dropbox/');
+
+    let list = this.get(listSelector);
+    graph.getKnots().forEach(knot => {
+      let option = document.createElement('option');
+      
+      option.innerHTML = knot.label();
+      option.value = knot.url;
+      option.dataset.url = knot.url;
+      
+      list.appendChild(option);
+    });
+  }
   
+  // https://derickbailey.com/2016/03/23/get-a-data-attribute-value-from-the-selected-datalist-option/
+  getURLStringFor(inputSelector, listSelector) {
+    let input = this.get(inputSelector);
+    let list = this.get(listSelector);
+    
+    var value = input.value;
+    lively.notify('input value: ' + value);
+    // TODO: check for empty value ('')
+    // TODO: value could be a literal or a url
+    var option = this.get(`${listSelector} [value='${value}']`);
+    // value could also be an external url
+    if(!option) return;
+    lively.notify(option.innerHTML)
+    let url = option.dataset.url;
+    lively.notify('url: ' + url);
+    return url;
+  }
+  setLabel(text) {}
   setPlaceholder(text) {
     
   }
