965a0a8623cd02641da5c8768075a89729c5a98a
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-07-24 18:37:49 +0200

    fixed #207 and fixed #208
    SQUASHED: AUTO-COMMIT-demos-drag_and_drop.html,AUTO-COMMIT-templates-lively-halo-grab-item.js,

--- templates/lively-halo-grab-item.js
+++ templates/lively-halo-grab-item.js
@@ -17,7 +17,7 @@ export default class HaloGrabItem extends HaloItem {
  
  static get droppingBlacklist() {
       return {"*": 
-        ["button", "input", "lively-halo", "html",  "lively-selection", "lively-connector"]
+        ["h1","h2","h3","h4","h5", "lively-window", "button", "input", "lively-halo", "html",  "lively-selection", "lively-connector"]
       }
   };
  
