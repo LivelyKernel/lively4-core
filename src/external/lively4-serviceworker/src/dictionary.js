31211ee44bd59cf65c4334fd2035553759df3a9a
Thulur <koall.sebastian@gmail.com>
2017-11-21 11:40:27 +0100

    Refactor sw db code, create FavoritsTracker
    SQUASHED: SQUASHED-AUTO-COMMIT-README.md-AUTO-COMMIT-src-external-lively4-serviceworker-src-cache.js-AUTO-COMMIT-src-external-lively4-serviceworker-src-dbobject.js-AUTO-COMMIT-src-external-lively4-serviceworker-src-dictionary.js-AUTO-COMMIT-src-external-lively4-serviceworker-src-favoritstracker.js-AUTO-COMMIT-src-external-lively4-serviceworker-src-queue.js,

--- src/external/lively4-serviceworker/src/dictionary.js
+++ src/external/lively4-serviceworker/src/dictionary.js
@@ -6,18 +6,8 @@ import { DbObject } from './dbobject.js';
  */
 export class Dictionary extends DbObject {
   constructor() {
-    super('lively-sw-dictionary', 'cache');
-    
-    var request = indexedDB.open(this._dbName, 1);
-    
-    request.onupgradeneeded = (event) => {
-      this._db = event.target.result;
-      this._db.createObjectStore(this._storeName);
-    }; 
-    
-    request.onsuccess = (event) => {
-      this._db = event.target.result;
-    }
+    super('dictionary');
+    this._connect();
   }
   
   /**
