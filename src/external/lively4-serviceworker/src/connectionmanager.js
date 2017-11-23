067bb202b6913cd140cf66140964a2e3ed63dd09
davidrauch <mail@davidrauch.at>
2017-11-23 08:56:14 +0100

    Improve offline_online polling in case events are not forwarded
    SQUASHED: AUTO-COMMIT-src-client-load.js,AUTO-COMMIT-src-external-lively4-serviceworker-src-connectionmanager.js,

--- src/external/lively4-serviceworker/src/connectionmanager.js
+++ src/external/lively4-serviceworker/src/connectionmanager.js
@@ -4,7 +4,8 @@
  */
 
 const EVENT_NAME = 'statusChanged';
-const CHECK_INTERVAL = 5000;
+const CHECK_BROWSER_INTERVAL = 1000;
+const CHECK_NETWORK_INTERVAL = 5000;
 
 export class ConnectionManager {
   constructor() {
@@ -15,6 +16,9 @@ export class ConnectionManager {
     this._listeners = [];
     
     // Add a listener to receive forwarded 'offline' and 'online' events from the window
+    // This is unreliable since events won't be forwarded when there is an error in the window
+    // Or when the window is not fully loaded, e.g. We don't get notified or changes in network
+    // state while lively is loading. So we also poll the status in case we are not notified
     self.addEventListener('message', (e) => { 
       let data = e.data;
       if(data.type && data.message && data.type === 'network') {
@@ -27,8 +31,11 @@ export class ConnectionManager {
       }
     });
     
+    // Repeatedly check if the browser thinks we are online
+    self.setInterval(this._checkBrowserOnline, CHECK_BROWSER_INTERVAL, this);
+    
     // Repeatedly check if we are really online, since the browser's 'online' status is not reliable
-    self.setInterval(this._checkOnline, CHECK_INTERVAL, this);
+    self.setInterval(this._checkNetworkOnline, CHECK_NETWORK_INTERVAL, this);
   }
   
   /**
@@ -57,13 +64,25 @@ export class ConnectionManager {
   }
   
   /**
+   * Is called repeatedly to check whether the browser says we are online
+   * Calls _statusChanged if the network status changes
+   * @param connectionManager A reference to the ConnectionManager,
+   *                          since this function is called from setInterval
+   *                          and therefore does not have access to 'this'
+   */
+  _checkBrowserOnline(connectionManager) {
+    // Simply forward the state from the browser
+    connectionManager._setIsOnline(self.navigator.onLine);
+  }
+  
+  /**
    * Is called repeatedly to check whether we are still online
    * Calls _statusChanged if the network status changes
    * @param connectionManager A reference to the ConnectionManager,
    *                          since this function is called from setInterval
    *                          and therefore does not have access to 'this'
    */
-  _checkOnline(connectionManager) {
+  _checkNetworkOnline(connectionManager) {
     // Only check if we think we are online
     if(!connectionManager.isOnline) {
       return;
