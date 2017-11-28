f192bacc4e616d864fa95cf504bf0f9eddbd6d7b
onsetsu <stefan.ramson@gmail.com>
2017-11-28 14:54:04 +0100

    first aexpr fiddles
    SQUASHED: AUTO-COMMIT-src-client-boot.js,AUTO-COMMIT-src-external-aexpr-test-temp.js,AUTO-COMMIT-src-external-aexpr-test-temp.spec.js,AUTO-COMMIT-templates-lively-ast-explorer.js,

--- templates/lively-ast-explorer.js
+++ templates/lively-ast-explorer.js
@@ -3,7 +3,6 @@
 import Morph from './Morph.js';
 import {babel} from 'systemjs-babel-build';
 import SyntaxChecker from 'src/client/syntax.js'
-import locals from 'babel-plugin-locals'
 import sourcemap from 'src/external/source-map.min.js'
 import {modulesRegister} from 'systemjs-babel-build';
 import { uuid as generateUUID, debounce, flatmap, executeAllTestRunners, promisedEvent } from 'utils';
