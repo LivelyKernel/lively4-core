1a0a92497dc8e993d7f9f7afa803a392452026cd
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2017-12-14 15:41:53 +0100

    refactor dropbox, github and googledrive authentification
    SQUASHED: AUTO-COMMIT-src-client-auth-dropbox.js,AUTO-COMMIT-src-client-auth-github.js,AUTO-COMMIT-src-client-auth-googledrive.js,AUTO-COMMIT-src-client-auth.js,

--- src/client/auth-dropbox.js
+++ src/client/auth-dropbox.js
@@ -1,122 +1,40 @@
-import * as messaging from "./messaging.js";
-import focalStorage from '../external/focalStorage.js';
+import Auth from "./auth.js"
 
-export default class AuthDropbox {
-  
-  static parseAuthInfoFromUrl(data) {
-    var authInfo = JSON.parse(
-      '{"' + data.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-      function(key, value) { return key === "" ? value : decodeURIComponent(value); });
-    return authInfo;
+export default class AuthDropbox extends Auth {
+  static get name() {
+    return "Dropbox"
   }
 
-  static notifyMe(title, text, cb) {
-    if (Notification.permission !== "granted")
-      Notification.requestPermission();
-    else {
-      var notification = new Notification(title, {
-        icon: 'https://cf.dropboxstatic.com/static/images/brand/glyph-vflK-Wlfk.png',
-        body: text,
-      });
-      notification.onclick = cb
-    }
-  }
-
-  static onAuthenticated(windowUuid, authInfo) {
-    var state = authInfo.state
-    var token = authInfo.token
-
-    if (!state) { 
-      throw new Error("AuthDropbox Error, no state! Authinfo: " + JSON.stringify(authInfo))
-    }
-    // console.log("authInfo: ", authInfo)
-    focalStorage.setItem("dropboxToken", token).then(function() {
-      var cb = AuthDropbox.onAuthenticatedCallbacks[state]
-      if (cb) {
-        // console.log("running onAuthenticated callback: " + cb)
-        cb(token)
-      } else {
-        throw new Error("AuthDropbox Error: no callback found for" + state)
-      }
-    })
-  }
-
-  static logout(cb) {
-    focalStorage.setItem("dropboxToken", null).then(cb)
-  }
-
-  static popup(url) {
-    var width = 525,
-      height = 525,
-      screenX = window.screenX,
-      screenY = window.screenY,
-      outerWidth = window.outerWidth,
-      outerHeight = window.outerHeight;
-    var left = screenX + Math.max(outerWidth - width, 0) / 2;
-    var top = screenY + Math.max(outerHeight - height, 0) / 2;
-    var features = [
-      "width=" + width,
-      "height=" + height,
-      "top=" + top,
-      "left=" + left,
-      "status=no",
-      "resizable=yes",
-      "toolbar=no",
-      "menubar=no",
-      "scrollbars=yes"];
-    var popupWindow = window.open(url, "oauth", features.join(","));
-    if (!popupWindow) {
-      this.notifyMe("Dropbox Authenfication required", "click here to authenticate", function() {
-        // console.log("try to open window")
-      window.open(url, "oauth", features.join(","));
-      })
-    } else {
-      // popup.uuid = lively.net.CloudStorage.addPendingRequest(req);
-      popupWindow.focus();
-    }
+  static get tokenName() {
+    return "dropboxToken"
   }
   
-  static challengeForAuth(uuid, cb) {
-    if (uuid && cb) {
-      AuthDropbox.onAuthenticatedCallbacks[uuid] = cb
-    }
-    var appInfo = {
+  static tokenFromAuthInfo(authInfo) {
+    return authInfo.token
+  }
+
+  static get notificationIconURL() {
+    return 'https://cf.dropboxstatic.com/static/images/brand/glyph-vflK-Wlfk.png'
+  }
+
+  static get appInfo() {
+    return {
           "clientId": "1774dvkirby4490",
           "redirectUri": "https://lively-kernel.org/lively4-auth/oauth/dropbox.html"
     };
-    fetch("https://lively-kernel.org/lively4-auth/open_dropbox_accesstoken?state="+uuid)
-      .then(r => r.text()).then(data => {
-        // console.log("challenge got a token, too: " + data)
-        var authInfo = this.parseAuthInfoFromUrl(data)
-        AuthDropbox.onAuthenticated(uuid, authInfo)
-    })
-    var url =
-      "https://www.dropbox.com/1/oauth2/authorize" +
-      "?client_id=" + appInfo.clientId +
-      "&response_type=token" +
-      "&state=" + uuid +
-      "&redirect_uri=" + encodeURIComponent(appInfo.redirectUri);
-    // console.log("query dropbox")
-    this.popup(url);
   }
 
-  static load() {
-    this.onAuthenticatedCallbacks = {}
-    // receive messages
-    navigator.serviceWorker.addEventListener("message", function(event) {
-        if (event.data.name == 'githubAuthTokenRequired') {
-          // console.log("goth auth token required: " + JSON.stringify(event.data))
-          var callbackId = event.data.callbackId
-          AuthDropbox.challengeForAuth(Date.now(), function(token) {
-            messaging.postMessage({
-                type: 'callback',
-              callbackId: callbackId,
-              args: [token]
-          })
-          })
-        }
-    })
+  static oauthTokenURL(uuid) {
+    return "https://lively-kernel.org/lively4-auth/open_dropbox_accesstoken?state="+uuid
   }
+  
+  static oauthURL(uuid) {
+    return "https://www.dropbox.com/1/oauth2/authorize" +
+      "?client_id=" + this.appInfo.clientId +
+      "&response_type=token" +
+      "&state=" + uuid +
+      "&redirect_uri=" + encodeURIComponent(this.appInfo.redirectUri);
+  } 
 }
 AuthDropbox.load()
 
