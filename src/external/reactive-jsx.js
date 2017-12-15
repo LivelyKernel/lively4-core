40427e9fa2083788307f78caded765ed52d9d7d3
onsetsu <stefan.ramson@gmail.com>
2017-12-15 18:35:48 +0100

    added todo
    SQUASHED: AUTO-COMMIT-src-external-reactive-jsx.js,

--- src/external/reactive-jsx.js
+++ src/external/reactive-jsx.js
@@ -138,6 +138,7 @@ export function childExpression(expression) {
 }
 
 export function childSpread(array) {
+  // #TODO: <ul>{roq}</ul> also gets the reactive behavior, do we want this?
   if(isActiveGroup(array)) {
     return [array];
   } else {
