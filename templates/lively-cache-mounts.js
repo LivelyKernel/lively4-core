f35539044900eb65e86721da6dcd9c4924214e5c
davidrauch <mail@davidrauch.at>
2018-01-06 15:16:14 +0100

    Add proof of concept for full mount caching
    SQUASHED: AUTO-COMMIT-src-external-lively4-serviceworker-src-cache.js,AUTO-COMMIT-src-external-lively4-serviceworker-src-util.js,AUTO-COMMIT-templates-lively-cache-mounts.js,AUTO-COMMIT-templates-lively-cache-viewer.js,

--- templates/lively-cache-mounts.js
+++ templates/lively-cache-mounts.js
@@ -4,6 +4,8 @@ import focalStorage from 'src/external/focalStorage.js';
 export default class LivelyCacheMounts extends Morph {
   async initialize() {
     this.windowTitle = "Lively Cache Mounts";
+    this.registerButtons();
+    
     this.selectedMount = null;
     this.lastSelection = null;
     
@@ -19,20 +21,25 @@ export default class LivelyCacheMounts extends Morph {
     let ul = document.createElement("ul");
     for (let mount of this._mounts) {
       let li = document.createElement("li");
-      li.innerText = mount.name;
+      li.innerText = mount.path;
       li.addEventListener("click", () => {
         if (this.lastSelection !== null) {
           this.lastSelection.style.backgroundColor = "#fff";
         }
         
         this.lastSelection = li;
-        this.selectedMount = mount;
+        this._selectMount(mount);
         li.style.backgroundColor = "#eee";
       });
       ul.appendChild(li);
-    }
+      }
     
     mountList.appendChild(ul);
+  }
+  
+  _selectMount(mount) {
+    this.selectedMount = mount;
+    this.get("#cacheMountButton").disabled = (this.selectedMount == null);
   }
   
   _checkMounts() {
@@ -40,11 +47,14 @@ export default class LivelyCacheMounts extends Morph {
       (mounts) => {
         if (mounts === null) {
           // Warn about missing mounts
+          let mountList = this.get("#mountList");
+          mountList.innerHTML = "";
           let ul = document.createElement("ul");
           let li = document.createElement("li");
           li.style.backgroundColor = "#f00";
           li.innerText = "Nothing mounted yet!";
           ul.appendChild(li);
+          mountList.appendChild(ul);
         } else {
           this.loadMounts(mounts);
         }
@@ -63,5 +73,24 @@ export default class LivelyCacheMounts extends Morph {
     }
     
     this._mounts = tmpMounts;
+  }
+  
+  async onCacheMountButton() {
+    let cachedMounts = await focalStorage.getItem("lively4cachedmounts");
+    if(cachedMounts == null) {
+      cachedMounts = [];
+    }
+    
+    // Check if this mount is already cached
+    if(cachedMounts.indexOf(this.selectedMount.path) == -1) {
+      cachedMounts.push(this.selectedMount.path);
+      await focalStorage.setItem("lively4cachedmounts", cachedMounts);
+    }
+    
+    navigator.serviceWorker.controller.postMessage({
+      type: 'dataRequest',
+      command: "preloadFull",
+      data: `https://lively4${this.selectedMount.path}/`
+    });
   }
 }
