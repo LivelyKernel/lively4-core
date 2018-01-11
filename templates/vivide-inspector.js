05438d2ba6396d7ed733917982077bd825089898
jchromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2018-01-11 16:39:12 +0100

    vivide_ start implementing function exchange in inspector
    SQUASHED: AUTO-COMMIT-templates-vivide-inspector.js,

--- templates/vivide-inspector.js
+++ templates/vivide-inspector.js
@@ -5,47 +5,70 @@ export default class VivideInspector extends Morph {
   }
   
   inspect(widget) {
+    this.widget = widget;
     this.get("#model-inspector").inspect(widget.raw_model);
     
-    if(widget.transformations) {
-      this.fillFunctionsTableMultiple(widget);
+    if(this.widget.transformations) {
+      this.fillFunctionsTableMultiple();
       return;
     }
     
-    if(widget.transformation) {
-      this.fillFunctionsTableSingle(widget.transformation, undefined);
+    if(this.widget.transformation) {
+      this.fillFunctionsTableSingle(this.widget.transformation);
     }
   }
   
-  fillFunctionsTableSingle(transformation, depiction) {
-    let tr = document.createElement("tr");
+  buildTransformationCell(transformation, number) {
     let transformationTd = document.createElement("td");
     let transformationEditor = document.createElement("juicy-ace-editor");
     
     transformationEditor.value = transformation;
-    transformationTd.append(transformationEditor);
-    tr.append(transformationTd);
-    
-    if(!depiction) {
-      this.get("#functions-table").append(tr);
-      return;
+    transformationEditor.doSave = content => {
+      if(number === undefined) {
+        this.widget.transformation = eval(content);
+        this.widget.refresh();
+      }
     }
     
+    transformationTd.append(number != undefined ? "Transformation #" + number + ":" : "Transformation:");
+    transformationTd.append(transformationEditor);
+    
+    return transformationTd;
+  }
+  
+  buildDepictionCell(depiction, number) {
     let depictionTd = document.createElement("td");
     let depictionEditor = document.createElement("juicy-ace-editor");
+    
     depictionEditor.value = depiction;
+    depictionEditor.doSave = content => {
+      lively.notify(content);
+    }
+    
+    depictionTd.append(number != undefined ? "Depiction #" + number + ":" : "Depiction:");
     depictionTd.append(depictionEditor);
-    tr.append(depictionTd);
+    
+    return depictionTd;
+  }
+  
+  fillFunctionsTableSingle(transformation, depiction=undefined, number=undefined) {
+    let tr = document.createElement("tr");
+    
+    tr.append(this.buildTransformationCell(transformation, number));
+    
+    if(depiction) {
+      tr.append(this.buildDepictionCell(depiction, number));
+    }
 
     this.get("#functions-table").append(tr);
   }
   
-  fillFunctionsTableMultiple(widget) {
-    for(let i=0; i<widget.transformations.length; ++i) {
-      if(widget.depictions.length > i) {
-        this.fillFunctionsTableSingle(widget.transformations[i], widget.depictions[i]);
+  fillFunctionsTableMultiple() {
+    for(let i=0; i<this.widget.transformations.length; ++i) {
+      if(this.widget.depictions.length > i) {
+        this.fillFunctionsTableSingle(this.widget.transformations[i], this.widget.depictions[i], i);
       } else {
-        this.fillFunctionsTableSingle(widget.transformations[i], undefined);
+        this.fillFunctionsTableSingle(this.widget.transformations[i], undefined, i);
       }
     }
   }
