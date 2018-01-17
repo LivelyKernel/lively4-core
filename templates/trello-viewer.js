60cf7952a33077fc0ad8487f42b1fc54f800ccad
onsetsu <stefan.ramson@gmail.com>
2018-01-17 07:59:10 +0100

    simple Trello viewer
    SQUASHED: AUTO-COMMIT-templates-trello-viewer.html,AUTO-COMMIT-templates-trello-viewer.js,

--- templates/trello-viewer.js
+++ templates/trello-viewer.js
@@ -14,15 +14,6 @@ export default class TrelloViewer extends Morph {
     this.windowTitle = "TrelloViewer";
     
     this.get('button').addEventListener('click', e => this.init(e));
-    this.get('#execute').addEventListener('click', e => this.execute(e));
-    this.get('#editor').value = `function asyncOutput(output) {
-  console.log(JSON.stringify(output));
-  $morph("output").innerHTML = JSON.stringify(output, null, 2);
-}
-var success = function(successMsg) { asyncOutput(successMsg); };
-var error = function(errorMsg) { asyncOutput(errorMsg); };
-return Trello.get('/boards/Q21U0eYi/lists', success, error);
-`;
   }
   async init(e) {
     var key = await focalStorage.getItem(TrelloViewer.DEVELOPER_KEY_FOCAL_STORAGE);
@@ -63,18 +54,86 @@ return Trello.get('/boards/Q21U0eYi/lists', success, error);
         this.loadBoards(org.id);
       }}><a>{org.displayName}</a></li>);
     });
+    if(organizations[0]) {
+      this.loadBoards(organizations[0].id);
+    }
   }
   async loadBoards(organizationId) {
     this.boardList.innerHTML = '';
     let boards = await this.getRequest(`organizations/${organizationId}/boards?fields=all`);
     boards.forEach(board => {
       // #TODO: 'boards/53da8c65c30fb964fe268467/cards?fields=all'
-      this.boardList.appendChild(<li><a>{board.name}</a></li>);
+      this.boardList.appendChild(<li click={e => {
+        this.loadLists(board.id);
+      }}><a>{board.name}</a></li>);
     });
+    if(boards[0]) {
+      this.loadLists(boards[0].id);
+    }
+  }
+  async loadLists(boardId) {
+    this.listList.innerHTML = '';
+    let lists = await this.getRequest(`boards/${boardId}/lists?fields=all`);
+    lists.forEach(list => {
+      // #TODO: 'lists/53da8c65c30fb964fe268467/cards?fields=all'
+      this.listList.appendChild(<li click={e => {
+        this.loadCards(list.id);
+      }}><a>{list.name}</a></li>);
+    });
+    if(lists[0]) {
+      this.loadCards(lists[0].id);
+    }
+  }
+  async loadCards(listId) {
+    this.cardList.innerHTML = '';
+    let cards = await this.getRequest(`lists/${listId}/cards?fields=all`);
+    cards.forEach(card => {
+      this.cardList.appendChild(<li click={e => {
+        this.get('#editor').value = JSON.stringify(card, null, 2);
+      }}><a>{card.name}</a></li>);
+    });
+    if(cards[0]) {
+      this.get('#editor').value = JSON.stringify(cards[0], null, 2);
+    }
   }
   async execute(e) {
     this.get('#editor').setDoitContext(this);
     this.get('#editor').boundEval();
     lively.notify('sent request');
   }
+}
+
+function playground() {
+  Trello.authorize({
+    type: 'popup',
+    name: 'Trello Sandbox',
+    interactive: true,
+    scope: { read: true, write: true, account: false },
+    expiration: '1day',
+    persist: true,
+    success: () => {
+      lively.notify("success");
+      resolve();
+    },
+    error: () => {
+      lively.notify("error");
+      reject();
+    },
+  });
+  Trello.authorized()
+  that.get('#editor').boundEval()
+
+    function makeProm(func, ...params) {
+    return new Promise((resolve, reject) => {
+      func(...params, resolve, reject);
+    });
+  }
+
+  makeProm(Trello.get.bind(Trello), 'member/me?fields=all')
+  makeProm(Trello.get.bind(Trello), 'boards/53da8c65c30fb964fe268467/cards?fields=all')
+  makeProm(Trello.get.bind(Trello), 'boards/53da8c65c30fb964fe268467/lists?fields=all')
+  makeProm(Trello.get.bind(Trello), 'lists/53da8c65c30fb964fe268469/cards?fields=all')
+  makeProm(Trello.get.bind(Trello), 'cards/53da8c65c30fb964fe268470?fields=all')
+  makeProm(Trello.get.bind(Trello), 'member/me/organizations?fields=all')
+  makeProm(Trello.get.bind(Trello), 'organizations/546effef22c3faabe7b68657/boards?fields=all')
 }
