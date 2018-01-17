44b83a7f8071030c1c58025c415dde1aaefeb97e
HDinger <henriette.dinger@student.hpi.uni-potsdam.de>
2018-01-17 14:14:30 +0100

    Use highlight annotations
    SQUASHED: AUTO-COMMIT-doc-WebDev2017-project_3-annotations4.pdf,AUTO-COMMIT-templates-lively-pdf.js,

--- templates/lively-pdf.js
+++ templates/lively-pdf.js
@@ -82,7 +82,7 @@ export default class LivelyPDF extends Morph {
   }  
   
   onAnnotationClick(e) {
-    this.editAnnotation(e.path[1]);
+    this.editAnnotation(e.path[0]);
   }
   
   editAnnotation(annotationSection) {
@@ -118,7 +118,7 @@ export default class LivelyPDF extends Morph {
     let that = this;
     
     this.pdfViewer.pagesPromise.then(() => {
-      let annotations = that.getAllSubmorphs(".annotationLayer section.textAnnotation");
+      let annotations = that.getAllSubmorphs(".annotationLayer section.highlightAnnotation");
       annotations.forEach((annotation) => {
         lively.addEventListener("pdf", annotation, "click", eventFunctionObject);
       });
