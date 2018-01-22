1fcdb63c0a257ba4db999336a49bd1be5fe7c327
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2018-01-22 18:18:54 +0100

    edit style on selection
    SQUASHED: AUTO-COMMIT-src-components-halo-lively-halo.html,AUTO-COMMIT-src-components-halo-lively-hand.js,AUTO-COMMIT-src-components-tools-lively-style-editor.js,AUTO-COMMIT-src-components-widgets-lively-crayoncolors.js,

--- src/components/tools/lively-style-editor.js
+++ src/components/tools/lively-style-editor.js
@@ -11,6 +11,9 @@ class HTMLElementStyleProxy {
    
   }
   
+  toString() {
+    return "["+this.constructor.name + " on: " + this.target+"]"
+  }
   setupAccessors() {    
     this.createAttributeAccessor("fill", "backgroundColor")
     this.createAttributeAccessor("strokeColor", "borderColor")
@@ -29,6 +32,16 @@ class HTMLElementStyleProxy {
       enumerable: true,
       configurable: true
     });
+  }
+  
+  static proxyFor(target) {
+    if (target instanceof SVGElement) {
+      return new SVGElementStyleProxy(target)
+    } else if (target.tagName == "LIVELY-SELECTION") {
+      return new SelectionStyleProxy(target)
+    } else {
+      return new HTMLElementStyleProxy(target)      
+    }
   }
 }
 
@@ -62,6 +75,40 @@ class SVGElementStyleProxy extends HTMLElementStyleProxy {
 
 }
 
+
+
+class SelectionStyleProxy extends HTMLElementStyleProxy {
+  
+  constructor(target) {
+    super(target)
+    this.proxies = target.nodes.filter(ea => {
+      return !ea.isMetaNode
+    }).map(ea => HTMLElementStyleProxy.proxyFor(ea))   
+  }
+
+  setupAccessors() {    
+    this.createAttributeAccessor("fill")
+    this.createAttributeAccessor("strokeColor")
+    this.createAttributeAccessor("strokeWidth")
+    this.createAttributeAccessor("strokeStyle")
+  }
+  
+  createAttributeAccessor(name, styleName, map= v => v) {
+    Object.defineProperty(this, name, {
+      get() { 
+        return this.proxies[0] && this.proxies[0][name]; 
+      },
+      set(newValue) { 
+        debugger
+        lively.notify("set " + newValue)
+        this.proxies.forEach(ea => ea[name] = newValue);  
+      },
+      enumerable: true,
+      configurable: true
+    });
+  }
+}
+
 export default class LivelyStyleEditor extends Morph {
   async initialize() {
     this.windowTitle = "LivelyStyleEditor";   
@@ -74,6 +121,10 @@ export default class LivelyStyleEditor extends Morph {
     this.register('#strokeColorChooser', 'strokeColor')
     this.register('#strokeWidthChooser', 'strokeWidth')
     this.register('#strokeStyleChooser', 'strokeStyle', "change", (e,c) => c.value)
+    
+    this.shadowRoot.querySelectorAll("lively-crayoncolors").forEach(ea => {
+      ea.useAlwaysCustom();
+    })
   }
   
   register(elementSelector, styleName, eventName="value-changed", map= (e,c) => e.detail.value) {
@@ -81,15 +132,25 @@ export default class LivelyStyleEditor extends Morph {
     this.choosers.set(element, styleName)
     
     element.addEventListener(eventName, e => {
-      if (!this.target) return;
-      debugger
-      lively.notify("set " + styleName + " " + map(e, this.target))
+      if (!this.target || this.isInitializing) return;
+      // lively.notify("set " + styleName + " " + map(e, this.target))
       this.proxy[styleName] = map(e, element)
     });  
   }
   
   setTarget(target) {
-    lively.showElement(target)
+    if (target.tagName == "svg") {
+       var paths = target.querySelectorAll(":not(marker) > path")
+       if (paths.length == 1) {
+         // we are a single path... the user wan's to style it!
+         target = paths[0]
+         // either we do this... or we do the magic in the proxies? 
+         // e.g. modifying all paths when editing an svg... 
+         // part of the problem is that we currently have problems selecting 
+         // the contents of the svg...
+       }
+    }
+    
     this.onUpdateTarget(target)
   }
   
@@ -101,14 +162,12 @@ export default class LivelyStyleEditor extends Morph {
     this.target = target
     if (!this.target) return
     // dispatch to a proxy that does the translation work 
-    if (target instanceof SVGElement) {
-      this.proxy = new SVGElementStyleProxy(target)
-    } else {
-      this.proxy = new HTMLElementStyleProxy(target)      
-    }
+    this.proxy = HTMLElementStyleProxy.proxyFor(target)
+    this.isInitializing = true; // cheap #COP
     // fill the style editor with values found in the target
     this.choosers.forEach((style, chooser) => {
       chooser.value = this.proxy[style] 
     })
+    this.isInitializing = false;
   }  
 }
