236b41a38ed39d36ca4947766cd79eb56cd8bb4e
onsetsu <stefan.ramson@gmail.com>
2018-01-26 14:48:14 +0100

    structured tests more
    SQUASHED: AUTO-COMMIT-src-external-aexpr-test-aexpr.spec.js,AUTO-COMMIT-src-external-aexpr-test-life-cycle-management.spec.js,

--- src/external/aexpr/test/aexpr.spec.js
+++ src/external/aexpr/test/aexpr.spec.js
@@ -22,20 +22,6 @@ describe('simplify locals', function() {
     _ =  myIdentifier = 2;
   });
 });
-describe('disposeOnLastCallbackDetached', function() {
-  it('is defined', () => {
-    let exp = aexpr(() => {});
-    expect(exp).to.respondTo('disposeOnLastCallbackDetached');
-  });
-  xit('is chainable', () => {
-    let exp = aexpr(() => {})
-    let exp2 = exp.disposeOnLastCallbackDetached();
-    expect(exp2).to.equal(exp);
-  });
-  xit('works', () => {
-    // #TODO: proper test first
-  });
-});
 describe('loop constructs', function() {
   it('for loop/local variable (var)', () => {
     let x = 0;
@@ -663,68 +649,3 @@ describe("misc", () => {
     expect(spy).to.be.calledWithMatch(2);
   });
 })
-
-describe("life-cycle management", function() {
-  describe("dispose", function() {
-    function wait(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    it("aexprs define dispose", () => {
-      expect(aexpr(() => {})).to.respondTo('dispose');
-    });
-
-    it("dispose frame-based aexprs", async done => {
-      try {
-        let value = 0;
-        let spy = sinon.spy();
-        let axp = frameBasedAExpr.aexpr(() => value);
-        axp.onChange(spy);
-
-        expect(spy).not.to.be.called;
-
-        value = 1;
-        await wait(200);
-        expect(spy).to.be.calledOnce;
-        expect(spy).to.be.calledWithMatch(1);
-        spy.reset();
-
-        axp.dispose();
-
-        value = 2;
-        await wait(200);
-        expect(spy).not.to.be.called;
-
-        let spy2 = sinon.spy();
-        axp.onChange(spy2);
-        value = 3;
-        await wait(200);
-        expect(spy2).not.to.be.called;
-
-        done();
-      } catch(error) {
-        done(error);
-      }
-    }).timeout(3000);
-
-    it("dispose rewriting aexprs", () => {
-      let value = 0;
-      let spy = sinon.spy();
-      let axp = aexpr(() => value);
-      axp.onChange(spy);
-
-      expect(spy).not.to.be.called;
-
-      value = 1;
-      expect(spy).to.be.calledOnce;
-      expect(spy).to.be.calledWithMatch(1);
-      spy.reset();
-
-      axp.dispose();
-
-      value = 2;
-      expect(spy).not.to.be.called;
-    });
-  });
-});
-
