4363a8781e1c53abbb80457019817a2318cefe22
julianweise <privat@julianweise.de>
2018-05-15 22:23:24 +0200

    add genesisBlock and blockchain data structure
    SQUASHED: AUTO-COMMIT-doc-PX2018-project_1-toDo.md,AUTO-COMMIT-src-blockchain-model-block-block.js,AUTO-COMMIT-src-blockchain-model-blockchain-blockchain.js,AUTO-COMMIT-src-blockchain-model-blockchain-genesisBlock.js,AUTO-COMMIT-src-blockchain-model-transaction-transaction.js,AUTO-COMMIT-src-blockchain-model-transaction-transactionOutputCollection.js,

---
+++ src/blockchain/model/blockchain/blockchain.js
@@ -0,0 +1,21 @@
+import GenesisBlock from './genesisBlock.js';
+
+export default class Blockchain {
+  constructor(wallet) {
+    this._blocks = new Map();
+    this._headOfChain = new GenesisBlock(wallet);
+    this._blocks.set(this._headOfChain.hash, this._headOfChain);
+  }
+  
+  add(block) {
+    if(!block.isVerified()) {
+      // only accept valid blocks
+      return;
+    }
+    if(block.previousHash != this._headOfChain.hash) {
+      return;
+    }
+    this._blocks.set(block.hash, block);
+    this._headOfChain = block;
+  }
+}
