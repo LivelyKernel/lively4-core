31e5aa7d5eb61b7622cb92374b5089cc488be1a9
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2018-06-08 11:28:32 +0200

    new essay.js and added demos to karma
    SQUASHED: AUTO-COMMIT-demos-contextjs-adaptablepromise.js,AUTO-COMMIT-demos-contextjs-logpromises.js,AUTO-COMMIT-demos-contextjs-logpromises_old.js,AUTO-COMMIT-karma.conf.js,AUTO-COMMIT-src-client-essay.js,

--- karma.conf.js
+++ karma.conf.js
@@ -41,6 +41,10 @@ module.exports = function(config) {
       
       {pattern: 'test/**/*.js', included: false},
       {pattern: 'test/**/*.html', included: false},
+
+      {pattern: 'demos/**/*.js', included: false},
+      {pattern: 'demos/**/*.html', included: false},
+
       
       {pattern: 'vendor/**/*.js', included: false},
       {pattern: 'templates/**/*', included: false},
