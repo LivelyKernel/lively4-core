f33114ed3e7a44c3489ad74459c7b0c9865277a7
onsetsu <stefan.ramson@gmail.com>
2018-06-23 09:29:21 +0200

    better style for vivide inport halo
    SQUASHED: AUTO-COMMIT-src-client-reactive-active-expressions-babel-plugin-aexpr-source-transformation-example.js,AUTO-COMMIT-src-client-vivide-components-vivide.css,AUTO-COMMIT-src-components-halo-lively-halo-vivide-combine-item.html,AUTO-COMMIT-src-components-widgets-lively-code-mirror-widget-import.js,AUTO-COMMIT-src-components-widgets-lively-code-mirror.js,AUTO-COMMIT-templates-ast-node-program.html,AUTO-COMMIT-templates-ast-node-program.js,

---
+++ templates/ast-node-program.js
@@ -0,0 +1,9 @@
+"enable aexpr";
+
+import Morph from 'src/components/widgets/lively-morph.js';
+
+export default class AstNodeProgram extends Morph {
+  async initialize() {
+    this.windowTitle = "AstNodeProgram";
+  }
+}
