7c51d1a33002ab546266fbfbae08dbad67a7985b
julianweise <privat@julianweise.de>
2018-07-02 16:27:38 +0200

    implemented cryptochallenge on blockchain
    SQUASHED: AUTO-COMMIT-src-blockchain-model-block-block.js,AUTO-COMMIT-src-blockchain-model-block-miningProof.js,

--- src/blockchain/model/block/miningProof.js
+++ src/blockchain/model/block/miningProof.js
@@ -1,15 +1,16 @@
 import forge from 'node_modules/node-forge/dist/forge.min.js';
 
 const C_MIN_MINING_DIFFICULTY = 1;
-const C_MAX_MINING_DIFFICULTY = 60;
+const C_MAX_MINING_DIFFICULTY = 5;
 
 export default class MiningProof {
   constructor(miningDifficulty) {
     this.miningDifficulty = Math.max(C_MIN_MINING_DIFFICULTY,
-                                     Math.min(C_MAX_MINING_DIFFICULTY, miningDifficulty));
+                                     Math.min(C_MAX_MINING_DIFFICULTY, Math.round(miningDifficulty)));
     this.startTimestamp = null;
     this.finishTimestamp = null;
     this.hash = null;
+    this.nonce = 0;
   }
   
   get displayName() {
@@ -32,13 +33,22 @@ export default class MiningProof {
   }
   
   _solveCryptoPuzzle() {
-    // simulate proof of work by sleeping
-    return new Promise(sleep => setTimeout(sleep, this.miningDifficulty * 1000));
+    const zeroString = "0".repeat(this.miningDifficulty);
+    do {
+      this.hash(this.nonce + 1);
+    } while(this.hash.substring(this.hash.length - 1 - this.miningDifficulty) != zeroString);
+  }
+  
+  hash(nonce) {
+    this.nonce = nonce;
+    this.hash = this._hash();
+    return this.hash;
   }
   
   _hash() {
     const sha256 = forge.md.sha256.create();
     sha256.update(
+      this.nonce +
       this.miningDifficulty +
       this.startTimestamp +
       this.finishTimestamp
