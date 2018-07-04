a099d65548c85558373d516f39cacde1634ae446
Thulur <koall.sebastian@gmail.com>
2018-07-04 14:44:03 +0200

    Work on first active presentation example
    SQUASHED: AUTO-COMMIT-doc-PX2018-project_2-lively-mpm.html,AUTO-COMMIT-doc-PX2018-project_2-lively-mpm.js,AUTO-COMMIT-doc-PX2018-project_2-presentation-presentation-2018-07-18.md,AUTO-COMMIT-doc-PX2018-project_2-presentation.css,AUTO-COMMIT-doc-PX2018-project_2-vibratingpoint.js,

--- doc/PX2018/project_2/lively-mpm.js
+++ doc/PX2018/project_2/lively-mpm.js
@@ -17,22 +17,44 @@ export default class LivelyMpm extends Morph {
     
     lively.addEventListener("template", this, "dblclick", 
       evt => this.onDblClick(evt));
-     
+    
+    this.variables = {};
+    this.particleSize = 2;
+    this.animation = new VibratingPoint();
     this.canvas = this.get("#mpm");
     this.youngInput = this.get("#young-modulus");
     
     this.context = this.canvas.getContext("2d");
     this.context.fillStyle = "rgba(" + 255 + "," + 0 + "," + 0 + "," + 1 + ")";
     
-    
+    this.input = this.get("#input");
+    let inputUpdate = function() {
+      this.variables[this.input.name] = this.input.value;
+      // Resets particles
+      this.animation.numParticles = this.animation.numParticles;
+    }
+    $(this.input).on("input change", inputUpdate.bind(this));
   }
   
   draw(particles) {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     for (let i = 0; i < particles.length; ++i) {
-    let posX = particles[i] + (this.canvas.width / 2) - (this.animation.L / 2);
+      let posX = particles[i] + (this.canvas.width / 2) - (this.animation.L / 2);
       let posY = (this.canvas.height / (particles.length + 1)) * (i + 1);
       this.context.fillRect(posX, posY, this.particleSize, this.particleSize);
+    }
+  }
+  
+  set explanation(value) {
+    if (!Array.isArray(value)) {
+      console.log('Given object is not of type Array');
+      return;
+    }
+    
+    let explanation = this.get('#explanation');
+    for (let item of value) {
+      let element = <li>{item}</li>;
+      explanation.appendChild(element);
     }
   }
   
@@ -42,13 +64,15 @@ export default class LivelyMpm extends Morph {
   }
   
   onToggleAnimation() {
+    if (!this.animation) return;
+    
     if (this.animation.running) {
       this.animation.stopAnimating();
-      this.get("#toggleAnimation").innerHTML = "start";
+      this.get("#toggleAnimation").innerHTML = "Start Animation";
     } else {
       // Start animating
       this.animation.startAnimating(this);
-      this.get("#toggleAnimation").innerHTML = "stop";
+      this.get("#toggleAnimation").innerHTML = "Stop Animation";
     }
   }
 
