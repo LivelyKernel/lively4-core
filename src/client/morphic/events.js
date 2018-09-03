c2d3033efaa99b811a1fe8d05ba860340622f3bd
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2018-09-03 17:23:14 +0200

    refactored XRay events to use new hooks
    SQUASHED: AUTO-COMMIT-doc-journal-2018-09-03.md,AUTO-COMMIT-src-client-morphic-events.js,AUTO-COMMIT-src-parts-XRayEvents.html,

--- src/client/morphic/events.js
+++ src/client/morphic/events.js
@@ -2,63 +2,94 @@ import * as cop  from 'src/client/ContextJS/src/contextjs.js'
 
 // var beforeEvent = new WeakMap()
 
+
 export default class Events {
   
+  /* before */
+    
+  static disconnectBeforeEvent(cb) {
+    if (!this.beforeEventCallbacks) return
+    this.beforeEventCallbacks.delete(cb)
+  }
+  
+  static registerBeforeEvent(cb) {
+    if (!this.beforeEventCallbacks) {
+      this.beforeEventCallbacks = new Set()
+    }  
+    this.beforeEventCallbacks.add(cb)
+  }
+  
+  // before and after are easier to use, but not so powerful without the additional Map
   static logBeforeEvent(obj, type, evt, cb) {
-    // do nothing
-    // beforeEvent.set(evt, performance.now())
-    // console.log("before evt ", evt)
+    
+    if (this.beforeEventCallbacks) {
+      this.beforeEventCallbacks.forEach(eaCb => {
+        eaCb(obj, type, evt, cb)
+      })
+    }
+    
+  }
+  
+  /* after */
+  static disconnectAfterEvent(cb) {
+    if (!this.afterEventCallbacks) return
+    this.afterEventCallbacks.delete(cb)
+  }
+  
+  static registerAfterEvent(cb) {
+    if (!this.afterEventCallbacks) {
+      this.afterEventCallbacks = new Set()
+    }  
+    this.afterEventCallbacks.add(cb)
   }
 
-//   static logAroundEvent(obj, type, event, cb, proceed) {
-//     return proceed()
-//   }
-
   static logAfterEvent(obj, type, evt, cb) {
-    // do nothing
-    // console.log("after evt ", type, performance.now() - beforeEvent.get(evt))
-
+    if (this.afterEventCallbacks) {
+      this.afterEventCallbacks.forEach(eaCb => {
+        eaCb(obj, type, evt, cb)
+      })
+    }
   }
 
   static installHooks() {
     // this.installHooksContextJS()
     this.installHooksPlain()
   }
+//   // Alternative Implementation using Contetjs 
+//   static installHooksContextJS() {
   
-  static installHooksContextJS() {
-  
-    if (!window.__Events_callbackbMap) {
-      window.__Events_callbackbMap = new WeakMap()
-    }
-    var cbMap = window.__Events_callbackbMap
+//     if (!window.__Events_callbackbMap) {
+//       window.__Events_callbackbMap = new WeakMap()
+//     }
+//     var cbMap = window.__Events_callbackbMap
 
-    var layer = cop.layer(window, "EventHookLayer")
-    layer.refineClass(HTMLElement, {
+//     var layer = cop.layer(window, "EventHookLayer")
+//     layer.refineClass(HTMLElement, {
     
-    removeEventListener(type, cb, ...rest) {
-      var wrappedCB = cbMap.get(cb) // we have to keep the illusion
-      return cop.proceed(type, wrappedCB || cb, ...rest)
-    },
+//     removeEventListener(type, cb, ...rest) {
+//       var wrappedCB = cbMap.get(cb) // we have to keep the illusion
+//       return cop.proceed(type, wrappedCB || cb, ...rest)
+//     },
     
-    addEventListener(type, cb, ...rest) {
-        // we cannot just wrapp a callback cb, because callbacks are also used in removeEventListener...
-        var func = cbMap.get(cb) || (function(...args) {
-          cop.withoutLayers([layer], () => {
-            EventHooks.logBeforeEvent(this, type, args[0], cb)
-          })
-          var result = cb.apply(this, args)
-          cop.withoutLayers([layer], () => {
-            EventHooks.logAfterEvent(this, type, args[0], cb)
-          })
-          return result
-        })
-        cbMap.set(cb, func)
-        return cop.proceed(type, func, ...rest);
-      }
-    })
+//     addEventListener(type, cb, ...rest) {
+//         // we cannot just wrapp a callback cb, because callbacks are also used in removeEventListener...
+//         var func = cbMap.get(cb) || (function(...args) {
+//           cop.withoutLayers([layer], () => {
+//             EventHooks.logBeforeEvent(this, type, args[0], cb)
+//           })
+//           var result = cb.apply(this, args)
+//           cop.withoutLayers([layer], () => {
+//             EventHooks.logAfterEvent(this, type, args[0], cb)
+//           })
+//           return result
+//         })
+//         cbMap.set(cb, func)
+//         return cop.proceed(type, func, ...rest);
+//       }
+//     })
     
-    layer.beGlobal()
-  }
+//     layer.beGlobal()
+//   }
 
   static installHooksPlain() {
   
@@ -85,22 +116,30 @@ export default class Events {
         var func = cbMap.get(cb) || (function(...args) {
           
           // this becomes dangerous... without COP, we need a global flag
-          if (!EventHooks.__isLogging) {
-            try {
-              EventHooks.__isLogging = true
-              EventHooks.logBeforeEvent(this, type, args[0], cb)
-            } finally {
-              EventHooks.__isLogging = false
-            }
+          try {
+            if (!EventHooks.__isLogging) {
+              try {
+                EventHooks.__isLogging = true
+                EventHooks.logBeforeEvent(this, type, args[0], cb)
+              } finally {
+                EventHooks.__isLogging = false
+              }
+            }            
+          } catch(e) {
+            console.error(e)
           }
           var result = cb.apply(this, args)
-          if (!EventHooks.__isLogging) {
-            try {
-              EventHooks.__isLogging = true
-              EventHooks.logAfterEvent(this, type, args[0], cb)
-            } finally {
-              EventHooks.__isLogging = false
-            }
+          try {
+            if (!EventHooks.__isLogging) {
+              try {
+                EventHooks.__isLogging = true
+                EventHooks.logAfterEvent(this, type, args[0], cb)
+              } finally {
+                EventHooks.__isLogging = false
+              }
+            }            
+          } catch(e) {
+            console.error(e)
           }
           return result
         })
