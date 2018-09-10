e22da5df437e73aa019e8f9260b3e5baab2a3ed5
Stefan Ramson <stefan.ramson@hpi.de>
2018-09-10 10:47:30 +0200

    removed aexpr-ticking, again

--- src/client/reactive/active-expressions/aexpr-ticking/src/aexpr-ticking.js
+++
@@ -1,7 +0,0 @@
-export {
-  aexpr,
-  check,
-  clearDefaultActiveExpressions,
-  TickingActiveExpression
-} from './ticking/ticking-active-expressions.js';
-import {} from 'src/client/reactive/active-expressions/aexpr-ticking/src/ticking/ticking-active-expressions.js'
