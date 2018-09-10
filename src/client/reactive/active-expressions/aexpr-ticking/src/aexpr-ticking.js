f5792ee2458400267f07cefffb3d1e7a5821d158
Stefan Ramson <stefan.ramson@hpi.de>
2018-09-10 10:45:00 +0200

    added aexpr-ticking

---
+++ src/client/reactive/active-expressions/aexpr-ticking/src/aexpr-ticking.js
@@ -0,0 +1,7 @@
+export {
+  aexpr,
+  check,
+  clearDefaultActiveExpressions,
+  TickingActiveExpression
+} from './ticking/ticking-active-expressions.js';
+import {} from 'src/client/reactive/active-expressions/aexpr-ticking/src/ticking/ticking-active-expressions.js'
