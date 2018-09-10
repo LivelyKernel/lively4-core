68061645a0328d18fa210255f0d14bbaba5c3a8a
Stefan Ramson <stefan.ramson@hpi.de>
2018-09-10 17:11:57 +0200

    align ast worker config with main file

--- src/babylonian-programming-editor/worker/ast-worker-babel-wrapper.js
+++ src/babylonian-programming-editor/worker/ast-worker-babel-wrapper.js
@@ -44,11 +44,8 @@ SystemJS.config({
     'active-expression': lively4url + '/src/client/reactive/active-expression/active-expression.js',
     'active-expression-rewriting': lively4url + '/src/client/reactive/active-expression-rewriting/active-expression-rewriting.js',
     'babel-plugin-active-expression-rewriting': lively4url + '/src/client/reactive/babel-plugin-active-expression-rewriting/index.js',
-    'stack-es2015-modules': lively4url + '/src/client/reactive/active-expressions/stack-es2015-module/src/stack.js',
-    'frame-based-aexpr': lively4url + '/src/client/reactive/active-expressions/frame-based-aexpr.js',
-    // #TODO: duplicated, remove roq in imports
-    'roq': lively4url + '/src/client/reactive/active-group/src/select.js',
-    'active-groups': lively4url + '/src/client/reactive/active-group/src/select.js',
+    'frame-based-aexpr': lively4url + '/src/client/reactive/active-expression-convention/active-expression-frame-based.js',
+    'active-group': lively4url + '/src/client/reactive/active-group/select.js',
 
     // jsx support
     'babel-plugin-syntax-jsx': lively4url + '/src/external/babel-plugin-syntax-jsx.js',
