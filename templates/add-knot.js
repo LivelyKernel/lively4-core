881c2da8eeaf7b9f97370a3a18e10bccd999121b
onsetsu <stefan.ramson@gmail.com>
2018-11-08 11:53:43 +0100

    knot view remembers triples now
    SQUASHED: AUTO-COMMIT-src-client-triples-triples.js,AUTO-COMMIT-templates-add-knot.js,AUTO-COMMIT-templates-knot-view.js,

--- templates/add-knot.js
+++ templates/add-knot.js
@@ -8,23 +8,23 @@ export default class AddKnot extends Morph {
     this.windowTitle = "Add Knot";
 
     let title = this.get("#title");
-    title.addEventListener('keyup',  event => {
+    title.addEventListener('keyup', event => {
       if (event.keyCode == 13) { // ENTER
         this.save();
       }
     });
-    
+
     let button = this.get('#save');
     button.addEventListener('click', event => this.save());
   }
-  
+
   focus() {
     this.get("#title").focus();
   }
-  
+
   async save() {
     let graph = await Graph.getInstance();
-    
+
     let directory = this.get('#directory').value;
     let title = this.get('#title').value;
     let fileEnding = this.get('#file-ending').value;
@@ -32,10 +32,10 @@ export default class AddKnot extends Morph {
     let knot = await graph.createKnot(directory, title, fileEnding);
     let knotView = await lively.openComponentInWindow("knot-view");
     knotView.loadKnotForURL(knot.url);
-    
+
     this.afterSubmit(knot);
   }
-  
+
   // TODO: employ nice event-based approach or AOP/COP
   afterSubmit(knot) {}
 }
