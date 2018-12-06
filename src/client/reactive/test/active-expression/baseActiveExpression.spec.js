0ade2a7e302a7971d8c93413fe16d28a4a9a16b2
onsetsu <stefan.ramson@gmail.com>
2018-12-06 14:57:10 +0100

    matchers for base aexprs
    SQUASHED: AUTO-COMMIT-src-client-reactive-active-expression-active-expression.js,AUTO-COMMIT-src-client-reactive-test-active-expression-baseActiveExpression.spec.js,

--- src/client/reactive/test/active-expression/baseActiveExpression.spec.js
+++ src/client/reactive/test/active-expression/baseActiveExpression.spec.js
@@ -156,7 +156,113 @@ describe('Configurable Comparison Function', () => {
 
       expect(spy).not.to.be.called;
     });
+    
+    describe('explicit option configured to identity', () => {
+      it('array modification do not trigger callbacks', () => {
+        const spy = sinon.spy();
+        const arr = [1,2];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'identity' }).onChange(spy);
+
+        arr.push(42);
+        aexpr.checkAndNotify();
+
+        expect(spy).not.to.be.called;
+      });
+      it('identity changes matter', () => {
+        const spy = sinon.spy();
+        let arr = [1,2];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'identity' }).onChange(spy);
+
+        arr = [1,2];
+        aexpr.checkAndNotify();
+
+        expect(spy).to.be.calledOnce;
+        expect(spy.getCall(0).args[0]).to.equal(arr);
+      });
+    });
+    
+    describe('explicit option set to shallow', () => {
+      it('array modification triggers callbacks', () => {
+        const spy = sinon.spy();
+        const arr = [1,2];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'shallow' }).onChange(spy);
+
+        arr.push(42);
+        aexpr.checkAndNotify();
+
+        expect(spy).to.be.calledOnce;
+        expect(spy.getCall(0).args[0]).to.equal(arr);
+      });
+      it('identity changes do not matter', () => {
+        const spy = sinon.spy();
+        let arr = [1,2];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'shallow' }).onChange(spy);
+
+        arr = [1,2];
+        aexpr.checkAndNotify();
+
+        expect(spy).not.to.be.called;
+      });
+    });
+    
+    describe('explicit option set to deep', () => {
+      it('array modification triggers callbacks', () => {
+        const spy = sinon.spy();
+        const arr = [1, { x: 4 }];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'deep' }).onChange(spy);
+
+        arr.push(42);
+        aexpr.checkAndNotify();
+
+        expect(spy).to.be.calledOnce;
+        expect(spy.getCall(0).args[0]).to.equal(arr);
+      });
+      it('identity changes do not matter1', () => {
+        const spy = sinon.spy();
+        let arr = [1, { x: 4 }];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'deep' }).onChange(spy);
+
+        arr = [1, { x: 4 }];
+        aexpr.checkAndNotify();
+
+        expect(spy).not.to.be.called;
+      });
+      it('nested property identity changes do not trigger1', () => {
+        const spy = sinon.spy();
+        let arr = [1, { x: 4 }];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'deep' }).onChange(spy);
+
+        arr[1] = { x: 4 };
+        aexpr.checkAndNotify();
+
+        expect(spy).not.to.be.called;
+      });
+      it('nested property changes trigger', () => {
+        const spy = sinon.spy();
+        let arr = [1, { x: 4 }];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'deep' }).onChange(spy);
+
+        arr[1] = { x: 5};
+        aexpr.checkAndNotify();
+
+        expect(spy).to.be.calledOnce;
+        expect(spy.getCall(0).args[0]).to.equal(arr);
+      });
+      it('nested property changes trigger (modify only a deep property)', () => {
+        const spy = sinon.spy();
+        let arr = [1, { x: 4 }];
+        const aexpr = new BaseActiveExpression(() => arr, { match: 'deep' }).onChange(spy);
+
+        arr[1].x = 5;
+        aexpr.checkAndNotify();
+
+        expect(spy).to.be.calledOnce;
+        expect(spy.getCall(0).args[0]).to.equal(arr);
+      });
+    });
+    
   });
+  
   describe('Sets as Data Structures', () => {
     it('detects a newly added element', () => {
       const spy = sinon.spy();
