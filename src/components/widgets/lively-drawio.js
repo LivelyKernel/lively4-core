a58bb368e41a60bd70fb2c9c47f6a5a9f733043d
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-01-27 16:39:01 +0100

    drawio printing WIP
    SQUASHED: AUTO-COMMIT-src-components-widgets-lively-drawio.js,

--- src/components/widgets/lively-drawio.js
+++ src/components/widgets/lively-drawio.js
@@ -114,11 +114,43 @@ export default class LivelyDrawio extends Morph {
     } else {
       lively.notify("editing not supported for", this.src)
     }
-    
-    
-    
   }
   
+  async getPDFDataURL() {
+    var form =  new FormData();
+    var config = {
+      format: "pdf",
+      bg: "#ffffff",
+      base64: 1,
+      embedXml: 0,
+      xml: "%3Cmxfile%20modified%3D%222019-01-26T19%3A00%3A48.908Z%22%20host%3D%22www.draw.io%22%20agent%3D%22Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F71.0.3578.98%20Safari%2F537.36%22%20version%3D%2210.1.4%22%20etag%3D%22JVROaZdnLW3DEGmiwtOJ%22%20type%3D%22github%22%3E%3Cdiagram%20name%3D%22Page-1%22%20id%3D%2297a40813-6b03-0c74-37c1-0168de264b11%22%3ErZRLb4MwDMc%2FDXeeexxb2nWHTprUw84BXIgWMEpNofv0S5rwGq00TeMAzs924vzj4ARx2e0kq4s3zEA4vpt1TrBxfD%2BKQvXW4GLAc%2FRoQC55ZpA3ggP%2FAgtdSxuewWkWSIiCeD2HKVYVpDRjTEps52FHFPNVa5bDAhxSJpb0g2dUGPoUuSN%2FBZ4XduXItY6EpZ%2B5xKayyzl%2BcLw%2Bxl2yfiobfypYhu0EBVsniCUiGavsYhBa2V41k%2FdyxzuULaGi3yT4JuHMRAN9xde66NJLoUqstZk2ifqs24ITHGqWataqw1esoFKokafMRO8dsn3Sg2VFtsgzSIJugmyFO8ASSF5UiPU%2BWLFsL4V22I4HE%2FZ6FpNDGfKYbYZ8mHlURBlWlNsChQuBVimhvKtSUwoTEKz1BrlqqT1LQLzjiRPHSoUkSISlChDasR46Jkah8zZjz4xzrATPdS7hD72xIcEriId74P6P5r47F93zlqoHN0T%2Fg%2BZqODb81Tf5pwTbbw%3D%3D%3C%2Fdiagram%3E%3C%2Fmxfile%3E",
+      filename: "testdrawio.pdf"
+    }
+    for ( var key in config ) {
+      form.append(key, config[key]);
+    }
+    var convertToPDFRequest = fetch("https://exp.draw.io/ImageExport4/export", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: form,
+      // body: `format=pdf&bg=#ffffff&base64=1&embedXml=0&xml=%3Cmxfile%20modified%3D%222019-01-26T19%3A00%3A48.908Z%22%20host%3D%22www.draw.io%22%20agent%3D%22Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F71.0.3578.98%20Safari%2F537.36%22%20version%3D%2210.1.4%22%20etag%3D%22JVROaZdnLW3DEGmiwtOJ%22%20type%3D%22github%22%3E%3Cdiagram%20name%3D%22Page-1%22%20id%3D%2297a40813-6b03-0c74-37c1-0168de264b11%22%3ErZRLb4MwDMc%2FDXeeexxb2nWHTprUw84BXIgWMEpNofv0S5rwGq00TeMAzs924vzj4ARx2e0kq4s3zEA4vpt1TrBxfD%2BKQvXW4GLAc%2FRoQC55ZpA3ggP%2FAgtdSxuewWkWSIiCeD2HKVYVpDRjTEps52FHFPNVa5bDAhxSJpb0g2dUGPoUuSN%2FBZ4XduXItY6EpZ%2B5xKayyzl%2BcLw%2Bxl2yfiobfypYhu0EBVsniCUiGavsYhBa2V41k%2FdyxzuULaGi3yT4JuHMRAN9xde66NJLoUqstZk2ifqs24ITHGqWataqw1esoFKokafMRO8dsn3Sg2VFtsgzSIJugmyFO8ASSF5UiPU%2BWLFsL4V22I4HE%2FZ6FpNDGfKYbYZ8mHlURBlWlNsChQuBVimhvKtSUwoTEKz1BrlqqT1LQLzjiRPHSoUkSISlChDasR46Jkah8zZjz4xzrATPdS7hD72xIcEriId74P6P5r47F93zlqoHN0T%2Fg%2BZqODb81Tf5pwTbbw%3D%3D%3C%2Fdiagram%3E%3C%2Fmxfile%3E&filename=testdrawio.pdf`
+    })
+    var resp = await convertToPDFRequest
+    var text = await resp.text()
+    var dataURL = "data:application/pdf;base64,"+text
+    return dataURL
+  
+  // fetch("data:application/pdf;base64,"+text).then(r => r.blob()).then(blob => {
+  //   fetch("https://lively-kernel.org/lively4/lively4-jens/doc/figures/test.pdf", 
+  //     {
+  //       method: "PUT",
+  //       body: blob
+  //   })  
+  // })
+  }
+   
   async livelyExample() {
     // this.src = "https://lively-kernel.org/lively4/lively4-jens/doc/figures/testdrawio.xml"
     this.src = "https://raw.githubusercontent.com/JensLincke/drawio-figures/master/contextjs_promises_01.xml"
