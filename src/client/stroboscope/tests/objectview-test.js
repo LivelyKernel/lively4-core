01d66adde336f2b5d1b276b33a9d085893383525
livelyratnim <martin5fischer@web.de>
2019-01-27 22:58:22 +0100

    fixes testcases for Stroboscope ObjectView
    SQUASHED: AUTO-COMMIT-src-client-stroboscope-tests-objectview-test.js,

--- src/client/stroboscope/tests/objectview-test.js
+++ src/client/stroboscope/tests/objectview-test.js
@@ -17,13 +17,12 @@ describe('object view logic', () => {
     var event1 = new StroboscopeEvent(1, "Test", "solution", "number", "create", 1)
     var event2 = new StroboscopeEvent(1, "Test", "question", "string", "create", "Does it work?")
     
-    var view = new ObjectView(event)
+    var view = new ObjectView(event1)
     view.append(event2)
     
-    expect(view.id).to.equal(event.object_id);
-    expect(view.propertiesEventsMap[event1.property]).to.equal(1)
+    expect(view.propertiesEventsMap[event1.property].length).to.equal(1)
     expect(view.propertiesEventsMap[event1.property][0]).to.equal(event1)
-    expect(view.propertiesEventsMap[event2.property]).to.equal(1)
+    expect(view.propertiesEventsMap[event2.property].length).to.equal(1)
     expect(view.propertiesEventsMap[event2.property][0]).to.equal(event2)
   });
   
@@ -31,10 +30,11 @@ describe('object view logic', () => {
     var event1 = new StroboscopeEvent(1, "Test", "solution", "number", "create", 1)
     var event2 = new StroboscopeEvent(1, "Test", "solution", "number", "delete", undefined)
 
-    var view = new ObjectView(event)
+    var view = new ObjectView(event1)
+    view.append(event2)
     
-    expect(view.propertiesEventsMap[event.property]).to.equal(2)
-    expect(view.propertiesEventsMap[event.property][0]).to.equal(event1)
-    expect(view.propertiesEventsMap[event.property][1]).to.equal(event2)
+    expect(view.propertiesEventsMap[event1.property].length).to.equal(2)
+    expect(view.propertiesEventsMap[event1.property][0]).to.equal(event1)
+    expect(view.propertiesEventsMap[event1.property][1]).to.equal(event2)
   });
 });
