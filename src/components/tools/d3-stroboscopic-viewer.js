4e3a8349c663166705816e92459b902a0d71cbd4
livelyratnim <martin5fischer@web.de>
2019-01-27 16:57:44 +0100

    adds stroboscope object view
    SQUASHED: AUTO-COMMIT-src-components-tools-d3-stroboscopic-viewer.html,AUTO-COMMIT-src-components-tools-d3-stroboscopic-viewer.js,AUTO-COMMIT-src-components-tools-lively-stroboscope.html,AUTO-COMMIT-src-components-tools-lively-stroboscope.js,

--- src/components/tools/d3-stroboscopic-viewer.js
+++ src/components/tools/d3-stroboscopic-viewer.js
@@ -1,208 +1,275 @@
-import Morph from "src/components/widgets/lively-morph.js"
+import Morph from 'src/components/widgets/lively-morph.js';
 import d3 from "src/external/d3.v5.js"
+import Stroboscope from 'src/client/stroboscope/stroboscope.js';
+import StroboscopeEvent from 'src/client/stroboscope/stroboscopeevent.js';
 
+
+import { debounce } from "utils";
 
 export default class D3StroboscopicViewer extends Morph {
 
-  getTreeData() {
-    if (!this.treeData) {
-      this.treeData = {
-        "name": "Top Level",
-        "children": [{
-            "name": "Level 2: A",
-            "children": [
-              { "name": "Son of A" },
-              { "name": "Daughter of A" }
-            ]
-          },
-          { "name": "Level 2: B" }
-        ]
-      };
-    }
-    return this.treeData
-  }
-
-  setTreeData(data) {
-    this.treeData = data;
+  initialize() {
+    this.d3 = d3 // for scripting...
     this.updateViz()
+    this.options = {}
+    
+    this.addEventListener('extent-changed', ((evt) => {
+      this.onExtentChanged(evt);
+    })::debounce(500));
+
+    // Map of objects displayed by viewer
+    this.objectsToView = new Map();    
+  }
+  
+  
+  /*
+    async initialize() {
+    this.options = {}
+    this.loaded = new Promise(async (resolve) => {
+      this.updateViz()
+      this.addEventListener('extent-changed', ((evt) => {
+        this.onExtentChanged(evt);
+      })::debounce(500));
+      resolve()
+    })
+  }
+  */
+    
+  getData() {
+    return this.objectsToView
   }
 
-  async initialize() {
-    
-    var object1 = {offset : 0}
-    var object2 = {offset : 110}
-    var object3 = {offset : 220}
-
-    this.objects = [object1, object2, object3]
+  setData(data) {
+    this.data = data;
     this.updateViz()
-
+  }  
+  
+  dataColor(node) {
+    if (this.options.color !== undefined) return this.options.color(node)
+    return "gray"
   }
 
-  updateViz() {
-    var bounds = this.getBoundingClientRect()
-    this.shadowRoot.querySelector("svg").innerHTML = ""
-
-    var margin = { top: 10, right: 20, bottom: 10, left: 20 },
-      width = bounds.width - margin.right - margin.left,
-      height = bounds.height - margin.top - margin.bottom;
-
-    var svg = d3.select(this.shadowRoot.querySelector("svg"))
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    this.svg = svg
-    this.duration = 750
-    this.updateObjects()
-
-    //this.i = 0;
-
-    //var root;
-
-    //var treemap = d3.tree().size([height, width]);
-    //this.treemap = treemap
-
-    //root = d3.hierarchy(treeData, d => d.children);
-    //root.x0 = height / 2;
-    // root.y0 = 0;
-    // this.root = root
-
-    // Collapse after the second level
-    // root.children && root.children.forEach(ea => this.collapse(ea));
-
-    //this.update(root);
-    // d3.select(self.frameElement).style("height", "500px");    
+  dataWidth(node) {
+    if (this.options.width !== undefined) return this.options.width(node)
+    return 20
   }
 
-  // Collapse the node and all it's children
-  collapse(d) {
-    if (d.children) {
-      d._children = d.children
-      d._children.forEach(ea => this.collapse(ea))
-      d.children = null
+  dataHeight(node) {
+    if (this.options.height !== undefined) return this.options.height(node)
+    return 40
+  }
+  
+  dataFontsize(node) {
+    if (this.options.fontsize !== undefined) return this.options.fontsize(node)
+    return 10
+  }
+  
+  onNodeClick(node, evt, element) {
+    if (this.options.onclick) return this.options.onclick(node, evt, element)
+    lively.notify("clicked on " + node.id)
+  }
+  
+  config(config) {
+    Object.keys(config).forEach(key => {
+      // lively.notify("key " + key) 
+      this.options[key] = config[key] // we could check them here...      
+    })
+  }
+
+  livelyInspect(contentNode, inspector) {
+    if (this.data) {
+      contentNode.appendChild(inspector.display(this.data, false, "#data", this));
     }
   }
 
-  // Toggle children on click.
-  click(d) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-    this.update(d);
-  }
-
-  updateObjects() {
-
-    // Update the nodes…
-    var node = this.svg.selectAll("g.node")
-      .data(this.objects);
-
-    // Enter any new nodes at the parent's previous position.
-    var nodeEnter = node.enter().append("g")
-      .attr("class", "object")
-      .attr("transform", d => "translate(" + 0 + "," + d.offset + ")")
-      .on("click", (d) => this.click(d));
-
-    // Add Circle for the nodes
-    nodeEnter.append("rect")
-      .attr('class', 'object')
-      .attr("width", 50)
-      .attr("height", 100)
-
-    // Add labels for the nodes
-    nodeEnter.append("text")
-      .attr("x", 10)
-      .attr("dy", 10)
-      .text(d => this.id ? d.id : "undefined id")
-    
-    // UPDATE
-    var nodeUpdate = nodeEnter.merge(node)
-
-    // Update the node attributes and style
-    nodeUpdate.select("rect.object")
-      .attr("width", 50)
-      .attr("height", 100)
-      .style("fill", "lightgreen")
-      .attr('cursor', 'pointer');
-  }
-
-  update(source) {
-
-    // Assigns the x and y position for the nodes
-    var treeData = this.treemap(this.root);
-
-    // Compute the new tree layout
-    const nodes = treeData.descendants()
-    const links = treeData.descendants().slice(1) // links() // 
-
-    // Normalize for fixed-depth.
-    nodes.forEach((d) => { d.y = d.depth * 180; });
-
-    // Update the nodes…
-    var node = this.svg.selectAll("g.node")
-      .data(nodes, (d) => d.id || (d.id = ++this.i));
-
-    // Enter any new nodes at the parent's previous position.
-    var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", d => "translate(" + source.y0 + "," + source.x0 + ")")
-      .on("click", (d) => this.click(d));
-
-    // Add Circle for the nodes
-    nodeEnter.append("circle")
-      .attr('class', 'node')
-      .attr("r", 1e-6)
-      .style("fill", d => d._children ? "lightsteelblue" : "#fff");
-
-    // Add labels for the nodes
-    nodeEnter.append("text")
-      .attr("x", d => d.children || d._children ? -13 : 13)
-      .attr("dy", ".35em")
-      .attr("text-anchor", d => d.children || d._children ? "end" : "start")
-      .text(d => this.dataName ? this.dataName(d.data) : d.data.name)
-    // .style("fill-opacity", 1e-6);
-
-    // UPDATE
-    var nodeUpdate = nodeEnter.merge(node)
-
-    // Transition nodes to their new position.
-    nodeUpdate.transition()
-      .duration(this.duration)
-      .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
-
-
-    // Update the node attributes and style
-    nodeUpdate.select("circle.node")
-      .attr("r", 10)
-      .style("fill", d => d._children ? "lightsteelblue" : "white")
-      .attr('cursor', 'pointer');
-
-    // nodeUpdate.select("text")
-    //   .style("fill-opacity", 1);
-
-  }
-
-  // Creates a curved (diagonal) path from parent to the child nodes
-  diagonal(s, d) {
-    var path =
-      `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`
-    return path
-  }
-
-  livelyExample() {
-
+  onExtentChanged() {
+    //lively.notify("extent changed")
+    this.updateViz()
   }
 
   livelyMigrate(other) {
-    this.treeData = other.treeData
-    this.dataName = other.dataName
+    this.options = other.options
+    this.setData(other.getData())
+  }
+    
+  // check if object is known
+  knowAboutObject(id) {
+    return this.objectsToView.has(id);
   }
 
+  // check if known object has property
+  knownObjHasProperty(object_id, property) {
+    return this.objectsToView[object_id][property] != undefined;
+  }
+  
+  
+  /*
+   *
+   *
+   */
+  evaluateEvent(event) {
+    
+    // before all check if object is known
+    if( this.knowAboutObject(event.object_id)) {
+      // create new property
+      if(event.event_type === "create") {
+        this.objectsToView[event.object_id][event.property] = {
+          value: event.value,
+          type: event.property_type
+        }
+      }
+      // change property
+      if(event.event_type === "change") {
+        // check if property exists
+        if(knownObjHasProperty(event.object_id, event.property) ) {
+          this.objectsToView[event.object_id][event.property] = {
+            value: event.value,
+            type: event.property_type
+          }
+        }
+        else {
+          lively.notify("no changes on non existing properties");
+        }
+      }
+      // delete property
+      if(event.event_type === "delete") {
+        // check if property exists
+        if(knownObjHasProperty(event.object_id, event.property) ) {
+          delete this.objectsToView[event.object_id][event.property];
+        }
+        else {
+          lively.notify("no delete on non existing properties");
+        }
+      }
+    } else {
+ 
+      if(event.event_type === "create") {
+        var obj = {
+          id: event.object_id
+        }
+        //create property object
+        obj[event.property] = {
+          value: event.value,
+          type: event.property_type
+        }
+        // add property to object
+        this.objectsToView.set(obj);
+      }
+
+      if(event.event_type === "change") {
+        lively.notify("no changes on non existing objects");
+      }
+
+      if(event.event_type === "delete") {
+        lively.notify("no deletes on non existing objects");
+      }
+    }
+    
+    this.updateViz();
+  }
+  
+  
+  
+  
+  
+  
+  
+  updateViz() {
+    
+    //var object_container = this.get("#objectCntr");
+    //object_container.style.backgroundColor = "red";
+    //object_container.innerHTML=""
+    
+    //var e = document.createElement('div');
+    //e.innerHTML = "Object and Properties";
+    //object_container.appendChild(e.firstChild);
+   
+    
+    /*
+    var objCntr = d3.select(this.get("svg"))
+      .attr("width", width + margin.right + margin.left)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+    */
+    
+    /*
+    var svgContainer = this.get("#container")
+    svgContainer.style.width = this.style.width // hard to find out how to do this in CSS, ... with "relative"
+    svgContainer.style.height = this.style.height
+    
+    
+    //var bounds = this.getBoundingClientRect()
+    //this.get("svg").innerHTML = ""
+    var knownObjects = this.getData()
+    if (!knownObjects) return; // nothing to render
+    
+    
+    var object_container = this.get("#objectCntr")
+    //object_container.style.backgroundColor = "red";
+    */
+    
+    
+    /*
+    var margin = { top: 20, right: 20, bottom: 20, left: 20 }
+    var width = bounds.width,
+      height = bounds.height;
+    
+    var data = this.getData();
+    if (!data) return;// nothing to to
+    var width = lively.getExtent(this).x - 30
+    
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.x1)])
+        .range([0, width]);
+    
+    var lineHeight = 20
+    
+    
+    var svgOuter = d3.select(this.get("svg"))
+      .attr("width", width + margin.right + margin.left)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
+    */
+  }
+    
+  
+  
+  
+  
+  /*
+  
+  
+  
+  
+import Stroboscope from 'src/client/stroboscope/stroboscope.js';    
+var tt = {name:"name", length:8, width:4, height:2};
+var stroboscope = new Stroboscope(tt)
+stroboscope.init().then(() => {
+  stroboscope.slice();
+})
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  async livelyExample() {
+    var tt = {name:"name", length:8, width:4, height:2};
+    var stroboscope = new Stroboscope(tt)
+    stroboscope.init().then(() => {
+      stroboscope.slice();
+    })
+  }
+  */
+  
 }
--- src/components/tools/lively-stroboscope.js
+++ src/components/tools/lively-stroboscope.js
@@ -1,6 +1,19 @@
 import Morph from "src/components/widgets/lively-morph.js"
 import d3 from "src/external/d3.v5.js"
 
+class ObjectView
+{
+  constructor(event){
+    this.id = event.object_id
+    this.events = []
+    this.events.push(event)
+  }
+  
+  append(event){
+    this.events.push(event)
+  }
+}
+
 
 export default class LivelyStroboscope extends Morph {
 
@@ -29,17 +42,44 @@ export default class LivelyStroboscope extends Morph {
 
   async initialize() {
     
-    var object1 = {offset : 0}
-    var object2 = {offset : 110}
-    var object3 = {offset : 220}
-
+    this._objectViewsMap = new Map();
     
-    this.objects = [object1, object2, object3]
+    this._addObjectView(new ObjectView({object_id : 0}))
+    this._addObjectView(new ObjectView({object_id : 1}))
+    this._addObjectView(new ObjectView({object_id : 2}))
+    
     this.updateViz()
 
   }
 
+  onEvents(events){
+    for(var i = 0; i < events.length; ++i){
+      this._handleEvent(events[i])
+    }
+    
+    this.updateViz()
+  }
+  
   updateViz() {
+    this._updateSVGViz()
+    this._updateObjectsViz()
+  }
+
+  _handleEvent(event){
+    if(event.object_id in this._objectViewsMap){
+      this._objectViewsMap[event.object_id].append(event);
+    }
+    else
+    {
+      this._addObjectView(new ObjectView(event));
+    }
+  }
+  
+  _addObjectView(objectView){
+    this._objectViewsMap.set(objectView.id, objectView)
+  }
+  
+  _updateSVGViz(){
     var bounds = this.getBoundingClientRect()
     this.shadowRoot.querySelector("svg").innerHTML = ""
 
@@ -54,83 +94,51 @@ export default class LivelyStroboscope extends Morph {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     this.svg = svg
     this.duration = 750
-    this.updateObjects()
-
-    //this.i = 0;
-
-    //var root;
-
-    //var treemap = d3.tree().size([height, width]);
-    //this.treemap = treemap
-
-    //root = d3.hierarchy(treeData, d => d.children);
-    //root.x0 = height / 2;
-    // root.y0 = 0;
-    // this.root = root
-
-    // Collapse after the second level
-    // root.children && root.children.forEach(ea => this.collapse(ea));
-
-    //this.update(root);
-    // d3.select(self.frameElement).style("height", "500px");    
   }
-
-  // Collapse the node and all it's children
-  collapse(d) {
-    if (d.children) {
-      d._children = d.children
-      d._children.forEach(ea => this.collapse(ea))
-      d.children = null
-    }
-  }
-
-  // Toggle children on click.
-  click(d) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-    this.update(d);
-  }
-
-  updateObjects() {
-
-    // Update the nodes…
-    var node = this.svg.selectAll("g.node")
-      .data(this.objects);
+  
+  _updateObjectsViz() {
+    this._reset_offset()
+    
+    var objects = this.svg.selectAll("g.object")
+      .data(this._objectViews());
 
     // Enter any new nodes at the parent's previous position.
-    var nodeEnter = node.enter().append("g")
+    var objectsEnter = objects.enter().append("g")
       .attr("class", "object")
-      .attr("transform", d => "translate(" + 0 + "," + d.offset + ")")
-      .on("click", (d) => this.click(d));
+      .attr("transform", d => "translate(" + 0 + "," + this._next_offset() + ")")
 
-    // Add Circle for the nodes
-    nodeEnter.append("rect")
+    objectsEnter.append("rect")
       .attr('class', 'object')
-      .attr("width", 50)
-      .attr("height", 100)
 
     // Add labels for the nodes
-    nodeEnter.append("text")
+    objectsEnter.append("text")
       .attr("x", 10)
-      .attr("dy", 10)
-      .text(d => this.id ? d.id : "undefined id")
+      .attr("dy", 20)
+      .text(d => this.id !== undefined ? "ID: " + d.id : "undefined id")
     
     // UPDATE
-    var nodeUpdate = nodeEnter.merge(node)
+    var objectsUpdate = objectsEnter.merge(objects)
 
     // Update the node attributes and style
-    nodeUpdate.select("rect.object")
-      .attr("width", 50)
-      .attr("height", 100)
-      .style("fill", "lightgreen")
-      .attr('cursor', 'pointer');
+    objectsUpdate.select("rect.object")
+      .attr("width", 100)
+      .attr("height", 30);
   }
 
+  _objectViews(){
+    return Array.from(this._objectViewsMap.values());
+  }
+  
+  _next_offset() {
+    var o = this._offset
+    this._offset += 50
+    return o
+  }
+  
+  _reset_offset() {
+    this._offset = 0
+  }
+  
   update(source) {
 
     // Assigns the x and y position for the nodes
@@ -185,16 +193,6 @@ export default class LivelyStroboscope extends Morph {
     // nodeUpdate.select("text")
     //   .style("fill-opacity", 1);
 
-  }
-
-  // Creates a curved (diagonal) path from parent to the child nodes
-  diagonal(s, d) {
-    var path =
-      `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`
-    return path
   }
 
   livelyExample() {
