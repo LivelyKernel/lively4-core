6d2f3bd55517e20e7399f671a571b3b312ca7175
onsetsu <stefan.ramson@gmail.com>
2019-01-30 14:39:13 +0100

    pre refactoring jsx ray
    SQUASHED: AUTO-COMMIT-src-client-reactive-components-basic-jsx-ray.html,AUTO-COMMIT-src-client-reactive-components-basic-jsx-ray.js,AUTO-COMMIT-src-client-reactive-components-basic-node-ancestry-list.html,AUTO-COMMIT-src-client-reactive-components-basic-node-ancestry-list.js,AUTO-COMMIT-src-client-reactive-reactive-jsx-reactive-jsx.js,

---
+++ src/client/reactive/components/basic/node-ancestry-list.js
@@ -0,0 +1,9 @@
+"enable aexpr";
+
+import Morph from 'src/components/widgets/lively-morph.js';
+
+export default class NodeAncestryList extends Morph {
+  async initialize() {
+    this.windowTitle = "NodeAncestryList";
+  }
+}
