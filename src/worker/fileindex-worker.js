00eda2fd958b662e4f3fc515b5bc060bddfab9af
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-02-20 15:48:10 +0100

    disable FileIndexAnalysis for now
    SQUASHED: AUTO-COMMIT-src-worker-fileindex-worker.js,

--- src/worker/fileindex-worker.js
+++ src/worker/fileindex-worker.js
@@ -1,24 +1,24 @@
 
-
-import FileIndexAnalysis from "src/client/fileindex-analysis.js"
-import FileCache from "src/client/fileindex.js"
+// disabled for now, will be merged into FileIndex #TODO
+// import FileIndexAnalysis from "src/client/fileindex-analysis.js"
+import FileIndex from "src/client/fileindex.js"
 
 export function onmessage(evt) {
   var msg = evt.data
   if (msg.message == "updateDirectory") {
-    FileCache.current().updateDirectory(msg.url).then(() => {
+    FileIndex.current().updateDirectory(msg.url).then(() => {
       postMessage({message: "updateDirectoryFinished", url: msg.url})
     })
-    FileIndexAnalysis.current().updateDirectory(msg.url).then(() => {
-      postMessage({message: "FileIndexAnalysis - updateDirectoryFinished", url: msg.url})
-    })
+    // FileIndexAnalysis.current().updateDirectory(msg.url).then(() => {
+    //   postMessage({message: "FileIndexAnalysis - updateDirectoryFinished", url: msg.url})
+    // })
   } else if (msg.message == "updateFile") {
-    FileCache.current().updateFile(msg.url).then(() => {
+    FileIndex.current().updateFile(msg.url).then(() => {
       postMessage({message: "updateFileFinished", url: msg.url})
     })
-     FileIndexAnalysis.current().updateFile(msg.url).then(() => {
-      postMessage({message: "FileIndexAnalysis - updateFileFinished", url: msg.url})
-    })
+    // FileIndexAnalysis.current().updateFile(msg.url).then(() => {
+    //   postMessage({message: "FileIndexAnalysis - updateFileFinished", url: msg.url})
+    // })
   } else {
     console.log("FileIndex message not understood", msg)
   }
