a1ccd05dbd0eb01ddc1ffb27bbd2fd2c4e37d3ee
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-05-09 11:17:49 +0200

    fetch proxy again
    SQUASHED: AUTO-COMMIT-index.md,fetch-proxy-again,

--- src/worker/service-worker.js
+++ src/worker/service-worker.js
@@ -1,4 +1,34 @@
+console.log("NEW SERVICE Worker")
+
+/* ## Workflow to edit / run this service worker
+
+1. edit file in lively
+2. check "Update on reload" in ![](chrome_debugger_tools.png)
+3. goto/open the "[...]/src/worker/service-worker.js" in a browser and press "F5"/reload 
+
+*/
 
 
+self.addEventListener('fetch', (evt) => {
+  
+  var url = evt.request.url 
+  console.log("[swx] fetch " +  evt.request.method  + " " + url)  
+  
+  var method = evt.request.method
+  if (url.match(/^https\:\/\/lively4\/scheme/,"")) {
+    
+    if(method == "GET") {
+      console.log("[swx] POID GET " + url)  
+      
+    } else if(method == "PUT") {
+      console.log("[swx] POID PUT " + url)  
+      
+    } else if(method == "OPTIONS") {    
+      console.log("[swx] POID OPTIONS " + url)  
+    }
+  }
 
-// do nothing?
+  // event.respondWith(promise);
+  
+})
+
--- swx-loader.js
+++ swx-loader.js
@@ -6,7 +6,11 @@ var pendingRequests  = []; // will be used in boot and unset in swx
 var startSwxTime = Date.now();
 
 try {
-  importScripts('swx-boot.js');
+  
+  // importScripts('swx-boot.js');
+  
+  importScripts('src/worker/service-worker.js');
+  
 } catch(e) {
   debugger
 }
