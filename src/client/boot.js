fdf142b660975158d1d32de86ffa3a579a9d3279
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-05-14 12:12:12 +0200

    don_t cache versioned file access
    SQUASHED: AUTO-COMMIT-boot.js,

--- src/client/boot.js
+++ src/client/boot.js
@@ -254,14 +254,17 @@ function installCachingFetch() {
         }) 
         if (!self.lively4syncCache) return
         if (method == "GET") {
-          var match = self.lively4syncCache.get(url)
+          if (options && options.headers && options.headers["fileversion"]) {
+            return // don't cache versions request...
+          }
+          let match = self.lively4syncCache.get(url)
           if (match) {
             // console.log("[boot] SYNC CACHED " + url)
             return {
               result: Promise.resolve(match.clone())
             }          
           } else {
-            // console.log("[boot] SYNC MISSED " + url)
+            console.log("[boot] SYNC MISSED " + url)
           }          
         } else if (method == "PUT") {
           // clear cache for PUT
@@ -274,14 +277,18 @@ function installCachingFetch() {
           
           // and don't further handle it... so that it will be saved on the server
         } else if (method == "OPTIONS") {
-          var match = self.lively4optionsCache.get(url)
+          if (options && options.headers && options.headers["showversions"]) {
+            return // don't cache versions request...
+          }
+          
+          let match = self.lively4optionsCache.get(url)
           if (match) {
             // console.log("[boot] SYNC OPTIONS CACHED " + url)
             return {
               result: Promise.resolve(match.clone())
             }          
           } else {
-            // console.log("[boot] SYNC OPTIONS MISSED " + url)
+            console.log("[boot] SYNC OPTIONS MISSED " + url)
           }      
         } else {
           // do nothing
@@ -293,92 +300,6 @@ function installCachingFetch() {
     }
   })
 }
-
-
-async function installProxyFetch() {
-  var focalStorage = (await System.import("src/external/focalStorage.js")).default
-  
-  var mounts = await focalStorage.getItem("lively4mounts")
-  
-  if (!mounts) return 
-  
-  self.lively4fetchHandlers = self.lively4fetchHandlers.filter(ea => !ea.isProxyFetch);
-  
-  async function proxyRequest(url, options={}) {
-    console.log("PROXY reqest: " + options.method + " " + url)
-    return self.originalFetch(url, {
-        mode: options.mode,
-        cache: options.cache,
-        method: options.method,
-        headers: options.headers,
-        redirect: options.redirect,
-        referrer: options.referrer,
-        credentials: options.credentials,
-        body: options.body && await options.body
-      })
-  }
-  
-  self.lively4fetchHandlers.push({
-    isProxyFetch: true,
-    handle(request, options) {
-      var url = (request.url || request).toString()
-      var method = "GET"
-      if (options && options.method) method = options.method;
-      
-      var m = url.match(/^https:\/\/lively4(\/[^/]*)(\/.*)/)
-      if (m) {
-        console.log("proxy fetch " + url)
-        var mountPoint = m[1]
-        var rest = m[2]
-       
-        if (mountPoint == "/sys" && rest == "/mounts") {
-          if (method == "GET") {
-            return {
-              result: new Response(JSON.stringify(mounts, null, 2))
-            }            
-          } else if (method == "PUT") {
-            
-            return {
-              result: Promise.resolve().then(async json => {
-                try {
-                  var json = options.body
-                  var newMounts = JSON.parse(json)
-                } catch(e) {
-                  // json could not be parsed
-                }
-                if (newMounts) {
-                  mounts = newMounts
-                  await focalStorage.setItem("lively4mounts", mounts)
-                  return new Response("updated mounts", {status: 200})
-                  
-                } else {
-                  return new Response("could not parse json: " + json, {status: 500})
-                }
-              })
-            }
-          }
-        }
-        
-        for (var proxy of mounts.filter(ea => ea.name == "http")) {
-          if (mountPoint == proxy.path) {
-            if (!proxy.options || !proxy.options.base) 
-              throw new Error("options.base is missing in mount config for " + mountPoint)
-            return {
-              result: proxyRequest(proxy.options.base + rest, options)
-            }  
-          }
-        }
-        // give SWX a chance to handle POID requests...
-        // return  {
-        //   result: new Response("Could not handle " + url)
-        // }          
-      }
-    }
-  })
-}
-
-// installProxyFetch()
-
 
 self.lively4invalidateFileCaches = invalidateFileCaches
 
@@ -417,7 +338,7 @@ if (self.lively && self.lively4url) {
     self.lively4bootGroupedMessages = []
     var lastMessage
     
-    var estimatedSteps = 10;
+    var estimatedSteps = 9;
     var stepCounter = 1;
     
     function groupedMessage( message) {
@@ -471,17 +392,12 @@ if (self.lively && self.lively4url) {
         await preloadFileCaches()
         // we could wait, or not... if we load transpiled things... waiting is better
       groupedMessageEnd();
-    
-      
+
       groupedMessage('Setup SystemJS');
         await loadJavaScriptThroughDOM("systemjs", lively4url + "/src/external/systemjs/system.src.js");
         await loadJavaScriptThroughDOM("systemjs-config", lively4url + "/src/systemjs-config.js");
       groupedMessageEnd();
 
-      groupedMessage('Setup fetch proxy');
-        await installProxyFetch()
-      groupedMessageEnd();
-    
       try {  
           groupedMessage('Initialize SystemJS');
             await System.import(lively4url + "/src/client/preload.js");
