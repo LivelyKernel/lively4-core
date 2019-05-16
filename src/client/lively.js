614634e33c21c691617d424302a7ff4bcf439e5c
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-05-16 10:23:33 +0200

    fixed unloadModule  to prevent trying to reloading a module a second time if there was an error #375
    SQUASHED: AUTO-COMMIT-src-client-lively.js,

--- src/client/lively.js
+++ src/client/lively.js
@@ -133,12 +133,14 @@ export default class Lively {
   static async unloadModule(path) {
     var normalizedPath = System.normalizeSync(path)
     try {
-      // #BUG this leads to trying to reloading a module a second time if there was an error #375
-      await System.import(normalizedPath).then(module => {
-        if(module && typeof module.__unload__ === "function") {
-          module.__unload__();
-        }
-      });
+      // check, to prevent trying to reloading a module a second time if there was an error #375
+      if (System.get(normalizedPath)) {
+        await System.import(normalizedPath).then(module => {
+          if(module && typeof module.__unload__ === "function") {
+            module.__unload__();
+          }
+        });        
+      }      
     } catch(e) {
       console.log("WARNING: error while trying to unload " + path)
     }
