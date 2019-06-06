5b5ba77112e9adfbe816035f95070268b526821e
onsetsu <stefan.ramson@gmail.com>
2019-06-06 12:01:41 +0200

    traverseAsAST
    SQUASHED: AUTO-COMMIT-src-client-lang-lang-ext.js,AUTO-COMMIT-src-client-lang-lang.js,AUTO-COMMIT-src-client-reactive-components-basic-codemirror-playground-example.js,AUTO-COMMIT-src-client-reactive-components-basic-codemirror-playground.js,

--- src/client/reactive/components/basic/codemirror-playground-example.js
+++ src/client/reactive/components/basic/codemirror-playground-example.js
@@ -1,9 +1,16 @@
 "enable aexpr";
-
 let x = 0;
 {
   let x = 6;
 }
 x = 76;
 
+let ae = aexpr(() => x);
+ae.onChange(lively.notify);
+
+
+let ae2 = aexpr(() => x);
+
+dsadw
+foo + bar
 that.prepend(<span>dkljwldjw</span>)
