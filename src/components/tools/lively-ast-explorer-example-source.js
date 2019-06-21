0e441888dc8b4aa319cc1cad771b1d96265c6671
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-06-21 15:13:16 +0200

    fixed lively script bound eval
    SQUASHED: AUTO-COMMIT-demos-hello.md,AUTO-COMMIT-demos-hello2.md,AUTO-COMMIT-src-components-tools-lively-ast-explorer-example-source.js,AUTO-COMMIT-src-components-widgets-lively-script.js,AUTO-COMMIT-src-external-babel-plugin-var-recorder.js,

--- src/components/tools/lively-ast-explorer-example-source.js
+++ src/components/tools/lively-ast-explorer-example-source.js
@@ -1,13 +1,1 @@
-that; import { location } from 'utils';
-
-
-
-let thatLoc = undefined;
-that.value.traverseAsAST({
-  Identifier(path) {
-    if (path.node.name === 'thatLoc') {
-      thatLoc = path.node.loc.start
-    }
-  }
-})
-Promise.resolve(thatLoc)
+var world = 3
