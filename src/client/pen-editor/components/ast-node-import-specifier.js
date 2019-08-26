37d36a66e213f20f43b7427bff8fc593a25da831
onsetsu <stefan.ramson@gmail.com>
2019-08-26 15:36:27 +0200

    factor out removeSubElementInSlot
    SQUASHED: AUTO-COMMIT-src-client-pen-editor-components-ast-node-import-specifier.js,

--- src/client/pen-editor/components/ast-node-import-specifier.js
+++ src/client/pen-editor/components/ast-node-import-specifier.js
@@ -10,17 +10,13 @@ export default class AstNodeImportSpecifier extends AbstractAstNode {
   
   async updateProjection() {
     const aliased = this.path.node.local.name !== this.path.node.imported.name;
-
     this.classList.toggle('basic', !aliased);
-
     if (aliased) {
       await this.createSubElementForPath(this.path.get('local'), 'local');
     } {
-      const local = this.get(':scope > [slot=local]');
-      if (local) {
-        local.remove();
-      }
+      this.removeSubElementInSlot('local')
     }
+
     await this.createSubElementForPath(this.path.get('imported'), 'imported');
   }
   
