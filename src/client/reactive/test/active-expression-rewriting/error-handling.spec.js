4b9af7c5b38eab5a45da0f481ebefe715bbcf0ee
onsetsu <stefan.ramson@gmail.com>
2019-10-01 15:59:13 +0200

    test file for error handling
    SQUASHED: AUTO-COMMIT-src-client-reactive-test-active-expression-rewriting-error-handling.js,AUTO-COMMIT-src-client-reactive-test-active-expression-rewriting-error-handling.spec.js,

---
+++ src/client/reactive/test/active-expression-rewriting/error-handling.spec.js
@@ -0,0 +1,37 @@
+'enable aexpr';
+'use strict';
+
+import chai, {expect} from 'src/external/chai.js';
+import sinon from 'src/external/sinon-3.2.1.js';
+import sinonChai from 'src/external/sinon-chai.js';
+chai.use(sinonChai);
+
+describe('Error in Expression analysis', () => {
+
+  xit('throwing an error does not update ', () => {
+    let value = 17
+    const spy = sinon.spy();
+    const ae = aexpr(() => {
+      return value;
+    }).onChange(spy)
+
+    value = 42;
+
+    expect(spy).to.have.been.calledOnce;
+  });
+
+});
+describe('Error in storage routine', () => {
+
+  it('read 0', () => {
+    expect(0).to.equal(0);
+  });
+
+});
+describe('Error in callbacks', () => {
+
+  it('read 0', () => {
+    expect(0).to.equal(0);
+  });
+
+});
