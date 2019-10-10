426eb27184dbc0fbe48fb420e061528bf7b42ba2
onsetsu <stefan.ramson@gmail.com>
2019-10-10 14:17:02 +0200

    ast explorer example
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-ast-explorer-example-source.js,

--- src/components/tools/lively-ast-explorer-example-source.js
+++ src/components/tools/lively-ast-explorer-example-source.js
@@ -1,15 +1,3 @@
-// l: foo;
-// () => foo.bar + foo.baz;
-
-class X {
-  method() {
-    // var foo = 42;
-    // () => {
-    //   foo;
-    // }
-    // if (false) foo.bar
-    // while (false) foo
-    // for (let x of [1,2]) foo.bar;
-    (baz) => foo.bar + 42 + baz
-  }
+() => {
+  class Foo {}
 }
