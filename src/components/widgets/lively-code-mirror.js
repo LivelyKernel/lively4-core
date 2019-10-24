7a0071585dfb7db31c22bbd1b7d91d6c8b013371
Markus-Brand <markusbrand97@gmail.com>
2019-10-24 15:29:31 +0200

    CodeMirror_ Add reduce selection capability and extract finding selected path
    SQUASHED: AUTO-COMMIT-src-components-widgets-lively-code-mirror-ast-capabilities.js,AUTO-COMMIT-src-components-widgets-lively-code-mirror.js,

--- src/components/widgets/lively-code-mirror.js
+++ src/components/widgets/lively-code-mirror.js
@@ -363,6 +363,7 @@ export default class LivelyCodeMirror extends HTMLElement {
         },
         // #KeyboardShortcut Alt-Down 
         "Alt-Down": cm => {
+          this.astCapabilities(cm).then(ac => ac.reduceSelection(cm));
         },
         // #KeyboardShortcut Alt-Enter ast refactoring/autocomplete menu
         "Alt-Enter": cm => {
