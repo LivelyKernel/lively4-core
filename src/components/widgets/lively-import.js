15b4e4a20ad0b5f2ef1d1f7cab856a3adc2d6312
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-10-30 17:10:35 +0100

    [doc] reworked issue page
    SQUASHED: AUTO-COMMIT-doc-issues.md,doc-reworked-issue-page,

--- src/components/widgets/lively-import.js
+++ src/components/widgets/lively-import.js
@@ -2,17 +2,29 @@ import Morph from 'src/components/widgets/lively-morph.js';
 
 export default class LivelyImport extends Morph {
   async initialize() {
-    var url = this.getAttribute("src")
+    lively.html.registerAttributeObservers(this)
+    this.update()
+  }
+      
+  async onSrcChanged() {
+    this.update()
+  }
+  
+  async update() {
+    let url = this.getAttribute("src")
     if (!url) return;
-    var src = await fetch(url).then(r => r.text())
+    let src = await fetch(url).then(r => r.text())
+    
+    if (url !== this.getAttribute("src")) return; // check if we are still on the same url...
     this.shadowRoot.innerHTML = "" + src 
-    var container = lively.query(this, "lively-container")
+    let container = lively.query(this, "lively-container")
     if (container) {
-      var dir = url.replace(/[^/]*$/,"")
-      lively.notify("IMPORT DIR " + dir)
+      let dir = url.replace(/[^/]*$/,"")
       lively.html.fixLinks(this.shadowRoot.childNodes, dir, 
         (path) => container.followPath(path))
     }
     await lively.components.loadUnresolved(this.shadowRoot, false, "lively-import", true);
   }
+  
+  
 }
