f77c1c7c9ee4c5a82cf3e17c9ffbddaa80722a55
ekrebs5 <eva.krebs@student.hpi.de>
2019-11-02 16:17:47 +0100

    halo item does something
    SQUASHED: AUTO-COMMIT-src-components-halo-lively-halo-connectors-item.js,

--- src/components/halo/lively-halo-connectors-item.js
+++ src/components/halo/lively-halo-connectors-item.js
@@ -1,6 +1,7 @@
 "enable aexpr";
 
 import HaloItem from 'src/components/halo/lively-halo-item.js';
+//import {pt} from './graphics.js';
 
 export default class LivelyHaloConnectorsItem extends HaloItem {
   async initialize() {
@@ -8,10 +9,29 @@ export default class LivelyHaloConnectorsItem extends HaloItem {
 
     this.registerEvent('click', 'onClick')
   }
-  
-  
-  onClick(){
-    lively.notify("hi")
-  }
-  
+   
+  onClick(evt) {
+      var target = window.that;
+      //lively.openContextMenu(document.body, evt, inspectTarget)
+      
+      let slider = target;
+    
+    
+      let worldContext = lively.findWorldContext(target);
+      let morph  = document.createElement("div");
+          morph.style.width = "200px";
+          morph.style.height = "100px";
+          morph.style.border = "1px solid black"
+          
+          worldContext.appendChild(morph);
+          //lively.setGlobalPosition(morph, pt(evt.clientX, evt.clientY)
+          //  .subPt(lively.getExtent(morph).scaleBy(0.5)))
+          morph.style.backgroundColor = 'rgba(40,40,80,0.5)';
+          lively.hand.startGrabbing(morph, evt)
+
+      let ae1 = aexpr(() => slider.value);
+      ae1.onChange(svalue => morph.style.width= svalue+"pt")
+    
+      this.hideHalo();
+    }
 }
