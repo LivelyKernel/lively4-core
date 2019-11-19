31ec8be1207cc08762bdddbdfecf1520c71dc040
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-11-19 11:42:56 +0100

    [base] no notificatrions in karma run
    SQUASHED: AUTO-COMMIT-src-client-lively.js,AUTO-COMMIT-src-components-tools-lively-ast-explorer-example-source.js,AUTO-COMMIT-src-external-babel-plugin-var-recorder.js,AUTO-COMMIT-tempfile.js,

--- src/client/lively.js
+++ src/client/lively.js
@@ -358,17 +358,19 @@ export default class Lively {
       if (document.querySelector("lively-console")) {
         console.log(error)
       } else {
-        console.error('#########################################', error, error.stack);
-        await lively.notify("Error: ", error, 10, () => {
-        		lively.openComponentInWindow("lively-error").then( comp => {
-              comp.stack =  error.stack
-              comp.parentElement.setAttribute("title",  "" + error.message)
-              comp.style.height = "max-content"
-              var bounds = comp.getBoundingClientRect()
-              comp.parentElement.style.height = (bounds.height + 20)+ "px"
-              comp.parentElement.style.width = bounds.width + "px"
-            })
-          }, "red");
+        console.error('[error] ', error, error.stack);
+        if (!window.__karma__) {
+          await lively.notify("Error: ", error, 10, () => {
+              lively.openComponentInWindow("lively-error").then( comp => {
+                comp.stack =  error.stack
+                comp.parentElement.setAttribute("title",  "" + error.message)
+                comp.style.height = "max-content"
+                var bounds = comp.getBoundingClientRect()
+                comp.parentElement.style.height = (bounds.height + 20)+ "px"
+                comp.parentElement.style.width = bounds.width + "px"
+              })
+            }, "red");
+        }
       }
     } catch(e) {
       console.log("An error happend while handling and error: " + e)
