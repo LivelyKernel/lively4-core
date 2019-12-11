e96e480f9c6d7fa60bd824a06aaea9b99896e55a
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2019-12-11 13:34:07 +0100

    removed service worker test AND markdown sourcemap WIP
    SQUASHED: removed-service-worker-test,

--- src/external/markdown-it-sourcemap.js
+++ src/external/markdown-it-sourcemap.js
@@ -2,9 +2,13 @@ const sourceMapPlugin = (md) => {
   const temp = md.renderer.renderToken.bind(md.renderer)
   md.renderer.renderToken = function (tokens, idx, options) {
     let token = tokens[idx]
-    if (token.level === 0 && token.map !== null && token.type.endsWith('_open')) {
+    // if (token.level === 0 && token.map !== null && token.type.endsWith('_open')) {
+    //   token.attrPush(['data-source-line', token.map[0] + 1])
+    // }
+    if (token.map !== null && token.type.endsWith('_open')) {
       token.attrPush(['data-source-line', token.map[0] + 1])
     }
+    
     return temp(tokens, idx, options)
   }
 }
