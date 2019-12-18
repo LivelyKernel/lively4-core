c9add346741e75731c329a04bde37d7210142dda
jonasgrunert <jonas@grunert-berlin.de>
2019-12-18 16:01:19 +0100

    Saving scripts initial work
    SQUASHED: AUTO-COMMIT-src-client-contextmenu.js,AUTO-COMMIT-src-client-vivide-components-index.md,AUTO-COMMIT-src-client-vivide-components-vivide-test-widget.html,AUTO-COMMIT-src-client-vivide-components-vivide-test-widget.js,AUTO-COMMIT-src-client-vivide-scripts-index.md,AUTO-COMMIT-src-client-vivide-scripts-scripts-vivide-script-example-test.json,AUTO-COMMIT-src-components-halo-lively-halo-vivide-save-script-item.html,AUTO-COMMIT-src-components-halo-lively-halo-vivide-save-script-item.js,AUTO-COMMIT-src-components-halo-lively-halo.html,AUTO-COMMIT-start.html,

---
+++ src/components/halo/lively-halo-vivide-save-script-item.js
@@ -0,0 +1,43 @@
+"enable aexpr";
+
+import HaloItem from 'src/components/halo/lively-halo-item.js';
+
+const basePath = "src/client/vivide/scripts/scripts/";
+
+export default class LivelyHaloVivideSaveScriptItem extends HaloItem {
+  async onClick(evt){
+    if(!this.name){
+      this.name = await lively.prompt("Please attach a name", "vivide-script-name");
+    }
+    const saveTarget = window.that;
+    /*
+    We have to save a multitude of things:
+    inputData
+    scriptSteps
+    And for applications:
+    input targets
+    output targets
+    */
+    const stringToSave = saveTarget.myCurrentScript.toJSON();
+    const url = `${lively4url}/${basePath}${this.name}.json`;
+    const exists = await lively.files.exists(url);
+    if(exists){   
+      const confirm = await lively.confirm(`Are you sure you want to overwrite ${this.name}?`);
+      if(confirm){
+        this.saveFile(url, stringToSave);
+      }
+    } else {
+      this.saveFile(url, stringToSave);
+    }
+  }
+  
+  async saveFile(url, content){
+    const res = await lively.files.saveFile(url, content);
+    if(res.ok){
+      lively.success("Saved");
+    }else{
+      lively.error(await res.text())
+    }
+  }
+  
+}
