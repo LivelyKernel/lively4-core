5ffe416c78bc7ec62ba1e56586ce0d3f7ff54d63
KuSpa <wil.friedemann@gmail.com>
2019-12-19 14:49:02 +0100

    basic dependencygraph for correct AExpressiondependencies
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-ast-explorer-example-source.js,AUTO-COMMIT-src-components-tools-lively-ast-inspector.js,basic-dependencygraph-for-correct-AExpressiondependencies,

--- src/client/reactive/components/basic/codemirror-playground-example.js
+++ src/client/reactive/components/basic/codemirror-playground-example.js
@@ -1,21 +1,10 @@
 "enable aexpr";
 
-// Alt-A to show hints!
-
 let x = 1;
-let y = 2;
-y = 42;
-x = 3;
+let v = 1;
 
-{
-  let x = 4;
-  y = 4;
-  aexpr(() => x + y);
-  x = 42;
-  y = 3;
+function foo(){
+  return x
 }
-
-foo();
-
-x = 42;
-y = 13;
+aexpr(() => foo());
+x = 2;
