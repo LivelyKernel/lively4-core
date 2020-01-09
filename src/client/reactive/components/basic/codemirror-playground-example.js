f6dae437bf60254a69bad834e9b0cc1df88720a6
KuSpa <wil.friedemann@gmail.com>
2020-01-09 09:51:59 +0100

    finish aExpr gutter
    SQUASHED: AUTO-COMMIT-src-client-html.js,AUTO-COMMIT-src-client-reactive-components-basic-codemirror-playground-example.js,AUTO-COMMIT-src-client-reactive-components-basic-codemirror-playground.js,AUTO-COMMIT-src-components-widgets-lively-morph.js,

--- src/client/reactive/components/basic/codemirror-playground-example.js
+++ src/client/reactive/components/basic/codemirror-playground-example.js
@@ -5,13 +5,14 @@ let v = 1;
 
 function foo(){
   let tmp = 0;
-
+  v;
   x;
   return tmp;
 }
 aexpr(() => {return foo()});
-aexpr(() => {return foo()});
+aexpr(()=> v);
 
-x = 2;
+x=5;
+
 v = 1;
 x = 5;
