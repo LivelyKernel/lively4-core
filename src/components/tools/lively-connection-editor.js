f8cfc97272e1c1cf58d0e4488272de0f591582b1
ekrebs5 <eva.krebs@student.hpi.de>
2020-02-27 16:49:47 +0100

    Connection move changing s_t to buttons
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-connection-editor.html,AUTO-COMMIT-src-components-tools-lively-connection-editor.js,

--- src/components/tools/lively-connection-editor.js
+++ src/components/tools/lively-connection-editor.js
@@ -10,9 +10,7 @@ export default class LivelyConnectionEditor extends Morph {
     this.registerButtons();
     this.activityCheckbox.addEventListener("click", () => this.activeChanged());
     this.sourcePicture.addEventListener('mouseenter', () => this.startDrawingArrowToSource(this.sourcePicture.children[0], this.connection.getSource()))
-    this.sourcePicture.addEventListener("click", () => this.chooseNewElement(true));
     this.targetPicture.addEventListener('mouseenter', () => this.startDrawingArrowToSource(this.targetPicture.children[0], this.connection.getTarget()))
-    this.targetPicture.addEventListener("click", () => this.chooseNewElement(false));
     this.get("#labelField").value = this.getAttribute("data-mydata-label") || 0;
     this.trackingCodeField.editorLoaded().then(() => {this.trackingCodeField.doSave = () => this.saveConnection()})
     this.modifyingCodeField.editorLoaded().then(() => {this.modifyingCodeField.doSave = () => this.saveConnection()})
@@ -101,6 +99,14 @@ export default class LivelyConnectionEditor extends Morph {
 
   get activityCheckbox() {
     return this.get("#activityCheckbox");
+  }
+  
+  onNewSourceButton() {
+    this.chooseNewElement(true)
+  }
+  
+  onNewTargetButton() {
+    this.chooseNewElement(false)
   }
 
   onDrawConnectionArrowButton() {
