5fc2abc274aee9ddb734c7395a738c38fd849d5f
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2020-02-27 14:45:16 +0100

    hide plus minus in smaug
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-smaug.html,AUTO-COMMIT-src-components-tools-lively-smaug.js,

--- src/components/tools/lively-smaug.js
+++ src/components/tools/lively-smaug.js
@@ -254,9 +254,6 @@ export default class LivelySmaug extends Morph {
   async livelyExample() {
     // this customizes a default instance to a pretty example
     // this is used by the 
-    this.style.backgroundColor = "lightgray";
-    this.someJavaScriptProperty = 42;
-    this.appendChild(<div>This is my content</div>);
   }
 
 }
