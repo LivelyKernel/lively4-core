59173e123d2fb94fca048ddf5985dfb712b74151
onsetsu <stefan.ramson@gmail.com>
2020-05-29 10:25:00 +0200

    css reloading bug example
    SQUASHED: AUTO-COMMIT-src-components-widgets-css-reloading-test-widget.css,AUTO-COMMIT-src-components-widgets-css-reloading-test-widget.html,AUTO-COMMIT-src-components-widgets-css-reloading-test-widget.js,

--- src/components/widgets/css-reloading-test-widget.js
+++ src/components/widgets/css-reloading-test-widget.js
@@ -5,5 +5,7 @@ import Morph from 'src/components/widgets/lively-morph.js';
 export default class CssReloadingTestWidget extends Morph {
   async initialize() {
     this.windowTitle = "CssReloadingTestWidget";
+    
+    this.style.setProperty("--my-color", `steelblue`);
   }
 }
