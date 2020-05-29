6bdd61c61c2a64039b4984e4f5916b2d5908644f
onsetsu <stefan.ramson@gmail.com>
2020-05-29 18:38:23 +0200

    started refactoring pen editor styling
    SQUASHED: started-refactoring-pen-editor-styles,

--- src/components/widgets/css-reloading-test-widget.js
+++ src/components/widgets/css-reloading-test-widget.js
@@ -5,7 +5,8 @@ import Morph from 'src/components/widgets/lively-morph.js';
 export default class CssReloadingTestWidget extends Morph {
   async initialize() {
     this.windowTitle = "CssReloadingTestWidget";
-    
-    this.style.setProperty("--my-color", `steelblue`);
+
+    const color = [`green`, `blue`, `red`, `yellow`, `black`].sample();
+    this.style.setProperty("--my-color", `5px dashed ${color}`);
   }
 }
