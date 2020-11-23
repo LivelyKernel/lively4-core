3c876f540956c5b523ce761e34d309f38e8b6972
s4pu <leopab@gmx.de>
2020-11-23 10:54:30 +0100

    add first semantics
    SQUASHED: AUTO-COMMIT-demos-bibliographie-leoUIexample.js,

--- demos/bibliographie/leoUIexample.js
+++ demos/bibliographie/leoUIexample.js
@@ -18,14 +18,53 @@ export default class LeoUIExample{
 
     world.addEventListener('drop', drop);
 
-    var g = ohm.grammar('Academic { \n  Exp = \n    AcademicQuery \n \n  AcademicQuery = Attribute Comparator Value -- simpleQuery \n    | ("And" | "Or") "(" AcademicQuery "," AcademicQuery ")" -- complexQuery \n    | "Composite(" CompositeQuery ")" -- compositeQuery \n \n  CompositeQuery = CompositeAttribute Comparator Value -- simpleCompositeQuery \n    | ("And" | "Or") "(" CompositeQuery "," CompositeQuery ")" -- complexCompositeQuery \n \n  Comparator = \n    PartialComparator "="? \n  PartialComparator = \n    "=" | "<" | ">" \n \n  Attribute (an attribute) = \n    letter letter? letter? \n  CompositeAttribute = \n    Attribute "." Attribute \n \n \n  Value (a value) = \n    "\'" alnum* "\'" -- string \n    | Number \n    | Date \n    | ( "[" | "(" ) Number "," Number ( "]" | ")" ) -- numberRange \n    | ( "[" | "(" ) Date "," Date ( "]" | ")" ) -- dateRange \n \n  Number = \n    digit+ \n  Date = \n    "\'" digit digit digit digit "-" digit digit "-" digit digit "\'" \n}');
+    var g = ohm.grammar('Academic { \n  Exp = \n    AcademicQuery \n \n  AcademicQuery = Attribute Comparator Value -- simple \n    | ("And" | "Or") "(" AcademicQuery "," AcademicQuery ")" -- complex \n    | "Composite(" CompositeQuery ")" -- composite \n \n  CompositeQuery = CompositeAttribute Comparator Value -- simple \n    | ("And" | "Or") "(" CompositeQuery "," CompositeQuery ")" -- complex \n \n  Comparator = \n    PartialComparator "="? \n  PartialComparator = \n    "=" | "<" | ">" \n \n  Attribute (an attribute) = \n    letter letter? letter? \n  CompositeAttribute = \n    Attribute "." Attribute \n \n \n  Value (a value) = \n    "\'" alnum* "\'" -- string \n    | Number \n    | Date \n    | ( "[" | "(" ) Number "," Number ( "]" | ")" ) -- numberRange \n    | ( "[" | "(" ) Date "," Date ( "]" | ")" ) -- dateRange \n \n  Number = \n    digit+ \n  Date = \n    "\'" digit digit digit digit "-" digit digit "-" digit digit "\'" \n}');
     
-    lively.notify(g.match("Composite(And(AA.AuN='mike smith',AA.AfN='harvard university'))").succeeded(), "Composite(And(AA.AuN='mike smith',AA.AfN='harvard university'))");
-    lively.notify(g.match("And(AA.AuN='mike smith',AA.AfN='harvard university')").succeeded(), "And(AA.AuN='mike smith',AA.AfN='harvard university')");
+    /*lively.notify(g.match("Composite(And(AA.AuN='mike smith',AA.AfN='harvard university'))").succeeded(), "Composite(And(AA.AuN='mike smith',AA.AfN='harvard university'))");
+    lively.notify(g.match("And(AA.AuN='mike smith',AA.AfN='harvard university')").succeeded(), "And(AA.AuN='mike smith',AA.AfN='harvard university')");*/
     
+    //var queryObject = {};
     
-    // TODO: Add Semantics
+    var s = g.createSemantics();
     
+    s.addOperation(
+      'interpret', {
+        Exp: function(e) {
+          return e.interpret();
+        },
+        
+        AcademicQuery_simple: function(attr, comp, val) {
+          var attribute = attr.interpret();
+          //var comparator = comp.interpret();
+          //var value = val.interpret();
+          return { [attribute]: {
+            "comparator": "a",//comparator,
+            "value": "b"//value,
+          }}
+        },
+        AcademicQuery_complex: function(conj, x, left, y, right, z) {
+          var conjunction = conj.interpret();
+          
+          return {conjunction: [
+            left.interpret(),
+            right.interpret()
+          ]}
+        },
+        
+        Attribute: function(a, b, c) {
+          var array = [a.interpret(), b.interpret(), c.interpret()];
+          return array.join('');
+        },
+        
+        letter: function(a) {
+          return a.sourceString;
+        }
+      }
+    )
+    
+    var r = g.match("A = 'e'")
+    var n = s(r)
+    lively.notify("A = 'e'", n.interpret());
 
     function dragStart(event) {
       description.innerHTML = "dragging";
