d132b96a88d622f54f949d9ee258290b0b834e9e
s4pu <leopab@gmx.de>
2021-03-28 14:57:46 +0200

    [dice_roller] fix event handler stuff
    SQUASHED: AUTO-COMMIT-demos-leo-dice_roller.js,AUTO-COMMIT-demos-leo-dice_roller.md,

--- demos/leo/dice_roller.js
+++ demos/leo/dice_roller.js
@@ -1,13 +1,13 @@
 export default class DiceRoller {
   
-  onMouseDownOverAmount(evt, element) {
+  onMouseDownOverAmount(evt, currentAmount) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.amount = element.value;
-    var all = element.parentElement.parentElement.parentElement.querySelectorAll("div[name='value']")
+    var all = currentAmount.parentElement.parentElement.parentElement.querySelectorAll("div[name='value']")
     all.forEach(e => e.style.border = "1px lightgray solid")
-    lively.notify("ALL", all)
-    element.style.border = "1px black solid";
+    lively.notify("ELEMENT", currentAmount.value)
+    currentAmount.style.border = "1px black solid";
+    this.amount = currentAmount.value;
   }
   
   onMouseOver(evt) {
@@ -15,34 +15,32 @@ export default class DiceRoller {
   }
   
   create() {
-    
-    var value1 = <div name="value" style="width:max; text-align:center; background-color: lightgray; border: 1px lightgray solid; cursor: grab">1</div>
-    value1.value = 1;
-    
-    var value2 = <div name="value" style="width:max; text-align:center; background-color: lightgray; border: 1px lightgray solid; cursor: grab">2</div>
-    value2.value = 2;
-    
-    var del = <div style="width:max; text-align:right; cursor: pointer">X</div>;
-    var del2 = <div style="width:max; text-align:right; cursor: pointer">X</div>;
+    this.amounts = []
+    var table = <table style="width:100%"></table>
     var amount = <div style="padding:5px">
         <div style="border: 1px solid black; margin:auto">
-          <table style="width:100%">
-            <tr>
-              <td style="width: 33%"></td>
-              <td style="width: 33%">{value1}</td>
-              <td style="width: 33%">{del}</td>
-            </tr>
-            <tr>
-              <td style="width: 33%"></td>
-              <td style="width: 33%">{value2}</td>
-              <td style="width: 33%">{del2}</td>
-            </tr>
-          </table>
+          {table}
         </div>
       </div>
-    
-    value1.addEventListener('mousedown', (evt) => this.onMouseDownOverAmount(evt, value1));
-    value2.addEventListener('mousedown', (evt) => this.onMouseDownOverAmount(evt, value2));
+    for (var i = 1; i <= 4; i++) {
+      var value = <div name="value" style="width:max; text-align:center; background-color: lightgray; border: 1px lightgray solid; cursor: grab">{i}</div>
+      value.value = i;
+      var del = <div style="width:max; text-align:right; cursor: pointer">X</div>;
+      
+      this.amounts.push(value)
+      
+      const currentAmount = value
+      
+      value.addEventListener('mousedown', (evt) => this.onMouseDownOverAmount(evt, currentAmount));
+      
+      var cell = 
+          <tr>
+            <td style="width: 33%"></td>
+            <td style="width: 33%">{value}</td>
+            <td style="width: 33%">{del}</td>
+          </tr>
+      table.appendChild(cell)
+    }
     
     amount.addEventListener('mouseover', (evt) => this.onMouseOver(evt));
     
