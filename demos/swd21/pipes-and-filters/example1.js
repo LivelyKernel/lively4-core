460394de12548367f4c47e41e04565a1936a20b6
JensLincke <sven.kaestle@gmail.com>
2021-07-01 14:42:05 +0200

    AUTO-COMMIT demos/swd21/pipes-and-filters/example1.js

--- demos/swd21/pipes-and-filters/example1.js
+++ demos/swd21/pipes-and-filters/example1.js
@@ -1,10 +1,26 @@
 export default class Example1 {
   
-  static createView(context) {
     
-     context.querySelector("div").style.border = "5px solid blue"
+  static async createView(context) {
+    var counter = 0
+    
+    animation()
+    
     
     return <div>This is my View</div>
   }
+  
+  async animation() {
+    if (this.isAnimating) return
+    this.isAnimating=true
+    while(lively.isInBody(context)) {
+      context.querySelector("div").style.border = "5px solid blue"
+      
+      await lively.sleep(100)
+    }
+    this.isAnimating=false
+    
+  }
+  
   
 }
