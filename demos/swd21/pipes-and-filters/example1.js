15de7d5882c2aba22103b49f69140e0167ee0d6f
JensLincke <sven.kaestle@gmail.com>
2021-07-01 14:18:57 +0200

    AUTO-COMMIT demos/swd21/pipes-and-filters/example1.js

--- demos/swd21/pipes-and-filters/example1.js
+++ demos/swd21/pipes-and-filters/example1.js
@@ -2,7 +2,7 @@ export default class Example1 {
   
   static createView(context) {
     
-     context.querySelector("div").style.border = "5px solid green"
+     context.querySelector("div").style.border = "5px solid blue"
     
     return <div>This is my View</div>
   }
