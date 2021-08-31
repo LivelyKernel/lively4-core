3510e117f40b6f612037a192502950473d6304a3
onsetsu <stefan.ramson@gmail.com>
2021-08-31 15:07:00 +0200

    [autocomplete] complete for this with any member in file
    SQUASHED: AUTO-COMMIT-src-components-widgets-lively-code-mirror-hint.js,

--- src/components/widgets/lively-code-mirror-hint.js
+++ src/components/widgets/lively-code-mirror-hint.js
@@ -30,6 +30,7 @@ async function searchImports() {
 }
 searchImports();
 
+/*MD # Completions MD*/
 class Completions {
 
   constructor(cm) {
@@ -47,18 +48,18 @@ class Completions {
     var token = cm.getTokenAt(cursor);
 
     token.state = CodeMirror.innerMode(cm.getMode(), token.state).state;
-    // lively.notify(token.string, token.type);
 
-    // If it's not a 'word-style' token, ignore the token.
+    // lively.notify(token.string, token.type);
     if (token.type === 'string') {
       token = {
         start: token.start + 1,
-        end: token.end - 1,
-        string: token.string.slice(1, -1),
+        end: cursor.ch,
+        string: token.string.substring(1, cursor.ch - token.start),
         state: token.state,
         type: 'string'
       };
     } else if (!/^[\w$_]*$/.test(token.string)) {
+      // If it's not a 'word-style' token, ignore the token.
       token = {
         start: cursor.ch,
         end: cursor.ch,
@@ -70,13 +71,11 @@ class Completions {
       token.end = cursor.ch;
       token.string = token.string.slice(0, cursor.ch - token.start);
     }
+
     this.token = token;
   }
 
   maybeAdd(completion) {
-    // if (completion === 'lively.') {
-    //   debugger;
-    // }
     const simple = typeof completion === 'string';
     const text = simple ? completion : completion.text;
 
@@ -131,8 +130,6 @@ class Completions {
   }
 }
 
-import { fileName, hintForLabel, getTempKeyFor } from 'utils';
-
 import ColorHash from 'src/external/color-hash.js';
 const colorHash = new ColorHash({ saturation: 0.8, lightness: 0.8 });
 function colorForString(str) {
@@ -148,8 +145,6 @@ function forOrigin(completion, origin) {
 
   return Object.assign({
     render(element, self, data) {
-      debugger;
-
       if (completion.innerRender) {
         completion.innerRender(element, self, data);
       } else {
@@ -161,6 +156,7 @@ function forOrigin(completion, origin) {
   }, completion);
 }
 
+/*MD # CompletionsBuilder MD*/
 class CompletionsBuilder {
 
   constructor(cm) {
@@ -174,6 +170,13 @@ class CompletionsBuilder {
 
   async buildHint(options) {
     lively.notify("JS completion");
+    await this.collectCompletions(options
+
+    // lively.notify(this.completions.list.length, 'num completions');
+    );return this.completions.asCompletionsObject();
+  }
+
+  async collectCompletions(options) {
     const cm = this.cm;
     const completions = this.completions;
 
@@ -181,185 +184,210 @@ class CompletionsBuilder {
       completions.maybeAdd(str);
     }
 
+    var cursor = cm.getCursor();
+
+    const line = cm.getLine(cursor.line);
+    if (line.startsWith("import")) {
+      await this.completeFromImport(cm);
+      return;
+    }
+
+    var token = completions.token;
+    lively.notify('type: ' + token.type, 'string: ' + token.string);
+
+    if (token.type === 'string') {
+      cssProperties.map(property => forOrigin(property, 'cssProperties')).forEach(maybeAdd);
+      domEvents.map(property => forOrigin(property, 'domEvents')).forEach(maybeAdd);
+      return;
+    }
+
+    if (token.type === 'comment') {
+      return;
+    }
+
+    this.completeLively();
+
+    await this.completeFromTern();
+
+    var tprop = token;
+    const getToken = ::cm.getTokenAt;
+    // If it is a property, find out what it is a property of.
+    while (tprop.type == "property") {
+      tprop = getToken(Pos(cursor.line, tprop.start));
+      if (tprop.string != ".") {
+        break;
+      }
+      tprop = getToken(Pos(cursor.line, tprop.start));
+      if (!context) {
+        var context = [];
+      }
+      context.push(tprop);
+    }
+
+    if (context && context.length === 1 && context[0].string === 'this') {
+      this.completeMembersOfThis()
+    }
+
+    var global = options && options.globalScope || window;
+
+    function addObjectProp(obj, prop) {
+      let completion = prop;
+      const descriptor = Object.getOwnPropertyDescriptor(obj, prop);
+      let value = descriptor.value;
+
+      if (value) {
+        if (typeof value === 'function') {
+          value = value.toString().split('\n').first.substring(0, 50);
+        } else if (value.toString) {
+          value = value.toString().substring(0, 50);
+        }
+
+        completion = {
+          text: prop,
+          innerRender(element, self, data) {
+            element.append(<span>{prop} <span style="color: rgba(200, 200, 200, 0.9)">{value}</span></span>);
+          }
+        };
+      }
+
+      maybeAdd(forOrigin(completion, 'prop'));
+    }
+
+    function allProps(obj) {
+      for (var o = obj; o; o = Object.getPrototypeOf(o)) {
+        Object.getOwnPropertyNames(o).forEach(prop => addObjectProp(o, prop));
+      }
+    }
+
+    function gatherCompletions(obj) {
+      allProps(obj);
+
+      if (typeof obj == "string") {
+        stringProps.map(name => forOrigin(name, 'str')).forEach(maybeAdd);
+      } else if (obj instanceof Array) {
+        arrayProps.map(name => forOrigin(name, 'arr')).forEach(maybeAdd);
+      } else if (obj instanceof Function) {
+        funcProps.map(name => forOrigin(name, 'fun')).forEach(maybeAdd);
+      }
+    }
+
+    if (context && context.length) {
+      // If this is a property, see if it belongs to some object we can
+      // find in the current environment.
+      var obj = context.pop(),
+          base;
+
+      if (obj.type && obj.type.indexOf("variable") === 0 || obj.string == "this") {
+        if (options && options.additionalContext) base = options.additionalContext[obj.string];
+
+        if (base === undefined && obj && obj.string) {
+          // #TODO instead of doing a blackbox evaluation of a string, we could look into the various scopes... 
+          // in the module or live prgoramming data structures... e.g. from AST with trace data such as #ContinuousEditor
+          // documentation: getDoitContext() and getTargetModule()
+          var result = await options.codemirror.boundEval(obj.string);
+          base = result.value;
+        }
+
+        if (!options || options.useGlobalScope !== false) {
+          base = base || global[obj.string];
+        }
+      } else if (obj.type == "string") {
+        base = "";
+      } else if (obj.type == "atom") {
+        base = 1;
+      } else if (obj.type == "function") {
+
+        // #TODO make it work for funciton calls such as pt(10,10), too.... 
+        if (global.jQuery != null && (obj.string == '$' || obj.string == 'jQuery') && typeof global.jQuery == 'function') base = global.jQuery();else if (global._ != null && obj.string == '_' && typeof global._ == 'function') base = global._();
+      }
+      while (base != null && context.length) base = base[context.pop().string];
+      if (base != null) gatherCompletions(base);
+      return;
+    }
+
+    const c = cm.getCursor();
+    const prevToken = cm.getTokenAt({ line: c.line, ch: token.start - 0 });
+    const prevPrevToken = cm.getTokenAt({ line: c.line, ch: token.start - 1 });
+    if (prevToken.string == ":" && prevPrevToken.string == ":") {
+      this.completeUtilsFunctions(token);
+      return;
+    }
+
+    // If not, just look in the global object and any local scope
+    // (reading into JS mode internals to get at the local and global variables)
+
+    for (var v = token.state.localVars; v; v = v.next) {
+      maybeAdd(forOrigin(v.name, 'local'));
+    }
+    for (var w = token.state.globalVars; w; w = w.next) {
+      maybeAdd(forOrigin(w.name, 'global'));
+    }
+    if (!options || options.useGlobalScope !== false) {
+      gatherCompletions(global);
+    }
+
+    javaScriptKeywords.map(keyword => forOrigin(keyword, 'keyword')).forEach(maybeAdd);
+  }
+
+  completeMembersOfThis() {
+    let currentClass;
+    this.cm.eachLine(lineHandle => {
+      const classMatch = lineHandle.text.match(/^(?:\s|export\s|default\s)*class\s+([_$a-zA-z][_$a-zA-Z0-9]*).*\{$/);
+      if (classMatch) {
+        currentClass = classMatch[1];
+        // lively.notify(Object.getOwnPropertyNames(lineHandle));
+        return;
+      }
+
+      const match = lineHandle.text.match(/^\s*((?:async\s|get\s|set\s|static\s)*)([_$a-zA-z][_$a-zA-Z0-9]*)(\*?\s*\(.*\)\s*\{)$/);
+      if (match) {
+        const [, modifiers, name, argsAndBodyStart] = match;
+        if (javaScriptKeywords.includes(name) || name === 'constructor') {
+          return;
+        }
+
+        const myClass = currentClass;
+        const completion = {
+          text: name,
+          innerRender(element, self, data) {
+            function grayish(content) {
+              const style = "color: rgba(200, 200, 200, 0.9); display:inline-block; max-width: 50ch;";
+              return <span style={style}>{content}</span>
+            }
+            element.append(<span>{grayish(modifiers + (myClass ? myClass + '::' : ''))}{name}{grayish(argsAndBodyStart)}</span>);
+          }
+        };
+        // lively.notify(name);
+        this.maybeAdd(forOrigin(completion, 'member'));
+      }
+    });
+  }
+
+  async completeFromImport(cm) {
     const getCompletionsFromImports = async () => {
       // #TODO make faster with index and caching
       // but: #TODO what is the workflox / programming XP, we aim for?
       var text = await searchImports();
       var imports = text.split("\n").map(ea => ea.replace(/.*:/, "").replace(/;$/, "").replace(/"/g, "'")).filter(ea => !ea.match('\'\.\.?/')).filter(ea => ea.startsWith("import ")).sort()::uniq();
-      imports.map(i => forOrigin(i, 'import')).forEach(maybeAdd);
+      imports.map(i => forOrigin(i, 'import')).forEach(::this.maybeAdd);
     };
 
-    const autocompleteImport = async cm => {
-      const getToken = ::cm.getTokenAt;
+    const getToken = ::cm.getTokenAt;
 
-      // Find the token at the cursor
-      var cursor = cm.getCursor();
-      var token = getToken(cursor);
+    // Find the token at the cursor
+    var cursor = cm.getCursor();
+    var token = getToken(cursor);
 
-      // use a "line" token
-      completions.token = {
-        start: 0,
-        end: cursor.ch,
-        string: cm.getLine(cursor.line),
-        state: token.state,
-        type: null
-      };
-      await getCompletionsFromImports();
+    // use a "line" token
+    this.completions.token = {
+      start: 0,
+      end: cursor.ch,
+      string: cm.getLine(cursor.line),
+      state: token.state,
+      type: null
     };
 
-    /*MD # starting point MD*/
-    const javascriptHint = async (cm, options) => {
-
-      // Find the token at the cursor
-      var cursor = cm.getCursor();
-
-      // IMPORT
-      const line = cm.getLine(cursor.line);
-      if (line.startsWith("import")) {
-        await autocompleteImport(cm);
-        return;
-      }
-
-      var token = completions.token;
-      lively.notify('type: ' + token.type, 'string: ' + token.string);
-
-      if (token.type === 'string') {
-        cssProperties.map(property => forOrigin(property, 'cssProperties')).forEach(maybeAdd);
-        domEvents.map(property => forOrigin(property, 'domEvents')).forEach(maybeAdd);
-        return;
-      }
-
-      if (token.type === 'comment') {
-        return;
-      }
-
-      this.completeLively();
-      
-      await this.completeFromTern()
-
-      var tprop = token;
-      const getToken = ::cm.getTokenAt;
-      // If it is a property, find out what it is a property of.
-      while (tprop.type == "property") {
-        tprop = getToken(Pos(cursor.line, tprop.start));
-        if (tprop.string != ".") {
-          break;
-        }
-        tprop = getToken(Pos(cursor.line, tprop.start));
-        if (!context) {
-          var context = [];
-        }
-        context.push(tprop);
-      }
-
-      await getCompletions(token, context, options);
-    };
-
-    // #CodeMirror #AutoCompletion happens here!
-    const getCompletions = async (token, context, options) => {
-      var global = options && options.globalScope || window;
-
-      function addObjectProp(obj, prop) {
-        let completion = prop;
-        const descriptor = Object.getOwnPropertyDescriptor(obj, prop);
-        let value = descriptor.value;
-
-        if (value) {
-          if (typeof value === 'function') {
-            value = value.toString().split('\n').first.substring(0, 50);
-          } else if (value.toString) {
-            value = value.toString().substring(0, 50);
-          }
-
-          completion = {
-            text: prop,
-            innerRender(element, self, data) {
-              element.append(<span>{prop} <span style="color: rgba(200, 200, 200, 0.9)">{value}</span></span>);
-            }
-          };
-        }
-
-        maybeAdd(forOrigin(completion, 'prop'));
-      }
-
-      function allProps(obj) {
-        for (var o = obj; o; o = Object.getPrototypeOf(o)) {
-          Object.getOwnPropertyNames(o).forEach(prop => addObjectProp(o, prop));
-        }
-      }
-
-      function gatherCompletions(obj) {
-        allProps(obj);
-        
-        if (typeof obj == "string") {
-          stringProps.map(name => forOrigin(name, 'str')).forEach(maybeAdd);
-        } else if (obj instanceof Array) {
-          arrayProps.map(name => forOrigin(name, 'arr')).forEach(maybeAdd);
-        } else if (obj instanceof Function) {
-          funcProps.map(name => forOrigin(name, 'fun')).forEach(maybeAdd);
-        }
-      }
-
-      if (context && context.length) {
-        // If this is a property, see if it belongs to some object we can
-        // find in the current environment.
-        var obj = context.pop(),
-            base;
-        
-        if (obj.type && obj.type.indexOf("variable") === 0 || obj.string == "this") {
-          if (options && options.additionalContext) base = options.additionalContext[obj.string];
-
-          if (base === undefined && obj && obj.string) {
-            // #TODO instead of doing a blackbox evaluation of a string, we could look into the various scopes... 
-            // in the module or live prgoramming data structures... e.g. from AST with trace data such as #ContinuousEditor
-            // documentation: getDoitContext() and getTargetModule()
-            var result = await options.codemirror.boundEval(obj.string);
-            base = result.value;
-          }
-
-          if (!options || options.useGlobalScope !== false) {
-            base = base || global[obj.string];
-          }
-        } else if (obj.type == "string") {
-          base = "";
-        } else if (obj.type == "atom") {
-          base = 1;
-        } else if (obj.type == "function") {
-
-          // #TODO make it work for funciton calls such as pt(10,10), too.... 
-          if (global.jQuery != null && (obj.string == '$' || obj.string == 'jQuery') && typeof global.jQuery == 'function') base = global.jQuery();else if (global._ != null && obj.string == '_' && typeof global._ == 'function') base = global._();
-        }
-        while (base != null && context.length) base = base[context.pop().string];
-        if (base != null) gatherCompletions(base);
-        return;
-      }
-
-      const cursor = cm.getCursor();
-      const prevToken = cm.getTokenAt({ line: cursor.line, ch: token.start - 0 });
-      const prevPrevToken = cm.getTokenAt({ line: cursor.line, ch: token.start - 1 });
-      if (prevToken.string == ":" && prevPrevToken.string == ":") {
-        this.completeUtilsFunctions(token);
-        return;
-      }
-
-      // If not, just look in the global object and any local scope
-      // (reading into JS mode internals to get at the local and global variables)
-
-      for (var v = token.state.localVars; v; v = v.next) {
-        maybeAdd(forOrigin(v.name, 'local'));
-      }
-      for (var v = token.state.globalVars; v; v = v.next) {
-        maybeAdd(forOrigin(v.name, 'global'));
-      }
-      if (!options || options.useGlobalScope !== false) {
-        gatherCompletions(global);
-      }
-      javaScriptKeywords.map(keyword => forOrigin(keyword, 'keyword')).forEach(maybeAdd);
-    };
-
-    await javascriptHint(cm, options);
-    lively.notify(completions.list.length, 'num completions');
-    return completions.asCompletionsObject();
+    await getCompletionsFromImports();
   }
 
   completeUtilsFunctions(token) {
@@ -392,7 +420,7 @@ class CompletionsBuilder {
       this.completions.maybeAdd(forOrigin(completion, 'utils'));
     });
   }
-  
+
   completeLively() {
     if (['variable', null].includes(this.completions.token.type)) {
       this.maybeAdd({
