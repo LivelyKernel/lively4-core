b442b0bdaa379ab58fe792df2bfa03256bdc5816
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2022-03-25 16:59:05 +0100

    add aexpr example and fix ui deps
    SQUASHED: AUTO-COMMIT-demos-aexp.js,AUTO-COMMIT-src-client-reactive-components-basic-aexpr-timeline.html,AUTO-COMMIT-src-client-reactive-components-basic-aexpr-timeline.js,

---
+++ demos/aexp.js
@@ -0,0 +1,24 @@
+"enable aexpr"
+
+var o = {bar: 3}
+
+var observer
+
+function observeO(object) {
+  if (observer) {
+    observer.dispose()
+  }
+  
+  observer = aexpr(() => {
+    return object.bar
+  }).onChange(() => {
+    lively.notify("change: " + o.bar)
+  })
+  
+  
+}
+
+
+observeO(o)
+
+o.bar = 4
