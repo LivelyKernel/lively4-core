bd46370c9fe9ff8bf84d5aeae051c3345e692ef9
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2022-05-31 13:28:35 +0200

    demo world WIP
    SQUASHED: AUTO-COMMIT-src-client-interactive.js,AUTO-COMMIT-src-client-reactive-components-rewritten-demo-world.html,AUTO-COMMIT-src-client-reactive-components-rewritten-demo-world.js,AUTO-COMMIT-src-components-d3-d3-graphviz.js,AUTO-COMMIT-src-external-d3-graphviz.js,fixed-graph-zooming-persistence-in-aegrpah-and-emojis,

--- src/components/d3/d3-graphviz.js
+++ src/components/d3/d3-graphviz.js
@@ -123,7 +123,6 @@ export default class D3GraphViz extends D3Component {
     var graph = d3.select(div);
     this.graph = graph;
     if (!graph.graphviz) {
-      debugger;
       console.warn("d3-graphviz: graph.graphviz undefined");
       return;
     }
