b36b58df365db08c0d163516311bd8a883e68608
onsetsu <stefan.ramson@gmail.com>
2022-07-01 16:43:57 +0200

    added own preference
    SQUASHED: AUTO-COMMIT-diss.tex,AUTO-COMMIT-src-client-preferences.js,

--- src/client/preferences.js
+++ src/client/preferences.js
@@ -38,6 +38,7 @@ export default class Preferences {
       SWEDebugging: {default: false, short: "bug showcase in swe lecture"},
       AEXPGraphExperimental: {default: false, short: "AExpr graph experimental"},
       GSFullLogInfo: {default: false, short: "full log info for GS"},
+      StefansExperimentalPreference: {default: false, short: "Stefans experimental preference"},
     }
   }
   
