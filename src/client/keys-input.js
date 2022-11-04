3451fddff8dceda0e70100bfe55139b0f9335388
onsetsu <stefan.ramson@gmail.com>
2022-11-04 13:03:29 +0100

    renamed modifiersRight to more general keyInput
    SQUASHED: AUTO-COMMIT-src-client-keys-input.js,AUTO-COMMIT-src-client-modifiers-right.js,AUTO-COMMIT-src-components-widgets-lively-code-mirror-modes.js,

--- src/client/keys-input.js
+++ src/client/keys-input.js
@@ -1,4 +1,4 @@
-// #TODO: this is more than 'right' modifiers now
+/* global input */
 
 import { extend } from 'src/client/lang/utils.js';
 
@@ -35,72 +35,89 @@ extend(self, {
 
 extend(KeyboardEvent.prototype, {
 
-  get ctrlRight() { return !!self.input.state('ControlRight'); },
-  get shiftRight() { return !!self.input.state('ShiftRight'); },
-  get altRight() { return !!self.input.state('AltRight'); },
+  get ctrlRight() { return !!self.input.key('ControlRight'); },
+  get shiftRight() { return !!self.input.key('ShiftRight'); },
+  get altRight() { return !!self.input.key('AltRight'); },
 
-  get f1() { return !!self.input.state('F1'); },
-  get f2() { return !!self.input.state('F2'); },
-  get f3() { return !!self.input.state('F3'); },
-  get f4() { return !!self.input.state('F4'); },
-  get f5() { return !!self.input.state('F5'); },
-  get f6() { return !!self.input.state('F6'); },
-  get f7() { return !!self.input.state('F7'); },
-  get f8() { return !!self.input.state('F8'); },
-  get f9() { return !!self.input.state('F9'); },
-  get f10() { return !!self.input.state('F10'); },
-  get f11() { return !!self.input.state('F11'); },
-  get f12() { return !!self.input.state('F12'); },
-  get f13() { return !!self.input.state('F13'); },
-  get f14() { return !!self.input.state('F14'); },
-  get f15() { return !!self.input.state('F15'); },
-  get f16() { return !!self.input.state('F16'); },
-  get f17() { return !!self.input.state('F17'); },
-  get f18() { return !!self.input.state('F18'); },
-  get f19() { return !!self.input.state('F19'); },
-  get f20() { return !!self.input.state('F20'); },
-  get f21() { return !!self.input.state('F21'); },
-  get f22() { return !!self.input.state('F22'); },
-  get f23() { return !!self.input.state('F23'); },
-  get f24() { return !!self.input.state('F24'); },
+  get f1() { return !!self.input.key('F1'); },
+  get f2() { return !!self.input.key('F2'); },
+  get f3() { return !!self.input.key('F3'); },
+  get f4() { return !!self.input.key('F4'); },
+  get f5() { return !!self.input.key('F5'); },
+  get f6() { return !!self.input.key('F6'); },
+  get f7() { return !!self.input.key('F7'); },
+  get f8() { return !!self.input.key('F8'); },
+  get f9() { return !!self.input.key('F9'); },
+  get f10() { return !!self.input.key('F10'); },
+  get f11() { return !!self.input.key('F11'); },
+  get f12() { return !!self.input.key('F12'); },
+  get f13() { return !!self.input.key('F13'); },
+  get f14() { return !!self.input.key('F14'); },
+  get f15() { return !!self.input.key('F15'); },
+  get f16() { return !!self.input.key('F16'); },
+  get f17() { return !!self.input.key('F17'); },
+  get f18() { return !!self.input.key('F18'); },
+  get f19() { return !!self.input.key('F19'); },
+  get f20() { return !!self.input.key('F20'); },
+  get f21() { return !!self.input.key('F21'); },
+  get f22() { return !!self.input.key('F22'); },
+  get f23() { return !!self.input.key('F23'); },
+  get f24() { return !!self.input.key('F24'); },
 
 });
 
 class KeyInput {
   
   constructor() {
-    this.resetStates()
+    this._resetStates()
   }
   
   /*MD ## querying state key MD*/
-  state(code) {
-    return this._states[code]
+  // #TODO: not yet implemented
+  // #TODO: bind actions to keys
+  state(action) {
+    throw new Error('not yet implemented')
+  }
+  
+  key(code) {
+    return !!this._keys[code]
+  }
+  
+  allKeys() {
+    return Object.keys(input._keys)
   }
 
   /*MD ## writing key state MD*/
-  setState(code, state) {
-    this._states[code] = state
+  _setKey(code, state) {
+    if (state) {
+      this._keys[code] = true
+    } else {
+      delete this._keys[code]
+    }
   }
   
-  resetStates() {
-    this._states = {}
+  _resetStates() {
+    this._keys = {}
   }
   
   /*MD ## hooks MD*/
   static setKeyState(e, state) {
-    self.input.setState(e.code, state)
+    self.input._setKey(e.code, state)
   }
 
   static onKeydown(e) {
     this.setKeyState(e, true);
   }
+
   static onKeyup(e) {
     this.setKeyState(e, false);
   }
-  static clearStates(e) {
-    self.input.resetStates();
+
+  static onFocus(e) {
+    self.input._resetStates();
   }
 
+  // #important
   static load() {
     self.input = new this();
     
@@ -113,7 +130,11 @@ class KeyInput {
     lively.removeEventListener(context);
     lively.addEventListener(context, document, "keydown", ::this.onKeydown, options);
     lively.addEventListener(context, document, "keyup", ::this.onKeyup, options);
-    lively.addEventListener(context, window, 'focus', ::this.clearStates, options);
+    lively.addEventListener(context, window, 'focus', ::this.onFocus, options);
+    
+    lively.addEventListener(context, document, "keydown", evt => {
+      lively.notify(input.allKeys())
+    }, options);
   }
 }
 
