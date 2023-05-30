88a64554c735b6d46685aced12eff00bf9e1de46
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2023-05-30 14:10:58 +0200

    helle 2
    SQUASHED: AUTO-COMMIT-demos-markdown-editor-drag-and-drop.js,helle,

--- demos/markdown/editor/drag-and-drop.js
+++ demos/markdown/editor/drag-and-drop.js
@@ -4,22 +4,22 @@
 
 // hello
 
-/*PW                              <lively-drawboard width="400px" height="400px" color="black" pen-size="4" tabindex="0" style="position: relative; background-color: rgb(255, 250, 205); left: 0px; top: 0px; width: 569px; height: 118px; z-index: 200;" class="lively-content" data-lively-id="3ac6b238-4cf0-4a7f-92f1-5becfd7db024"><svg xmlns="http://www.w3.org/2000/svg" id="svg" data-is-meta="true" style="position: absolute; top: 0px; left: 0px; width: 570.99px; height: 119.991px; border: none; opacity: 1; touch-action: none;"><path stroke="black" stroke-width="4" fill="none" d="M94.47052,24.52257c-8.64492,12.96732 -16.18051,25.98206 -18.33331,41.66667c-0.3665,2.67019 -1.88579,35.86908 1.66663,12.77777"></path><path stroke="black" stroke-width="4" fill="none" d="M108.35944,30.63368c-10.64976,7.98732 -14.97391,28.74541 -17.22223,41.1111c-0.33519,1.84352 -3.27532,19.06304 -5,13.88892"></path><path stroke="black" stroke-width="4" fill="none" d="M62.80383,48.96701c0,-1.74074 0.74076,-1.1111 2.22223,-1.1111c4.72132,0 9.69821,3.37028 13.88892,5c8.09765,3.14909 46.94519,14.02717 55,9.99998c0.78391,-0.39193 -2.95449,-8.68433 -5,-8.88889c-9.938,-0.99381 -31.10951,8.88982 -14.44446,17.22223c12.54115,6.27049 34.11912,-8.73508 41.66669,-16.11111c3.97326,-3.88296 7.37323,-8.438 10,-13.33333c1.52915,-2.84978 1.56262,-6.2783 2.22223,-9.44444c0.41717,-2.00244 2.49602,-6.75793 0.55554,-6.11111c-13.35091,4.45026 -17.63804,17.62999 -18.88892,30.55556c-0.35374,3.65525 -1.02256,13.09968 3.88892,15.55556c3.1817,1.59094 5.31972,-5.04091 6.66663,-8.33334c3.16172,-7.72866 7.5801,-12.31626 11.11115,-20.55556c0.50995,-1.18992 4.31654,-9.3019 2.22223,-10c-10.38927,-3.46306 -23.61081,23.79696 -17.22223,34.44444c1.06902,1.78168 24.31673,-0.33923 27.77777,-0.55554c1.29378,-0.08086 3.30915,1.15943 3.88892,0c0.61651,-1.23291 0.76719,-12.11637 -0.5556,-12.77779c-2.8714,-1.43574 -14.4444,9.07924 -14.4444,11.11113c0,8.56291 13.11946,16.20978 20.55554,12.77777c6.619,-3.0549 5.12792,-27.77779 -0.55554,-27.77779"></path></svg></lively-drawboard>                       PW*/
 
+/*PW                               <lively-drawboard width="400px" height="400px" color="black" pen-size="4" tabindex="0" style="position: relative; background-color: rgb(255, 250, 205); left: 0px; top: 0px; width: 569px; height: 118px; z-index: 200;" class="lively-content" data-lively-id="3ac6b238-4cf0-4a7f-92f1-5becfd7db024"><svg xmlns="http://www.w3.org/2000/svg" id="svg" data-is-meta="true" style="position: absolute; top: 0px; left: 0px; width: 570.104px; height: 119.106px; border: none; opacity: 1; touch-action: none;"><path stroke="black" stroke-width="4" fill="none" d="M94.47052,24.52257c-8.64492,12.96732 -16.18051,25.98206 -18.33331,41.66667c-0.3665,2.67019 -1.88579,35.86908 1.66663,12.77777"></path><path stroke="black" stroke-width="4" fill="none" d="M108.35944,30.63368c-10.64976,7.98732 -14.97391,28.74541 -17.22223,41.1111c-0.33519,1.84352 -3.27532,19.06304 -5,13.88892"></path><path stroke="black" stroke-width="4" fill="none" d="M62.80383,48.96701c0,-1.74074 0.74076,-1.1111 2.22223,-1.1111c4.72132,0 9.69821,3.37028 13.88892,5c8.09765,3.14909 46.94519,14.02717 55,9.99998c0.78391,-0.39193 -2.95449,-8.68433 -5,-8.88889c-9.938,-0.99381 -31.10951,8.88982 -14.44446,17.22223c12.54115,6.27049 34.11912,-8.73508 41.66669,-16.11111c3.97326,-3.88296 7.37323,-8.438 10,-13.33333c1.52915,-2.84978 1.56262,-6.2783 2.22223,-9.44444c0.41717,-2.00244 2.49602,-6.75793 0.55554,-6.11111c-13.35091,4.45026 -17.63804,17.62999 -18.88892,30.55556c-0.35374,3.65525 -1.02256,13.09968 3.88892,15.55556c3.1817,1.59094 5.31972,-5.04091 6.66663,-8.33334c3.16172,-7.72866 7.5801,-12.31626 11.11115,-20.55556c0.50995,-1.18992 4.31654,-9.3019 2.22223,-10c-10.38927,-3.46306 -23.61081,23.79696 -17.22223,34.44444c1.06902,1.78168 24.31673,-0.33923 27.77777,-0.55554c1.29378,-0.08086 3.30915,1.15943 3.88892,0c0.61651,-1.23291 0.76719,-12.11637 -0.5556,-12.77779c-2.8714,-1.43574 -14.4444,9.07924 -14.4444,11.11113c0,8.56291 13.11946,16.20978 20.55554,12.77777c6.619,-3.0549 5.12792,-27.77779 -0.55554,-27.77779"></path></svg></lively-drawboard>                        PW*/
 
-// other code
+// other code #TODO scripts are not initialized yet
 
 // hello ddd World
 
-/*PW                                <button style="position: relative;" class="lively-content" data-lively-id="4624db17-121a-4ff6-b69b-17cc4ac0d12f" data-lively-part-name="button">button<script data-name="livelyLoad" type="lively4script">function livelyLoad() {
+/*PW                                 <button style="position: relative;" class="lively-content" data-lively-id="4624db17-121a-4ff6-b69b-17cc4ac0d12f" data-lively-part-name="button">button<script data-name="livelyLoad" type="lively4script">function livelyLoad() {
   lively.addEventListener("me", this, "click", evt => this.onClick(evt)) 
 }</script><script data-name="onClick" type="lively4script">function onClick() {
   lively.notify("button  clicked!")
-}</script></button>             PW*/
+}</script></button>              PW*/
  
 
 // hallo 
-/*PW        <lively-list tabindex="0" class="lively-content" style="position: relative;" data-lively-id="36a4e6a2-f166-42ad-ac50-e07a31817569">
+/*PW         <lively-list tabindex="0" class="lively-content" style="position: relative;" data-lively-id="36a4e6a2-f166-42ad-ac50-e07a31817569">
               <li>one</li>
               <li>two<button style="position: relative;" class="lively-content" data-lively-id="941f4543-872e-4b7b-8241-3b9def1bf128" data-lively-part-name="button">button<script data-name="livelyLoad" type="lively4script">function livelyLoad() {
   lively.addEventListener("me", this, "click", evt => this.onClick(evt)) 
@@ -28,5 +28,5 @@
 }</script></button></li>
               <li>three</li>
               <li>four</li>
-            </lively-list>     PW*/
+            </lively-list>      PW*/
 
