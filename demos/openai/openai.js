2e91118b9f2b1d43a8c67119f48a9509126ae0e0
boehmchen <49753551+boehmchen@users.noreply.github.com>
2023-06-19 00:07:59 +0200

    Experiments with function call OpenAI parameters
    SQUASHED: SQUASHED-SQUASHED-SQUASHED-SQUASHED-AUTO-COMMIT-demos-openai-openai.js-AUTO-COMMIT-src-playground-example.js,

--- demos/openai/openai.js
+++ demos/openai/openai.js
@@ -17,17 +17,9 @@ export default class OpenAI {
   static getSubscriptionKey() {
     return localStorage.getItem(openAiSubscriptionKeyId);
   }
-
-  static async completeCode(code, placeholder="AI_COMPLETE_HERE") {
-    const apiKey = await this.ensureSubscriptionKey();
-    const url = "https://api.openai.com/v1/chat/completions";
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
+  
+  static jensPrompt(code, placeholder) {
+    return {
         "model": "gpt-3.5-turbo",
         "max_tokens": 500,
         "temperature": 0.1,
@@ -45,18 +37,71 @@ export default class OpenAI {
           { "role": "assistant", "content": "  return a + b " },
           { "role": "user", "content": code }
         ],
-      })
+      }
+  }
+  
+  static lukasPrompt(code) {
+    return {
+        "model": "gpt-3.5-turbo-0613",
+        "max_tokens": 500,
+        "temperature": 0.1,
+        "top_p": 1,
+        "n": 1,
+        "stream": false,
+        "stop": null,
+        "messages": [
+          { "role": "system", "content": "You are a JavaScript code completion tool. You complete whole functions." },
+          { "role": "user", "content": code }
+        ],
+        "functions": [
+          {
+          "name": "auto_complete_code",
+          "description": "Generate code completing the user written JavaScript code.",
+          "parameters": {
+            "type": "object",
+            "properties": {
+              "code": {
+                "type": "string", // User provided code.
+                "description": "Generated code which completes prior code."
+              },
+              "intent": {
+                "type": "string", // We can use this as further information for the user.
+                "description": "Assumed intent of the code of the user."
+              }
+            },
+            "required": ["code"]
+            }
+          }
+        ],
+        "function_call": {"name": "auto_complete_code"} // Enforce return value in form of 'auto_complete_code'
+      }
+  }
+
+  static async completeCode(code, placeholder="AI_COMPLETE_HERE") {
+    const apiKey = await this.ensureSubscriptionKey();
+    const url = "https://api.openai.com/v1/chat/completions";
+    const prompt = this.lukasPrompt(code);
+    
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify(prompt)
     };
+    
     try {
       const response = await fetch(url, requestOptions);
       const data = await response.json();
-      const result = data;
-      // document.getElementById('gpt-output').value = result;
-
+      //const arguments = JSON.parse(data.choices.first.message.function_call.arguments);
+      
       return {
         data: data,
-        completion: data.choices.first.message.content.replace(/.*```/g,"").replace(/```(?:\n|.)*/g,"")
+        // data.choices.first.message.content.replace(/.*```/g,"").replace(/```(?:\n|.)*/g,"")
+        completion: JSON.parse(data.choices.first.message.function_call.arguments).code,
       }
+      
     } catch (error) {
       return {
         isError: true,
