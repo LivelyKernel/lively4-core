e917b8cf25634a992f2a719de3a6a0562f79ed5d
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2024-06-07 09:48:38 +0200

    disable JavaScript linting for TypeScript
    SQUASHED: AUTO-COMMIT-src-external-eslint-eslint-lint.js,

--- src/external/eslint/eslint-lint.js
+++ src/external/eslint/eslint-lint.js
@@ -322,7 +322,10 @@ const defaultConfig = {
   }
 }
 
-export function validator(text, options) {
+export function validator(text, options, cm) {
+  
+  if (cm.options.mode === 'text/typescript') return [] // to TypeScript support at the moment
+  
   var result = [], config = defaultConfig;
   var linter = new eslint();
   linter.defineParser("babel-parser", {parse});
