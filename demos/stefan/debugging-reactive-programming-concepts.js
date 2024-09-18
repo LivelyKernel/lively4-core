183ed51730727baf5c03625d4e22696961d9a551
onsetsu <stefan.ramson@gmail.com>
2024-09-18 15:24:49 +0200

    debugging rpcs example
    SQUASHED: AUTO-COMMIT-demos-stefan-debugging-reactive-programming-concepts.js,

--- demos/stefan/debugging-reactive-programming-concepts.js
+++ demos/stefan/debugging-reactive-programming-concepts.js
@@ -1,51 +1,49 @@
-"enable aexpr"
-
 /*MD 
 # Debugging Active Expressions and Signals in One System
 
 ```javascript {.testScript .snippet}
-import {test1} from "demos/aexpr2.js"
-test1()
+import run from "demos/stefan/debugging-reactive-programming-concepts.js"
+run()
 ```
 <script>
   import {autoRunSnippet} from "src/client/essay.js"; 
   autoRunSnippet(this, ".testScript")
 </script>
-
-
 MD*/
 
+"enable aexpr"
 
+/*MD 
+#### Idea
+Our debugging toolset allows to debug multiple reactive programming concepts in conjunction, here Active Expressions and Signals
+MD*/
+export default async function runExample() {
+  var a = 2;
 
-export async function test1() {
+  const log = <div><button click={evt => a++}>Increment variable a</button><br />Result: <br /></div>
   
-  var result = <div>Result: <br /></div>
-  
-  var o = {bar: 3}
+/*MD
+     1. Reactive declarations and dependencies are highlighted by the left-hand side gutter, e.g. RE specifies that there is a reactive declaration (a Signal) that is also used as a dependency elsewhere
+     2. **click the icons** (RE, SI, AE, etc.) to
+       - navigate to related code locations
+       - **open** specific tools such as the timeline or an in-time graph view
+MD*/ 
+  always: var b = a + 1
+  always: var c = b + 2
 
-  var observer
+  const ae = aexpr(() => c).onChange(newValue => {
+    log.insertAdjacentHTML('beforeend', `c updated to: ${newValue}</br>`)
+  });
 
-  function observeO(object) {
-    if (observer) {
-      observer.dispose()
+  (async function fork() {
+    for (let i of [1,2,3]) {
+      await lively.sleep(500)
+/*MD
+     - changes to dependencies are visualized in the left-hand side gutter
+MD*/
+      a += i
     }
-
-    observer = aexpr(() => {
-      return object.bar
-    }).onChange(() => {
-      result.innerHTML += "change: " + o.bar + "</br>"
-    })
-
-
-  }
-  observeO(o)
+  })()
   
-  lively.sleep(500).then(async () => {
-    o.bar = 2
-    await lively.sleep(500)
-    o.bar = 4
-  })
-  
-  
-  return result
+  return log
 } 
