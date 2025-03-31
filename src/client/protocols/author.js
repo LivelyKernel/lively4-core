aab45472221e39a6a668ee8e04f06162a32f1b0f
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2025-03-31 16:39:43 +0200

    bibtex author search fix sorting
    SQUASHED: AUTO-COMMIT-src-client-protocols-author.js,

--- src/client/protocols/author.js
+++ src/client/protocols/author.js
@@ -11,7 +11,11 @@ export class AuthorScheme extends BibliographyScheme {
     var author = query
     let result = entries.filter(entry => entry.authors && entry.authors.find(ea => ea.match(author)))
     
+    
+    result = _.sortBy(result, ea => ea.key)
     result = _.uniqBy(result, ea => ea.key)
+    
+    
     
     return result
   }  
