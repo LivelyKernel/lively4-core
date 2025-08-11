86d3865ed5ebc1ec05c069941d1070f9741106fd
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2025-08-11 16:22:05 +0200

    [code_mirror] line based change indicator

---
+++ demos/claude/git-status-test.js
@@ -0,0 +1,24 @@
+// Git Status Test File
+// This file tests the new git line status visualization feature
+
+console.log("Testing git status indicators...")
+
+// Add some code here to test unsaved changes
+function testFunction() {
+  return "This line should show red indicator when modified"
+}
+
+// More test content
+const testVariable = "modify this to see changes"
+
+// Test array
+const testArray = [1, 2, 3, 4, 5]
+
+// Test object  
+const testObject = {
+  name: "test",
+  value: 42,
+  active: true
+}
+
+console.log("Git status test file loaded")
