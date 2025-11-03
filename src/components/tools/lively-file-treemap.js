709360f64bdd485e869385acf90723ddfddc2438
Lackus02 <Sakul.Heidisch@gmail.com>
2025-11-03 18:24:55 +0100

    Added TreemapRenderer class extension
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-file-treemap.html,AUTO-COMMIT-src-components-tools-lively-file-treemap.js,

--- src/components/tools/lively-file-treemap.js
+++ src/components/tools/lively-file-treemap.js
@@ -1,11 +1,14 @@
 "enable aexpr";
 
 import Morph from 'src/components/widgets/lively-morph.js';
-import Files from "src/client/files.js";
+import Files from 'src/client/files.js';
 
 export default class LivelyFileTreemap extends Morph  {
 
   async initialize() {
+    this.fileTreemapRenderer = new FileTreemapRenderer();
+    this.fileTreemapRenderer.initialize(this.treemapCanvas = this.get("#treemap-canvas"));
+    
     this.updateView();
   }
   
@@ -23,11 +26,33 @@ export default class LivelyFileTreemap extends Morph  {
     pane.appendChild(this.treemap);
     this.treemap.setTreeData(this.data);*/
     
-    this.treemapViewer = await (<lively-iframe></lively-iframe>);
-    var treemapViewerPane = this.get("#treemap-viewer-pane");
-    treemapViewerPane.innerHTML = "";
-    treemapViewerPane.appendChild(this.treemapViewer);
-    this.treemapViewer.setURL("https://youtube.com");
+    
+    
   }
+  
+}
+
+import {gloperate, Configuration, initialize, Renderer, Visualization} from 'https://lively-kernel.org/lively4/treemap-renderer/dist/treemap-renderer.js';
+
+class FileTreemapRenderer extends gloperate.Initializable {
+  
+  initialize(element: HTMLCanvasElement | string): boolean {
+      this._canvas = initialize(element);
+      this._visualization = new Visualization();
+      const renderer: Renderer = this._visualization.renderer as Renderer;
+      this._canvas.renderer = renderer;
+
+      super.expose();
+
+      return true;
+    }
+
+    //TODO: this should be called 
+    uninitialize(): void {
+      this._canvas.dispose();
+      (this._renderer as gloperate.Renderer).uninitialize();
+    }
+    
+    
   
 }
