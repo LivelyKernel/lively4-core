ac493f02ded22d93bb146ee7582d9cd224a1c049
Lackus02 <No public email set>
2025-11-10 18:26:49 +0100

    extended example config
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-file-treemap.js,

--- src/components/tools/lively-file-treemap.js
+++ src/components/tools/lively-file-treemap.js
@@ -51,10 +51,153 @@ class FileTreemapRenderer extends gloperate.Initializable {
   
   setData(data = undefined) {
     this.config = new Configuration();
-    this.config.topology = {edges: [[-1,0], [0, 1], [0, 2], [0, 3]], format: "tupled", semantics: "parent-id-id"};
-    this.config.layout = {algorithm: "strip", weight: "bufferView:weights"};
-    this.config.buffers = [{identifier: "source-weights", type: "numbers", data: [ 0.0, 0.0, 1.0, 2.0, 1.0 ], encoding: "native"}];
-    this.config.bufferViews = [{identifier: "weights", source: "buffer:source-weights", transformations: [{ type: "fill-invalid", value: 0.0, invalidValue: -1.0 }, { type: "propagate-up", operation: "sum" }]}];
+    
+    this.config.topology = {
+      edges: [0,1,0,2,0,3,0,4,1,5,1,6,2,7,2,8,3,9,3,10,4,11,4,12],
+      format: "interleaved",
+      semantics: "parent-id-id"
+    };
+    
+    this.config.layout = {
+      algorithm: "strip",
+      weight: "bufferView:weights",
+      sort: {
+      key: "bufferView:weights",
+      algorithm: "keep"
+      },
+      parentPadding: {
+        type: "relative",
+        value: 0.05
+      },
+      siblingMargin: {
+        type: "relative",
+        value: 0.05
+      },
+      accessoryPadding: {
+        type: "absolute",
+        direction: "bottom",
+        value: [
+          0,
+          0.02,
+          0.01,
+          0
+        ],
+        relativeAreaThreshold: 0.4,
+        targetAspectRatio: 8
+      }
+    };
+    
+    this.config.buffers = [
+      {
+        identifier: "source-weights",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": -1,
+          "2": -1,
+          "3": -1,
+          "4": -1,
+          "5": 0.125,
+          "6": 0.25,
+          "7": 0.125,
+          "8": 0.125,
+          "9": 0.5,
+          "10": 0.125,
+          "11": 0.5,
+          "12": 0.5
+        },
+        linearization: "topology"
+      },
+      {
+        identifier: "source-heights",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": -1,
+          "2": -1,
+          "3": -1,
+          "4": -1,
+          "5": 0.1,
+          "6": 0.1,
+          "7": 0.3,
+          "8": 0.2,
+          "9": 0.1,
+          "10": 0.1,
+          "11": 0.1,
+          "12": 0.2
+        },
+        linearization: "topology"
+      },
+      {
+        identifier: "source-colors",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": -1,
+          "2": -1,
+          "3": -1,
+          "4": -1,
+          "5": 0,
+          "6": 0.1,
+          "7": 0.1,
+          "8": 0.6,
+          "9": 0.7,
+          "10": 1,
+          "11": 0,
+          "12": 0
+        },
+        linearization: "topology"
+      }
+    ];
+    
+    this.config.bufferViews = [
+    {
+      identifier: "weights",
+      source: "buffer:source-weights",
+      transformations: [
+        {
+          type: "fill-invalid",
+          value: 0,
+          invalidValue: -1
+        },
+        {
+          type: "propagate-up",
+          operation: "sum"
+        }
+      ]
+    },
+    {
+      identifier: "heights-normalized",
+      source: "buffer:source-heights",
+      transformations: [
+        {
+          type: "fill-invalid",
+          value: 0,
+          invalidValue: -1
+        },
+        {
+          type: "normalize",
+          operation: "zero-to-max"
+        }
+      ]
+    },
+    {
+      identifier: "colors-normalized",
+      source: "buffer:source-colors",
+      transformations: [
+        {
+          type: "fill-invalid",
+          value: 0,
+          invalidValue: -1
+        },
+        {
+          type: "normalize",
+          operation: "zero-to-max"
+        }
+      ]
+    }
+  ];
+    
     this.config.colors = [
     {
       identifier: "emphasis",
@@ -79,10 +222,22 @@ class FileTreemapRenderer extends gloperate.Initializable {
     },
     {
       identifier: "leaf",
-      preset: "Oranges",
+      preset: "Blues",
       steps: 7
     }];
-    this.config.geometry = {parentLayer: {showRoot: false}};
+    this.config.geometry = {
+      parentLayer: {showRoot: false},
+      leafLayer: {
+        colorMap: "color:leaf",
+        height: "bufferView:heights-normalized",
+        colors: "bufferView:colors-normalized"
+      },
+      emphasis: {
+        outline: [],
+        highlight: []
+      },
+      heightScale: 0.5
+    };
     this.config.labels = {};
     
     this.visualization.configuration = this.config;
