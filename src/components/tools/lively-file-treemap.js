3cd8d4987006885304a4829affad1d40ed5728b0
Lackus02 <No public email set>
2025-11-11 11:46:31 +0100

    prepared parser
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-file-treemap.js,

--- src/components/tools/lively-file-treemap.js
+++ src/components/tools/lively-file-treemap.js
@@ -60,16 +60,40 @@ class FileTreemapRenderer extends gloperate.Initializable {
     this.visualization.renderer._altered.alter("frameSize");
   }
   
-  setData(data = undefined) {
+  setData(data = undefined) {    
+    if(!data) {
+      this.config = this.makeExampleConfig();
+      this.visualization.configuration = this.config;
+      this.updateView();
+      return;
+    }
+    
     this.config = new Configuration();
+    const topologyData = [];
+    const weightData = [];
+    const labelData = [];
+    const colorData = [];
     
-    this.config.topology = {
-      edges: [0,1,0,2,0,3,0,4,1,5,1,6,2,7,2,8,3,9,3,10,4,11,4,12],
-      format: "interleaved",
-      semantics: "parent-id-id"
-    };
+    //TODO recursively read Data
+      // Fill in arrays
+      // set them as config buffers
+      // lazy updating?
+      
     
-    this.config.layout = {
+    this.visualization.configuration = this.config;
+    this.updateView();
+    
+  }
+  
+  makeExampleConfig() {
+    const config = new Configuration();
+    config.topology = {
+        edges: [0,1,0,2,0,3,0,4,1,5,1,6,2,7,2,8,3,9,3,10,4,11,4,12],
+        format: "interleaved",
+        semantics: "parent-id-id"
+      };
+    
+    config.layout = {
       algorithm: "strip",
       weight: "bufferView:weights",
       sort: {
@@ -87,193 +111,191 @@ class FileTreemapRenderer extends gloperate.Initializable {
       accessoryPadding: {
         type: "absolute",
         direction: "bottom",
-        value: [
-          0,
-          0.02,
-          0.01,
-          0
-        ],
-        relativeAreaThreshold: 0.4,
-        targetAspectRatio: 8
-      }
-    };
-    
-    this.config.buffers = [
-      {
-        identifier: "source-weights",
-        type: "numbers",
-        data: {
-          "0": 0,
-          "1": -1,
-          "2": -1,
-          "3": -1,
-          "4": -1,
-          "5": 0.125,
-          "6": 0.25,
-          "7": 0.125,
-          "8": 0.125,
-          "9": 0.5,
-          "10": 0.125,
-          "11": 0.5,
-          "12": 0.5
+          value: [
+            0,
+            0.02,
+            0.01,
+            0
+          ],
+          relativeAreaThreshold: 0.4,
+          targetAspectRatio: 8
+        }
+      };
+
+      config.buffers = [
+        {
+          identifier: "source-weights",
+          type: "numbers",
+          data: {
+            "0": 0,
+            "1": -1,
+            "2": -1,
+            "3": -1,
+            "4": -1,
+            "5": 0.125,
+            "6": 0.25,
+            "7": 0.125,
+            "8": 0.125,
+            "9": 0.5,
+            "10": 0.125,
+            "11": 0.5,
+            "12": 0.5
+          },
+          linearization: "topology"
         },
-        linearization: "topology"
+        {
+          identifier: "source-heights",
+          type: "numbers",
+          data: {
+            "0": 0,
+            "1": -1,
+            "2": -1,
+            "3": -1,
+            "4": -1,
+            "5": 0.1,
+            "6": 0.1,
+            "7": 0.3,
+            "8": 0.2,
+            "9": 0.1,
+            "10": 0.1,
+            "11": 0.1,
+            "12": 0.2
+          },
+          linearization: "topology"
+        },
+        {
+          identifier: "source-colors",
+          type: "numbers",
+          data: {
+            "0": 0,
+            "1": -1,
+            "2": -1,
+            "3": -1,
+            "4": -1,
+            "5": 0,
+            "6": 0.1,
+            "7": 0.1,
+            "8": 0.6,
+            "9": 0.7,
+            "10": 1,
+            "11": 0,
+            "12": 0
+          },
+          linearization: "topology"
+        }
+      ];
+
+      config.bufferViews = [
+      {
+        identifier: "weights",
+        source: "buffer:source-weights",
+        transformations: [
+          {
+            type: "fill-invalid",
+            value: 0,
+            invalidValue: -1
+          },
+          {
+            type: "propagate-up",
+            operation: "sum"
+          }
+        ]
       },
       {
-        identifier: "source-heights",
-        type: "numbers",
-        data: {
-          "0": 0,
-          "1": -1,
-          "2": -1,
-          "3": -1,
-          "4": -1,
-          "5": 0.1,
-          "6": 0.1,
-          "7": 0.3,
-          "8": 0.2,
-          "9": 0.1,
-          "10": 0.1,
-          "11": 0.1,
-          "12": 0.2
-        },
-        linearization: "topology"
+        identifier: "heights-normalized",
+        source: "buffer:source-heights",
+        transformations: [
+          {
+            type: "fill-invalid",
+            value: 0,
+            invalidValue: -1
+          },
+          {
+            type: "normalize",
+            operation: "zero-to-max"
+          }
+        ]
       },
       {
-        identifier: "source-colors",
-        type: "numbers",
-        data: {
-          "0": 0,
-          "1": -1,
-          "2": -1,
-          "3": -1,
-          "4": -1,
-          "5": 0,
-          "6": 0.1,
-          "7": 0.1,
-          "8": 0.6,
-          "9": 0.7,
-          "10": 1,
-          "11": 0,
-          "12": 0
-        },
-        linearization: "topology"
+        identifier: "colors-normalized",
+        source: "buffer:source-colors",
+        transformations: [
+          {
+            type: "fill-invalid",
+            value: 0,
+            invalidValue: -1
+          },
+          {
+            type: "normalize",
+            operation: "zero-to-max"
+          }
+        ]
       }
     ];
-    
-    this.config.bufferViews = [
-    {
-      identifier: "weights",
-      source: "buffer:source-weights",
-      transformations: [
-        {
-          type: "fill-invalid",
-          value: 0,
-          invalidValue: -1
-        },
-        {
-          type: "propagate-up",
-          operation: "sum"
-        }
-      ]
-    },
-    {
-      identifier: "heights-normalized",
-      source: "buffer:source-heights",
-      transformations: [
-        {
-          type: "fill-invalid",
-          value: 0,
-          invalidValue: -1
-        },
-        {
-          type: "normalize",
-          operation: "zero-to-max"
-        }
-      ]
-    },
-    {
-      identifier: "colors-normalized",
-      source: "buffer:source-colors",
-      transformations: [
-        {
-          type: "fill-invalid",
-          value: 0,
-          invalidValue: -1
-        },
-        {
-          type: "normalize",
-          operation: "zero-to-max"
-        }
-      ]
-    }
-  ];
-    
-    this.config.colors = [
-    {
-      identifier: "emphasis",
-      colorspace: "hex",
-      value: "#00b0ff"
-    },
-    {
-      identifier: "auxiliary",
-      colorspace: "hex",
-      values: [
-        "#00aa5e",
-        "#71237c"
-      ]
-    },
-    {
-      identifier: "inner",
-      colorspace: "hex",
-      values: [
-        "#e8eaee",
-        "#eef0f4"
-      ]
-    },
-    {
-      identifier: "leaf",
-      preset: "Blues",
-      steps: 7
-    }];
-    this.config.geometry = {
-      parentLayer: {showRoot: false},
-      leafLayer: {
-        colorMap: "color:leaf",
-        height: "bufferView:heights-normalized",
-        colors: "bufferView:colors-normalized"
+
+      config.colors = [
+      {
+        identifier: "emphasis",
+        colorspace: "hex",
+        value: "#00b0ff"
       },
-      emphasis: {
-        outline: [],
-        highlight: []
+      {
+        identifier: "auxiliary",
+        colorspace: "hex",
+        values: [
+          "#00aa5e",
+          "#71237c"
+        ]
       },
-      heightScale: 0.5
+      {
+        identifier: "inner",
+        colorspace: "hex",
+        values: [
+          "#e8eaee",
+          "#eef0f4"
+        ]
+      },
+      {
+        identifier: "leaf",
+        preset: "Blues",
+        steps: 7
+      }];
+      config.geometry = {
+        parentLayer: {showRoot: false},
+        leafLayer: {
+          colorMap: "color:leaf",
+          height: "bufferView:heights-normalized",
+          colors: "bufferView:colors-normalized"
+        },
+        emphasis: {
+          outline: [],
+          highlight: []
+        },
+        heightScale: 0.5
+      };
+      config.labels = {"innerNodeLayerRange": [
+        1,
+        2
+      ],
+      numTopInnerNodes: 50,
+      numTopWeightNodes: 50,
+      numTopHeightNodes: 50,
+      numTopColorNodes: 50,
+      names: {
+        "1": "a",
+        "2": "b",
+        "3": "c",
+        "4": "d",
+        "5": "e",
+        "6": "f",
+        "7": "g",
+        "8": "h",
+        "9": "i",
+        "10": "j",
+        "11": "k",
+        "12": "l"
+      }
     };
-    this.config.labels = {"innerNodeLayerRange": [
-      1,
-      2
-    ],
-    numTopInnerNodes: 50,
-    numTopWeightNodes: 50,
-    numTopHeightNodes: 50,
-    numTopColorNodes: 50,
-    names: {
-      "1": "a",
-      "2": "b",
-      "3": "c",
-      "4": "d",
-      "5": "e",
-      "6": "f",
-      "7": "g",
-      "8": "h",
-      "9": "i",
-      "10": "j",
-      "11": "k",
-      "12": "l"
-    }
-  };
-    
-    this.visualization.configuration = this.config;
-    this.updateView();
+    return config;
   }
 }
