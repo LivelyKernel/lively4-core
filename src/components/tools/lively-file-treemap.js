bb3446356a24b80d4cb7f8aa07d5bf8df56e3efa
Lackus02 <No public email set>
2025-11-22 02:08:37 +0100

    wrote working parser for treemap data (only file data for now)
    SQUASHED: AUTO-COMMIT-src-components-tools-lively-file-treemap.js,SQUASHED-AUTO-COMMIT-src-components-tools-lively-file-treemap.js,

--- src/components/tools/lively-file-treemap.js
+++ src/components/tools/lively-file-treemap.js
@@ -3,48 +3,65 @@
 import Morph from 'src/components/widgets/lively-morph.js';
 import Files from 'src/client/files.js';
 import { debounce } from 'utils';
-import {gloperate, Configuration, initialize as initializeCanvas, Renderer, Visualization} from 'https://lively-kernel.org/lively4/treemap-renderer/dist/treemap-renderer.js';
+import { gloperate, Configuration, initialize as initializeCanvas, Renderer,
+  Visualization } from 'https://lively-kernel.org/lively4/treemap-renderer/dist/treemap-renderer.js';
 
-export default class LivelyFileTreemap extends Morph  {
+export default class LivelyFileTreemap extends Morph {
 
   async initialize() {
     this.fileTreemapRenderer = new FileTreemapRenderer();
     this.fileTreemapRenderer.initialize(this.treemapCanvas = this.get("#treemap-canvas"));
     await this.ensureData();
-    //console.log(this.data)
-    this.fileTreemapRenderer.setData(/*this.data*/);
+    this.fileTreemapRenderer.setData(this.data);
     this.fileTreemapRenderer.updateView();
     this.addEventListener('extent-changed', ((evt) => { this.onExtentChanged(evt); })::debounce(500));
-    this.fileTreemapRenderer.setColorScheme("Reds");
-    this.fileTreemapRenderer.setColorSteps(7); //TODO weird colors unless 7 or 8
-    //this.fileTreemapRenderer.setVisualizationType(VisualizationType.VISUALIZATION_2D);
   }
-  
+
   async ensureData() {
     if (this.data) return;
+    
+    /*this.data = {
+       children: [
+          {
+            name: "parts",
+            size: "1",
+            type: "file",
+            url: "src/parts",
+          },
+          {
+            name: "test",
+            size: "1",
+            type: "file",
+            url: "src/test" 
+          }
+      ],
+      name: "src"
+    };*/
     this.data = await Files.fileTree("src");
-  
+
   }
-  
+
   onExtentChanged() {
     this.fileTreemapRenderer.resize();
     this.fileTreemapRenderer.config.altered.alter('any');
     this.fileTreemapRenderer.updateView();
   }
-  
+
   //TODO lively migrate visualization._renderer._camera
+
 }
 
 const VisualizationType = {
-    VISUALIZATION_2D: 0,
-    VISUALIZATION_3D: 1
-  };
+  VISUALIZATION_2D: 0,
+  VISUALIZATION_3D: 1
+};
 
 class FileTreemapRenderer extends gloperate.Initializable {
-  
+
   initialize(htmlCanvasElement) {
     this.canvas = initializeCanvas(htmlCanvasElement);
-    this.visualization = new Visualization(this.visualizationType ? this.visualizationType : VisualizationType.VISUALIZATION_3D);
+    this.visualization = new Visualization(this.visualizationType ? this.visualizationType : VisualizationType
+      .VISUALIZATION_3D);
     this.renderer = this.visualization.renderer;
     this.canvas.renderer = this.renderer;
     this._initialized = true;
@@ -52,97 +69,107 @@ class FileTreemapRenderer extends gloperate.Initializable {
     return true;
   }
 
+  
   //TODO: this should be called to get rid of old WebGL Contexts
   uninitialize() {
     this.canvas.dispose();
     this.renderer.uninitialize();
   }
-  
-  
+
+
   updateView() {
     this.visualization.update();
   }
-  
-  
+
+
   resize() {
     //TODO: Doesn't work yet
     this.visualization.renderer._altered.alter("frameSize");
   }
+
   
-  setData(data = undefined) { 
-    this.config = this.makeExampleConfig();
+  setData(data = undefined, weightAttribute = undefined, heightAttribute = undefined, colorAttribute = undefined) {
+    //TODO: for generic use
+    if(weightAttribute) this.weightAttribute = weightAttribute;
+    if(heightAttribute) this.heightAttribute = heightAttribute;
+    if(colorAttribute) this.colorAttribute = colorAttribute;
+
+    if(!this.config) this.config = this.setupConfig();
     
-    if(!data) {
+    if (!data) {
       this.visualization.configuration = this.config;
       return;
     }
     
     const topologyData = [];
     const weightData = [];
+    const heightData = [];
     const labelData = [];
     const colorData = [];
+    let indexID = 0;
     
-    //TODO: optimize structure
-    function readChildren(parentURL, dataContext) {
-      if(parentURL) {
-        const identifier = dataContext.url;
-        
-        topologyData.push(parentURL, identifier);
-        colorData.push([identifier, (dataContext.type == "file" ? 0.9 : 0.2)]);
-        labelData.push([identifier, dataContext.name]);
-        
-        if(!dataContext.children) {
-          // this is a leaf node
-          weightData.push([identifier, dataContext.size]);
-          return dataContext.size;
-        }
-        
-        let accumulatedWeight = 0;
-        dataContext.children.forEach((child) => accumulatedWeight += Number(readChildren(dataContext.url, child)));
-        weightData.push([identifier, accumulatedWeight]);
-        return accumulatedWeight;
-
-      }
-      // this is the root
+    //read JSON using BFS
+    const queue = [{nodeData: data, parentID: undefined}]
+    
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      if(currentNode == undefined) continue;
+      const currentID = indexID.toString();
+      indexID++;
       
-      labelData.push([dataContext.name, dataContext.name]);
-      if(!dataContext.children) return 0;
-      let accumulatedWeight = 0;
-      dataContext.children.forEach((child) => accumulatedWeight += Number(readChildren(dataContext.name, child)));
-      console.log(dataContext.name);
-      console.log(accumulatedWeight);
-      weightData.push([dataContext.name, accumulatedWeight]);
-      return accumulatedWeight;
+      if(currentNode.parentID !== undefined) {
+        topologyData.push(currentNode.parentID, currentID);
+        labelData.push([currentID, currentNode.nodeData.name]); 
+      }
+      
+      weightData.push([currentID, Number((currentNode.nodeData.size && currentNode.nodeData.type == "file") ? currentNode.nodeData.size : 0)]);
+        heightData.push([currentID, 1]);
+        colorData.push([currentID, 0.5]);
+      
+      if(!currentNode.nodeData.children) continue;
+      for(const child of currentNode.nodeData.children) {
+        queue.push({nodeData: child, parentID: currentID});
+      }
     }
 
-    readChildren(undefined, data);
-    
-    //TODO: EvalError: Accumulated leaf weights as root weight expected.
-    // check Example parser
+    console.log("labels before", this.config.labels.names);
+    console.log("weights before", this.config.buffers[0].data);
+    console.log("height before", this.config.buffers[1].data);
+    console.log("colors before", this.config.buffers[2].data);
+    console.log("topology before", this.config.topology.edges);
     
     this.config.topology.edges = topologyData;
     this.config.buffers[0].data = Object.fromEntries(weightData);
+    this.config.buffers[1].data = Object.fromEntries(heightData);
     this.config.buffers[2].data = Object.fromEntries(colorData);
-    this.config.labels.names = Object.fromEntries(labelData);      
+    this.config.labels.names = new Map(Object.entries(Object.fromEntries(labelData)));
+    
+    console.log("labels after", this.config.labels.names);
+    console.log("weights after", this.config.buffers[0].data);
+    console.log("height after", this.config.buffers[1].data);
+    console.log("colors after", this.config.buffers[2].data);
+    console.log("topology after", this.config.topology.edges);
     
     this.visualization.configuration = this.config;
-    console.log(this.config);
+    
   }
+
   
-  makeExampleConfig() {
-    const config = new Configuration();
+  setupConfig() {
+    let config = new Configuration();
+    
     config.topology = {
-        edges: [0,1,0,2,0,3,0,4,1,5,1,6,2,7,2,8,3,9,3,10,4,11,4,12],
-        format: "interleaved",
-        semantics: "parent-id-id"
-      };
+      edges: [0,1, 0,2],
+      format: "interleaved",
+      semantics: "parent-id-id"
+    }
     
     config.layout = {
       algorithm: "strip",
       weight: "bufferView:weights",
       sort: {
-      key: "bufferView:weights",
-      algorithm: "keep"
+        key: "bufferView:weights",
+        algorithm: "keep"
       },
       parentPadding: {
         type: "relative",
@@ -155,86 +182,53 @@ class FileTreemapRenderer extends gloperate.Initializable {
       accessoryPadding: {
         type: "absolute",
         direction: "bottom",
-          value: [
-            0,
-            0.02,
-            0.01,
-            0
-          ],
-          relativeAreaThreshold: 0.4,
-          targetAspectRatio: 8
-        }
-      };
-
-      config.buffers = [
-        {
-          identifier: "source-weights",
-          type: "numbers",
-          data: {
-            "0": 0,
-            "1": -1,
-            "2": -1,
-            "3": -1,
-            "4": -1,
-            "5": 0.125,
-            "6": 0.25,
-            "7": 0.125,
-            "8": 0.125,
-            "9": 0.5,
-            "10": 0.125,
-            "11": 0.5,
-            "12": 0.5
-          },
-          linearization: "topology"
+        value: [
+          0,
+          0.02,
+          0.01,
+          0
+        ],
+        relativeAreaThreshold: 0.4,
+        targetAspectRatio: 8
+      }
+    }
+    
+    config.buffers = [{
+        identifier: "source-weights",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": 1,
+          "2": 1
         },
-        {
-          identifier: "source-heights",
-          type: "numbers",
-          data: {
-            "0": 0,
-            "1": -1,
-            "2": -1,
-            "3": -1,
-            "4": -1,
-            "5": 0.1,
-            "6": 0.1,
-            "7": 0.3,
-            "8": 0.2,
-            "9": 0.1,
-            "10": 0.1,
-            "11": 0.1,
-            "12": 0.2
-          },
-          linearization: "topology"
-        },
-        {
-          identifier: "source-colors",
-          type: "numbers",
-          data: {
-            "0": 0,
-            "1": -1,
-            "2": -1,
-            "3": -1,
-            "4": -1,
-            "5": 0,
-            "6": 0.1,
-            "7": 0.3,
-            "8": 0.6,
-            "9": 0.7,
-            "10": 1,
-            "11": 0,
-            "12": 0
-          },
-          linearization: "topology"
-        }
-      ];
-
-      config.bufferViews = [
+        linearization: "topology"
+      },
       {
+        identifier: "source-heights",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": 1,
+          "2": 1
+        },
+        linearization: "topology"
+      },
+      {
+        identifier: "source-colors",
+        type: "numbers",
+        data: {
+          "0": 0,
+          "1": 1,
+          "2": 0.5,
+        },
+        linearization: "topology"
+      }
+    ];
+
+    config.bufferViews = [{
         identifier: "weights",
         source: "buffer:source-weights",
-        transformations: [
-          {
+        transformations: [{
             type: "fill-invalid",
             value: 0,
             invalidValue: -1
@@ -248,8 +242,7 @@ class FileTreemapRenderer extends gloperate.Initializable {
       {
         identifier: "heights-normalized",
         source: "buffer:source-heights",
-        transformations: [
-          {
+        transformations: [{
             type: "fill-invalid",
             value: 0,
             invalidValue: -1
@@ -263,8 +256,7 @@ class FileTreemapRenderer extends gloperate.Initializable {
       {
         identifier: "colors-normalized",
         source: "buffer:source-colors",
-        transformations: [
-          {
+        transformations: [{
             type: "fill-invalid",
             value: 0,
             invalidValue: -1
@@ -276,9 +268,8 @@ class FileTreemapRenderer extends gloperate.Initializable {
         ]
       }
     ];
-
-      config.colors = [
-      {
+    
+    config.colors = [{
         identifier: "emphasis",
         colorspace: "hex",
         value: "#00b0ff"
@@ -303,21 +294,25 @@ class FileTreemapRenderer extends gloperate.Initializable {
         identifier: "leaf",
         preset: "Blues",
         steps: 7
-      }];
-      config.geometry = {
-        parentLayer: {showRoot: false},
-        leafLayer: {
-          colorMap: "color:leaf",
-          height: "bufferView:heights-normalized",
-          colors: "bufferView:colors-normalized"
-        },
-        emphasis: {
-          outline: [],
-          highlight: []
-        },
-        heightScale: 0.5
-      };
-      config.labels = {"innerNodeLayerRange": [
+      }
+    ];
+    
+    config.geometry = {
+      parentLayer: { showRoot: false },
+      leafLayer: {
+        colorMap: "color:leaf",
+        height: "bufferView:heights-normalized",
+        colors: "bufferView:colors-normalized"
+      },
+      emphasis: {
+        outline: [],
+        highlight: []
+      },
+      heightScale: 0.5
+    };
+    
+    config.labels = {
+      "innerNodeLayerRange": [
         1,
         2
       ],
@@ -326,28 +321,20 @@ class FileTreemapRenderer extends gloperate.Initializable {
       numTopHeightNodes: 50,
       numTopColorNodes: 50,
       names: {
-        "1": "a",
-        "2": "b",
-        "3": "c",
-        "4": "d",
-        "5": "e",
-        "6": "f",
-        "7": "g",
-        "8": "h",
-        "9": "i",
-        "10": "j",
-        "11": "k",
-        "12": "l"
+          "1": "leaf 1",
+          "2": "leaf 2"
       }
     };
     return config;
   }
+
   
   setColorScheme(preset) {
     this.config.colors[3].preset = preset;
     this.config.altered.alter("colors");
     this.updateView();
   }
+
   
   setColorSteps(steps) {
     this.config.colors[3].steps = steps;
@@ -355,31 +342,37 @@ class FileTreemapRenderer extends gloperate.Initializable {
     this.updateView();
 
   }
+
   
   //TODO figure out highlighting mechanism
-  highlightNodes(/*TODO*/) {
+  highlightNodes( /*TODO*/ ) {
     /*TODO*/
   }
+
   
-  removeNodeHighlights(/*TODO*/) {
+  removeNodeHighlights( /*TODO*/ ) {
     /*TODO*/
   }
+
   
   //TODO change mappings
-  setColorAttribute(/*TODO*/) {
+  setColorAttribute( /*TODO*/ ) {
     /*TODO*/
   }
+
   
-  setWeightAttribute(/*TODO*/) {
+  setWeightAttribute( /*TODO*/ ) {
     /*TODO*/
   }
+
   
-  setHeightAttribute(/*TODO*/) {
+  setHeightAttribute( /*TODO*/ ) {
     /*TODO*/
   }
+
   
   setVisualizationType(visualizationType) {
-    if(visualizationType !== this.visualizationType) {
+    if (visualizationType !== this.visualizationType) {
       this.visualizationType = visualizationType;
       this.visualization = new Visualization(this.visualizationType);
       this.renderer.uninitialize();
@@ -387,5 +380,4 @@ class FileTreemapRenderer extends gloperate.Initializable {
       this.canvas.renderer = this.renderer;
     }
   }
-  
 }
