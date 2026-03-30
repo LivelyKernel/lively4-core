6c4b468dccc80def35fdcd6eab54c14e950170a1
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2026-03-30 13:34:33 +0200

    updated icons in lively_simulation
    SQUASHED: AUTO-COMMIT-demos-lively-simulation-energy-simulation-original.html,

--- src/components/demo/lively-simulation.js
+++ src/components/demo/lively-simulation.js
@@ -229,7 +229,7 @@ export default class LivelySimulation extends Morph {
 // #Battery.max = 0.1 * 3600 // 1h 2kw
 // #HeatStorage.max = 0.1 * 3600 // 1h 3kw" data-view="codeView" data-loginterval="1" data-lively-id="db4cfe19-7ed2-4d9b-9b2b-2aa517e80e5f" data-should-skip="true"></lively-simulation-cell><lively-simulation-cell style="top: 320px; left: 23.2561px; width: 246.35px; height: 148.399px; z-index: 119;" data-name="Fuel" data-state="{&quot;gas&quot;:36000}" data-snippet="if (gas < 0) {
 //     throw Error(&quot;Gas Empty&quot;)
-// }" data-view="codeView" data-loginterval="1" data-lively-id="358a6017-4bb6-4d57-b998-4221423bfda2"><img src="https://image.flaticon.com/icons/svg/2362/2362783.svg" style="position: relative; width: 53px; height: 62px; left: 119px; top: 4px;" class="">
+// }" data-view="codeView" data-loginterval="1" data-lively-id="358a6017-4bb6-4d57-b998-4221423bfda2"><i class="fa fa-tint" style="position: relative; font-size: 53px; left: 119px; top: 4px;"></i>
 // <div class="lively-text lively-content" contenteditable="true" style="width: 183px;position: absolute;left: 24px;top: -59px;height: 63px;">Click here to see all cell that require fuel<div>|</div></div></lively-simulation-cell><lively-simulation-cell style="top: 498.74px; left: 322.256px; width: 539.826px; height: 279.414px; z-index: 124;" data-name="Thermal Power Station" data-state="{&quot;heat&quot;:0,&quot;factor&quot;:10,&quot;max&quot;:7.5}" data-snippet="var full = max * factor;
 // if (heat < full &amp;&amp; #Fuel.gas > max * dt
 //     &amp;&amp; (#Battery.energy < 0.9 * #Battery.max )) {
@@ -246,7 +246,7 @@ export default class LivelySimulation extends Morph {
 //     if (#Battery.energy < #Battery.max ) {
 //         #Battery.energy += 0.4 * delta;
 //     }
-// }" data-view="codeView" data-loginterval="1" data-lively-id="c6eae5ef-cdc4-46df-9258-c0db109f88b8"><img src="https://image.flaticon.com/icons/svg/857/857761.svg" style="width: 75px;height: 58px;position: relative;left: 10px;top: 0px;"><img src="https://image.flaticon.com/icons/svg/2983/2983973.svg" style="width: 53px;height: 37px;position: absolute;left: 101px;top: 232px;"></lively-simulation-cell><lively-simulation-cell style="top: 79px; left: 348.709px; z-index: 123; width: 500px; height: 264.28px;" data-name="Heating System" data-state="{&quot;heat&quot;:0,&quot;factor&quot;:20,&quot;max&quot;:5}" data-snippet="var full = max  * factor
+// }" data-view="codeView" data-loginterval="1" data-lively-id="c6eae5ef-cdc4-46df-9258-c0db109f88b8"><i class="fa fa-industry" style="font-size: 58px;position: relative;left: 10px;top: 0px;"></i><i class="fa fa-fire" style="font-size: 37px;position: absolute;left: 101px;top: 232px;"></i></lively-simulation-cell><lively-simulation-cell style="top: 79px; left: 348.709px; z-index: 123; width: 500px; height: 264.28px;" data-name="Heating System" data-state="{&quot;heat&quot;:0,&quot;factor&quot;:20,&quot;max&quot;:5}" data-snippet="var full = max  * factor
 // if (heat < full &amp;&amp; #Fuel.gas > max * dt &amp;&amp;
 //     #HeatStorage.energy < 0.3 * #HeatStorage.max) {
 //     var gas = max * dt;
@@ -260,9 +260,9 @@ export default class LivelySimulation extends Morph {
 //     #HeatStorage.energy += 0.8 * delta;
 // } else {
 //     heat -= 0.2 * delta;
-// }" data-view="codeView" data-loginterval="1" data-lively-id="80937f3f-2786-45de-8691-c83825696023"><img src="https://image.flaticon.com/icons/svg/857/857761.svg" style="width: 75px; height: 58px; position: relative; left: -2px; top: -2px;">
+// }" data-view="codeView" data-loginterval="1" data-lively-id="80937f3f-2786-45de-8691-c83825696023"><i class="fa fa-industry" style="font-size: 58px; position: relative; left: -2px; top: -2px;"></i>
 // <div class="lively-text lively-content" contenteditable="true" style="width: 300px; position: absolute; left: 143px; top: 216px;">A heating system requires fuel and produces heat</div></lively-simulation-cell><lively-simulation-cell style="top: 282px; left: 861.269px; z-index: 125; width: 284.115px; height: 74.4531px;" data-name="Heat Storage" data-state="{&quot;energy&quot;:0,&quot;max&quot;:360}" data-snippet="// Enter simulation code here" data-view="codeView" data-loginterval="1" data-lively-id="79eca28f-b50d-49b9-9d0e-b426c590fb67">
-// <img src="https://image.flaticon.com/icons/svg/870/870620.svg" style="width: 28px;height: 44px;position: relative;left: 30px;top: 0;"></lively-simulation-cell><lively-simulation-cell style="top: 394.26px; left: 863px; z-index: 126; width: 164.533px; height: 100px;" data-name="Battery" data-state="{&quot;energy&quot;:0,&quot;max&quot;:360}" data-snippet="" data-view="codeView" data-loginterval="1" data-lively-id="15c89e50-98bf-4476-91fb-eec24c7173da"><img src="https://image.flaticon.com/icons/svg/3165/3165660.svg" style="width: 174px;height: 46px;position: relative;left: -36px;top: 0;"></lively-simulation-cell><lively-simulation-cell style="top: 183px; left: 1191.27px; z-index: 128; width: 468.533px; height: 200px; position: absolute;" data-name="Heat Consumer" data-state="{&quot;consumed&quot;:0,&quot;extra&quot;:0,&quot;demand&quot;:3}" data-snippet="var delta = demand * dt
+// <i class="fa fa-database" style="font-size: 44px;position: relative;left: 30px;top: 0;"></i></lively-simulation-cell><lively-simulation-cell style="top: 394.26px; left: 863px; z-index: 126; width: 164.533px; height: 100px;" data-name="Battery" data-state="{&quot;energy&quot;:0,&quot;max&quot;:360}" data-snippet="" data-view="codeView" data-loginterval="1" data-lively-id="15c89e50-98bf-4476-91fb-eec24c7173da"><i class="fa fa-battery-full" style="font-size: 46px;position: relative;left: -36px;top: 0;"></i></lively-simulation-cell><lively-simulation-cell style="top: 183px; left: 1191.27px; z-index: 128; width: 468.533px; height: 200px; position: absolute;" data-name="Heat Consumer" data-state="{&quot;consumed&quot;:0,&quot;extra&quot;:0,&quot;demand&quot;:3}" data-snippet="var delta = demand * dt
 // if (#HeatStorage.energy > delta) {
 //     #HeatStorage.energy -= delta
 //     consumed += delta
@@ -271,7 +271,7 @@ export default class LivelySimulation extends Morph {
 //     extra  +=  delta
 //     throw Error(&quot;RoomToCold&quot;)
 // }
-// " data-view="codeView" data-loginterval="1" data-lively-id="8c62075a-2da9-4c8a-b389-2db5668abc39"><img src="https://image.flaticon.com/icons/svg/1422/1422837.svg" style="width: 48px;height: 38px;position: relative;left: 71px;top: -3px;"></lively-simulation-cell><lively-simulation-cell style="top: 399.735px; left: 1189px; z-index: 127; width: 447.844px; height: 180px;" data-name="Electric Consumer" data-state="{&quot;consumed&quot;:0,&quot;extra&quot;:0,&quot;demand&quot;:1.5}" data-snippet="demand += (Math.random() - 0.5) * 0.01 * dt
+// " data-view="codeView" data-loginterval="1" data-lively-id="8c62075a-2da9-4c8a-b389-2db5668abc39"><i class="fa fa-home" style="font-size: 38px;position: relative;left: 71px;top: -3px;"></i></lively-simulation-cell><lively-simulation-cell style="top: 399.735px; left: 1189px; z-index: 127; width: 447.844px; height: 180px;" data-name="Electric Consumer" data-state="{&quot;consumed&quot;:0,&quot;extra&quot;:0,&quot;demand&quot;:1.5}" data-snippet="demand += (Math.random() - 0.5) * 0.01 * dt
 // var delta = demand * dt
 // if (#Battery.energy > delta) {
 //     #Battery.energy -= delta
@@ -280,7 +280,7 @@ export default class LivelySimulation extends Morph {
 //     extra += delta
 //     throw Error(&quot;EngeryToLow&quot;)
 // }
-// " data-view="codeView" data-loginterval="1" data-lively-id="5ee77098-fadb-4e95-9dea-1870e9af548a"><img src="https://image.flaticon.com/icons/svg/2910/2910875.svg" style="width: 137px;height: 59px;position: relative;left: 163px;top: 0;"></lively-simulation-cell>
+// " data-view="codeView" data-loginterval="1" data-lively-id="5ee77098-fadb-4e95-9dea-1870e9af548a"><i class="fa fa-plug" style="font-size: 59px;position: relative;left: 163px;top: 0;"></i></lively-simulation-cell>
 // `;
 //       this.engine.setVelocity(10);
 //       this.engine.stopOnError = false;
